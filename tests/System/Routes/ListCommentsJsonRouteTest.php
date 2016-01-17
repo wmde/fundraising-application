61368b185f1c9bc982d80ceb10c581efd948397f
jeroendedauw <jeroendedauw@gmail.com>
2016-01-17 16:09:25 +0100

    Rename route to list-comments.json

--- tests/System/Routes/ListCommentsJsonRouteTest.php
+++ tests/System/Routes/ListCommentsJsonRouteTest.php
@@ -8,11 +8,11 @@ use WMDE\Fundraising\Frontend\Tests\System\SystemTestCase;
  * @licence GNU GPL v2+
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  */
-class ListCommentsRssRouteTest extends SystemTestCase {
+class ListCommentsJsonRouteTest extends SystemTestCase {
 
 	public function testRouteDoesSomething() {
 		$client = $this->createClient();
-		$client->request( 'GET', '/list.rss' );
+		$client->request( 'GET', '/list-comments.json' );
 
 		$this->assertSame(
 			'TODO',
