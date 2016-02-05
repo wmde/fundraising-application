3284280a0e048c7ee1fd88b0bdab002029c388c2
jeroendedauw <jeroendedauw@gmail.com>
2016-02-05 05:07:26 +0100

    Remove unused imports

--- src/UseCases/CancelDonation/CancelDonationUseCase.php
+++ src/UseCases/CancelDonation/CancelDonationUseCase.php
@@ -4,9 +4,6 @@ declare(strict_types = 1);
 
 namespace WMDE\Fundraising\Frontend\UseCases\CancelDonation;
 
-use WMDE\Fundraising\Frontend\Domain\Comment;
-use WMDE\Fundraising\Frontend\Domain\CommentRepository;
-
 /**
  * @licence GNU GPL v2+
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
