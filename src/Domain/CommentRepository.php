3f35040a12677fb0bf7296de85f20d4ec7482ade
jeroendedauw <jeroendedauw@gmail.com>
2016-02-05 04:57:16 +0100

    Remove no longer needed TODO

--- src/Domain/CommentRepository.php
+++ src/Domain/CommentRepository.php
@@ -5,9 +5,6 @@ declare(strict_types = 1);
 namespace WMDE\Fundraising\Frontend\Domain;
 
 /**
- * TODO: it is not clear at present that we gain much by having this additional
- * repository layer between the usecase and the data access implementation.
- *
  * @licence GNU GPL v2+
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  */
