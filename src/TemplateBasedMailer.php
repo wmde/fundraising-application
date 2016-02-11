b39e82fbcbf3ad39de515b3a6ea692e39809bb41
jeroendedauw <jeroendedauw@gmail.com>
2016-02-11 15:08:02 +0100

    Remove unused imports

--- src/TemplateBasedMailer.php
+++ src/TemplateBasedMailer.php
@@ -4,9 +4,6 @@ declare(strict_types = 1);
 
 namespace WMDE\Fundraising\Frontend;
 
-use Swift_Message;
-use Swift_Transport;
-
 /**
  * @licence GNU GPL v2+
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
