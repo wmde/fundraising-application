a021677e3a935d6b63728e2aae83a21cb79fb688
Gabriel Birke <gabriel.birke@wikimedia.de>
2016-02-25 16:39:59 +0100

    make actions module more DRY

--- app/js/lib/actions.js
+++ app/js/lib/actions.js
@@ -1,27 +1,23 @@
 'use strict';
 
-function newAddPageAction( pageName ) {
-	return {
-		type: 'ADD_PAGE',
-		payload: { name: pageName }
-	};
-}
-
-function newNextPageAction() {
-	return {
-		type: 'NEXT_PAGE'
-	};
-}
-
-function newStoreValidationResultAction( isValid ) {
-	return {
-		type: 'VALIDATION_RESULT',
-		payload: { isValid: isValid }
-	};
-}
-
 module.exports = {
-	newAddPageAction: newAddPageAction,
-	newNextPageAction: newNextPageAction,
-	newStoreValidationResultAction: newStoreValidationResultAction
+	newAddPageAction: function ( pageName ) {
+		return {
+			type: 'ADD_PAGE',
+			payload: { name: pageName }
+		};
+	},
+
+	newNextPageAction: function () {
+		return {
+			type: 'NEXT_PAGE'
+		};
+	},
+
+	newStoreValidationResultAction: function ( isValid ) {
+		return {
+			type: 'VALIDATION_RESULT',
+			payload: { isValid: isValid }
+		};
+	}
 };
