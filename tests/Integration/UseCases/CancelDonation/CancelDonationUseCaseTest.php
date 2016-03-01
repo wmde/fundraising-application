21db1b81a7a8a8f79e3a7288bb17713b0a767303
jeroendedauw <jeroendedauw@gmail.com>
2016-03-01 13:59:17 +0100

    Add basis for cancel donation confirmation email

--- tests/Integration/UseCases/CancelDonation/CancelDonationUseCaseTest.php
+++ tests/Integration/UseCases/CancelDonation/CancelDonationUseCaseTest.php
@@ -4,11 +4,19 @@ declare( strict_types = 1 );
 
 namespace WMDE\Fundraising\Frontend\Tests\Integration\UseCases\ConfirmSubscription;
 
+use PHPUnit_Framework_MockObject_MockObject;
+use Swift_NullTransport;
 use WMDE\Fundraising\Frontend\Domain\Model\Donation;
+use WMDE\Fundraising\Frontend\Domain\Model\MailAddress;
 use WMDE\Fundraising\Frontend\Domain\Model\PaymentType;
+use WMDE\Fundraising\Frontend\Domain\Repositories\DonationRepository;
+use WMDE\Fundraising\Frontend\Factories\FunFunFactory;
+use WMDE\Fundraising\Frontend\Infrastructure\Messenger;
+use WMDE\Fundraising\Frontend\Infrastructure\TemplateBasedMailer;
 use WMDE\Fundraising\Frontend\Tests\Data\ValidDonation;
 use WMDE\Fundraising\Frontend\Tests\TestEnvironment;
 use WMDE\Fundraising\Frontend\UseCases\CancelDonation\CancelDonationRequest;
+use WMDE\Fundraising\Frontend\UseCases\CancelDonation\CancelDonationUseCase;
 
 /**
  * @covers WMDE\Fundraising\Frontend\UseCases\CancelDonation\CancelDonationUseCase
@@ -19,15 +27,30 @@ use WMDE\Fundraising\Frontend\UseCases\CancelDonation\CancelDonationRequest;
 class CancelDonationUseCaseTest extends \PHPUnit_Framework_TestCase {
 
 	public function testGivenIdOfUnknownDonation_cancellationIsNotSuccessful() {
-		$useCase = TestEnvironment::newInstance()->getFactory()->newCancelDonationUseCase();
+		$useCase = $this->newUseCase();
 
 		$response = $useCase->cancelDonation( new CancelDonationRequest( 1337, 'token', 'updateToken' ) );
 
 		$this->assertFalse( $response->cancellationWasSuccessful() );
 	}
 
+	private function newUseCase(): CancelDonationUseCase {
+		return $this->newFactoryWithNullMailer()->newCancelDonationUseCase();
+	}
+
+	private function newFactoryWithNullMailer(): FunFunFactory {
+		$factory = TestEnvironment::newInstance()->getFactory();
+
+		$factory->setMessenger( new Messenger(
+			Swift_NullTransport::newInstance(),
+			$factory->getOperatorAddress()
+		) );
+
+		return $factory;
+	}
+
 	public function testResponseContainsDonationId() {
-		$useCase = TestEnvironment::newInstance()->getFactory()->newCancelDonationUseCase();
+		$useCase = $this->newUseCase();
 
 		$response = $useCase->cancelDonation( new CancelDonationRequest( 1337, 'token', 'updateToken' ) );
 
@@ -35,7 +58,7 @@ class CancelDonationUseCaseTest extends \PHPUnit_Framework_TestCase {
 	}
 
 	public function testGivenIdOfCancellableDonation_cancellationIsSuccessful() {
-		$factory = TestEnvironment::newInstance()->getFactory();
+		$factory = $this->newFactoryWithNullMailer();
 
 		$donation = ValidDonation::newDonation();
 		$donation->setStatus( Donation::STATUS_NEW );
@@ -44,13 +67,17 @@ class CancelDonationUseCaseTest extends \PHPUnit_Framework_TestCase {
 		$factory->getDonationRepository()->storeDonation( $donation );
 
 		$useCase = $factory->newCancelDonationUseCase();
-		$response = $useCase->cancelDonation( new CancelDonationRequest( $donation->getId(), 'token', 'updateToken' ) );
+		$response = $useCase->cancelDonation( new CancelDonationRequest(
+			$donation->getId(),
+			'token',
+			'updateToken'
+		) );
 
 		$this->assertTrue( $response->cancellationWasSuccessful() );
 	}
 
 	public function testGivenIdOfNonCancellableDonation_cancellationIsNotSuccessful() {
-		$factory = TestEnvironment::newInstance()->getFactory();
+		$factory = $this->newFactoryWithNullMailer();
 
 		$donation = ValidDonation::newDonation();
 		$donation->setStatus( Donation::STATUS_DELETED );
@@ -59,9 +86,73 @@ class CancelDonationUseCaseTest extends \PHPUnit_Framework_TestCase {
 		$factory->getDonationRepository()->storeDonation( $donation );
 
 		$useCase = $factory->newCancelDonationUseCase();
-		$response = $useCase->cancelDonation( new CancelDonationRequest( $donation->getId(), 'token', 'updateToken' ) );
+		$response = $useCase->cancelDonation( new CancelDonationRequest(
+			$donation->getId(),
+			'token',
+			'updateToken'
+		) );
 
 		$this->assertFalse( $response->cancellationWasSuccessful() );
+	}
+
+	public function testGivenValidRequest_cancellationConfirmationEmailIsSend() {
+		$donation = $this->newCancelableDonation();
+
+		$useCase = $this->newUseCaseWithMailerMock( $donation );
+
+		$response = $useCase->cancelDonation( new CancelDonationRequest(
+			$donation->getId(),
+			'token',
+			'updateToken'
+		) );
+
+		$this->assertTrue( $response->cancellationWasSuccessful() );
+	}
+
+	private function newCancelableDonation(): Donation {
+		$donation = ValidDonation::newDonation();
+
+		$donation->setStatus( Donation::STATUS_NEW );
+		$donation->setPaymentType( PaymentType::DIRECT_DEBIT );
+
+		return $donation;
+	}
+
+	private function newUseCaseWithMailerMock( Donation $donation ): CancelDonationUseCase {
+		$mailer = $this->newMailer();
+
+		$mailer->expects( $this->once() )
+			->method( 'sendMail' )
+			->with(
+				$this->equalTo( new MailAddress( $donation->getPersonalInfo()->getEmailAddress() ) ),
+				$this->callback( function( $value ) {
+					$this->assertInternalType( 'array', $value );
+					// TODO: assert parameters
+					return true;
+				} )
+			);
+
+		return new CancelDonationUseCase(
+			$this->getDonationRepositoryWithDonation( $donation ),
+			$mailer
+		);
+	}
+
+	/**
+	 * @return TemplateBasedMailer|PHPUnit_Framework_MockObject_MockObject
+	 */
+	private function newMailer(): TemplateBasedMailer {
+		return $this->getMockBuilder( TemplateBasedMailer::class )
+			->disableOriginalConstructor()
+			->getMock();
+	}
+
+	private function getDonationRepositoryWithDonation( Donation $donation ): DonationRepository {
+		$donationRepository = TestEnvironment::newInstance()->getFactory()->getDonationRepository();
+
+		$donationRepository->storeDonation( $donation );
+
+		return $donationRepository;
 	}
 
 }
