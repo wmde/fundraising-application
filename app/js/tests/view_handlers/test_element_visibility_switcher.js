490a7a5b1aecfeb8f222b315da657652669dc237
Gabriel Birke <gabriel.birke@wikimedia.de>
2016-04-20 14:07:04 +0200

    Add tests for slide animator

--- app/js/tests/view_handlers/test_element_visibility_switcher.js
+++ app/js/tests/view_handlers/test_element_visibility_switcher.js
@@ -2,7 +2,9 @@
 
 var test = require( 'tape' ),
 	sinon = require( 'sinon' ),
-	createElementCustomVisibilityHandler = require( '../../lib/view_handler/element_visibility_switcher' ).createElementCustomVisibilityHandler
+	visibilitySwitching = require( '../../lib/view_handler/element_visibility_switcher' ),
+	createElementCustomVisibilityHandler = visibilitySwitching.createElementCustomVisibilityHandler,
+	createElementSlideAnimationHandler = visibilitySwitching.createElementSlideAnimationHandler
 	;
 
 function createTestAnimator() {
@@ -66,5 +68,33 @@ test( 'When state matches value for hiding and element is already hidden, elemen
 	handler.update( '47' );
 
 	t.ok( animator.hideElement.calledOnce, 'hideElement was called once' );
+	t.end();
+} );
+
+test( 'Given a slide animation, When element is shown, element slides down', function ( t ) {
+	var element = {
+			slideDown: sinon.stub().returnsThis(),
+			animate: sinon.stub().returnsThis()
+		},
+		showValue = '4711',
+		handler = createElementSlideAnimationHandler( element, showValue );
+
+	handler.update( showValue );
+
+	t.ok( element.slideDown.calledOnce, 'slideDown was called once' );
+	t.end();
+} );
+
+test( 'When state does not match value for showing, element slides up', function ( t ) {
+	var element = {
+			slideUp: sinon.stub().returnsThis(),
+			animate: sinon.stub().returnsThis()
+		},
+		showValue = '4711',
+		handler = createElementSlideAnimationHandler( element, showValue );
+
+	handler.update( '23' );
+
+	t.ok( element.slideUp.calledOnce, 'slideUp was called once' );
 	t.end();
 } );
