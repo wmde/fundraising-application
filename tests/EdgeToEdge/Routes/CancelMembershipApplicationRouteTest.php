fdb2aad70a34f96556e8f6dc7edd95a402158933
jeroendedauw <jeroendedauw@gmail.com>
2016-04-20 17:24:52 +0200

    Added EtE tests for cancel membership application route

--- tests/EdgeToEdge/Routes/CancelMembershipApplicationRouteTest.php
+++ tests/EdgeToEdge/Routes/CancelMembershipApplicationRouteTest.php
@@ -4,7 +4,12 @@ declare( strict_types = 1 );
 
 namespace WMDE\Fundraising\Frontend\Tests\EdgeToEdge\Routes;
 
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\HttpKernel\Client;
+use WMDE\Fundraising\Frontend\Factories\FunFunFactory;
+use WMDE\Fundraising\Frontend\Tests\Data\ValidMembershipApplication;
 use WMDE\Fundraising\Frontend\Tests\EdgeToEdge\WebRouteTestCase;
+use WMDE\Fundraising\Store\MembershipApplicationData;
 
 /**
  * @licence GNU GPL v2+
@@ -12,11 +17,64 @@ use WMDE\Fundraising\Frontend\Tests\EdgeToEdge\WebRouteTestCase;
  */
 class CancelMembershipApplicationRouteTest extends WebRouteTestCase {
 
+	const CORRECT_UPDATE_TOKEN = 'b5b249c8beefb986faf8d186a3f16e86ef509ab2';
+
 	public function testGivenGetRequest_resultHasMethodNotAllowedStatus() {
 		$this->assertGetRequestCausesMethodNotAllowedResponse(
 			'cancel-membership-application',
 			[]
 		);
+	}
+
+	public function testGivenValidUpdateToken_confirmationPageIsShown() {
+		$this->createEnvironment( [], function( Client $client, FunFunFactory $factory ) {
+			$factory->setNullMessenger();
+
+			$applicationId = $this->storeApplication( $factory->getEntityManager() );
+
+			$client->request(
+				'POST',
+				'cancel-membership-application',
+				[
+					'sid' => (string)$applicationId,
+					'utoken' => self::CORRECT_UPDATE_TOKEN,
+				]
+			);
+
+			// TODO: replace when presenter is added
+			$this->assertContains( 'cancelled', $client->getResponse()->getContent() );
+		} );
+	}
+
+	public function testGivenInvalidUpdateToken_resultIsError() {
+		$this->createEnvironment( [], function( Client $client, FunFunFactory $factory ) {
+			$applicationId = $this->storeApplication( $factory->getEntityManager() );
+
+			$client->request(
+				'POST',
+				'cancel-membership-application',
+				[
+					'sid' => (string)$applicationId,
+					'utoken' => 'Not the correct update token',
+				]
+			);
+
+			// TODO: replace when presenter is added
+			$this->assertContains( 'error', $client->getResponse()->getContent() );
+		} );
+	}
+
+	private function storeApplication( EntityManager $entityManager ): int {
+		$application = ValidMembershipApplication::newDoctrineEntity();
+
+		$application->modifyDataObject( function( MembershipApplicationData $data ) {
+			$data->setUpdateToken( self::CORRECT_UPDATE_TOKEN );
+		} );
+
+		$entityManager->persist( $application );
+		$entityManager->flush();
+
+		return $application->getId();
 	}
 
 }
