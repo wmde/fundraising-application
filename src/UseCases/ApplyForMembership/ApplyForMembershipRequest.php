ce7d405f3f9ec58cc0c6f06a964cea65729920ea
jeroendedauw <jeroendedauw@gmail.com>
2016-04-21 23:35:08 +0200

    Added basic wiring for ApplyForDonation UC

--- src/UseCases/ApplyForMembership/ApplyForMembershipRequest.php
+++ src/UseCases/ApplyForMembership/ApplyForMembershipRequest.php
@@ -4,14 +4,11 @@ declare( strict_types = 1 );
 
 namespace WMDE\Fundraising\Frontend\UseCases\ApplyForMembership;
 
-use WMDE\Fundraising\Frontend\FreezableValueObject;
-
 /**
  * @license GNU GPL v2+
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  */
 class ApplyForMembershipRequest {
-	use FreezableValueObject;
 
 	// TODO
 
---
+++ tests/Integration/UseCases/ApplyForMembership/ApplyForMembershipUseCaseTest.php
@@ -0,0 +1,61 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace WMDE\Fundraising\Frontend\Tests\Integration\UseCases\CancelMembershipApplication;
+
+use WMDE\Fundraising\Frontend\Domain\Repositories\MembershipApplicationRepository;
+use WMDE\Fundraising\Frontend\Infrastructure\MembershipAppAuthUpdater;
+use WMDE\Fundraising\Frontend\Tests\Fixtures\InMemoryMembershipApplicationRepository;
+use WMDE\Fundraising\Frontend\Tests\Fixtures\TemplateBasedMailerSpy;
+use WMDE\Fundraising\Frontend\UseCases\ApplyForMembership\ApplyForMembershipRequest;
+use WMDE\Fundraising\Frontend\UseCases\ApplyForMembership\ApplyForMembershipUseCase;
+
+/**
+ * @covers WMDE\Fundraising\Frontend\UseCases\ApplyForMembership\ApplyForMembershipUseCase
+ *
+ * @license GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+class ApplyForMembershipUseCaseTest extends \PHPUnit_Framework_TestCase {
+
+	const ID_OF_NON_EXISTING_APPLICATION = 1337;
+
+	/**
+	 * @var MembershipApplicationRepository
+	 */
+	private $repository;
+
+	/**
+	 * @var MembershipAppAuthUpdater
+	 */
+	private $authUpdater;
+
+	/**
+	 * @var TemplateBasedMailerSpy
+	 */
+	private $mailer;
+
+	public function setUp() {
+		$this->repository = new InMemoryMembershipApplicationRepository();
+		$this->authUpdater = $this->getMock( MembershipAppAuthUpdater::class );
+		$this->mailer = new TemplateBasedMailerSpy( $this );
+	}
+
+	public function testGivenValidRequest_applicationSucceeds() {
+		$request = new ApplyForMembershipRequest();
+
+		$response = $this->newUseCase()->applyForMembership( $request );
+
+		$this->assertTrue( $response->isSuccessful() );
+	}
+
+	private function newUseCase(): ApplyForMembershipUseCase {
+		return new ApplyForMembershipUseCase(
+			$this->repository,
+			$this->authUpdater,
+			$this->mailer
+		);
+	}
+
+}
