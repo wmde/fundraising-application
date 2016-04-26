2778ff1b6a66fa27eecf6aecbe7f36f76b2e1c28
Gabriel Birke <gabriel.birke@gmail.com>
2016-04-26 21:02:43 +0200

    Add class to display address

---
+++ app/js/lib/view_handler/data_display.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var objectAssign = require( 'object-assign' ),
+	_ = require( 'lodash' ),
+	AddressDisplayHandler = {
+		fullName: null,
+		street: null,
+		postcode: null,
+		city: null,
+		country: null,
+		email: null,
+		update: function ( formContent ) {
+			var name;
+			if ( formContent.addressType === 'person' ) {
+				name = [
+					formContent.salutation,
+					formContent.title,
+					formContent.firstName,
+					formContent.lastName
+				].join( ' ' );
+			} else if ( formContent.addressType === 'firma' ) {
+				name = formContent.company;
+			} else {
+				name = '';
+			}
+			this.fullName.text( name );
+			this.street.text( formContent.street );
+			this.postcode.text( formContent.postcode );
+			this.city.text( formContent.city );
+			this.country.text( formContent.country );
+			this.email.text( formContent.email );
+		}
+	};
+
+module.exports = {
+	createAddressDisplayHandler: function ( elementConfig ) {
+		var expectedConfigProperties = [
+				'fullName',
+				'street',
+				'postcode',
+				'city',
+				'country',
+				'email'
+			],
+			uncofiguredElements = _.difference( expectedConfigProperties, _.keys( elementConfig ) );
+		if ( uncofiguredElements.length > 0 ) {
+			throw new Error( 'The following elements were not configured: ' + uncofiguredElements.join( ', ' ) );
+		}
+		return objectAssign( Object.create( AddressDisplayHandler ), elementConfig );
+	}
+};
---
+++ app/js/tests/view_handlers/test_data_display.js
@@ -0,0 +1,85 @@
+'use strict';
+
+var test = require( 'tape' ),
+	sinon = require( 'sinon' ),
+	dataDisplayHandlers = require( '../../lib/view_handler/data_display' ),
+	createAddressDisplayHandler = dataDisplayHandlers.createAddressDisplayHandler,
+	createElement = function () {
+		return {
+			text: sinon.spy()
+		};
+	},
+	createAddressConfigSpy = function () {
+		return {
+			fullName: createElement(),
+			street: createElement(),
+			postcode: createElement(),
+			city: createElement(),
+			country: createElement(),
+			email: createElement()
+		};
+	}
+	;
+
+test( 'Address data is set', function ( t ) {
+	var addressConfig = createAddressConfigSpy(),
+		handler = createAddressDisplayHandler( addressConfig );
+
+	handler.update( {
+		salutation: 'Herr',
+		title: 'Dr.',
+		firstName: 'Hans',
+		lastName: 'Gruber',
+		street: '2121 Avenue of the Stars',
+		postcode: '90067',
+		city: 'Los Angeles',
+		country: 'United States of America',
+		email: 'hans@example.com',
+		addressType: 'person'
+	} );
+	t.ok( addressConfig.fullName.text.calledWith( 'Herr Dr. Hans Gruber' ), 'full name is set with salutation and title' );
+	t.ok( addressConfig.street.text.calledWith( '2121 Avenue of the Stars' ), 'street is set'  );
+	t.ok( addressConfig.postcode.text.calledWith( '90067' ), 'postcode is set'  );
+	t.ok( addressConfig.city.text.calledWith( 'Los Angeles' ), 'city is set'  );
+	t.ok( addressConfig.country.text.calledWith( 'United States of America' ), 'country is set'  );
+	t.ok( addressConfig.email.text.calledWith( 'hans@example.com' ), 'email is set'  );
+
+	t.end();
+} );
+
+test( 'Given a company address, Company name is displayed in address instead of name', function ( t ) {
+	var addressConfig = createAddressConfigSpy(),
+		handler = createAddressDisplayHandler( addressConfig );
+
+	handler.update( {
+		salutation: 'Herr',
+		title: 'Dr.',
+		firstName: 'Hans',
+		lastName: 'Gruber',
+		company: 'Nakatomi Corporation',
+		street: '2121 Avenue of the Stars',
+		postcode: '90067',
+		city: 'Los Angeles',
+		country: 'United States of America',
+		email: 'hans@example.com',
+		addressType: 'firma'
+	} );
+	t.ok( addressConfig.fullName.text.calledWith( 'Nakatomi Corporation' ), 'company name is set' );
+
+	t.end();
+} );
+
+test( 'Address display handler checks if all elements are configured', function ( t ) {
+	var addressWithMissingFullName = {
+			street: createElement(),
+			postcode: createElement(),
+			city: createElement(),
+			country: createElement(),
+			email: createElement()
+		};
+	t.throws( function () {
+		createAddressDisplayHandler( addressWithMissingFullName );
+	} );
+
+	t.end();
+} );
