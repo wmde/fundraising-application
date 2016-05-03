47fd923e34f8aba27d44122aa5503ac9566e6f2f
Gabriel Birke <gabriel.birke@wikimedia.de>
2016-05-03 14:13:06 +0200

    Use amount and period view handler in form

--- app/js/tests/view_handlers/test_display_period_and_amount.js
+++ app/js/tests/view_handlers/test_display_period_and_amount.js
@@ -27,7 +27,7 @@ test( 'The amount is passed to the curreny formatter', function ( t ) {
 		handler = createPaymentPeriodAndAmountDisplayHandler( periodElement, amountElement, paymentPeriodTranslations, currencyFormatter );
 	handler.update( {
 		amount: '23,00',
-		period: '0'
+		paymentPeriodInMonths: '0'
 	} );
 	t.ok( currencyFormatter.format.calledOnce, 'format is called' );
 	t.equals( currencyFormatter.format.firstCall.args[ 0 ], '23,00', 'Amount is passed to formatter' );
@@ -40,9 +40,31 @@ test( 'Formatted amount is set in amount element', function ( t ) {
 		handler = createPaymentPeriodAndAmountDisplayHandler( periodElement, amountElement, paymentPeriodTranslations, currencyFormatter );
 	handler.update( {
 		amount: '23,0',
-		period: '0'
+		paymentPeriodInMonths: '0'
 	} );
 	t.ok( amountElement.text.calledOnce, 'Amount is set' );
 	t.equals( amountElement.text.firstCall.args[ 0 ], formattedAmount, 'amount is set' );
 	t.end();
 } );
+
+test( 'Formatted period is set in period element', function ( t ) {
+	var amountElement = createElement(),
+		periodElement = createElement(),
+		handler = createPaymentPeriodAndAmountDisplayHandler( periodElement, amountElement, paymentPeriodTranslations, currencyFormatter );
+	handler.update( {
+		amount: '23,0',
+		paymentPeriodInMonths: '0'
+	} );
+	t.ok( periodElement.text.calledOnce, 'Period is set' );
+	t.equals( periodElement.text.firstCall.args[ 0 ], 'einmalig', 'amount is set' );
+
+	handler.update( {
+		amount: '23,0',
+		paymentPeriodInMonths: '3'
+	} );
+	t.ok( periodElement.text.calledTwice, 'Period is set' );
+	t.equals( periodElement.text.secondCall.args[ 0 ], 'quartalsweise', 'amount is set' );
+
+	t.end();
+} );
+
