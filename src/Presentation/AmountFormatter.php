bd11b1df4d908f2db078eaa0c347e948a4d9b7aa
Gabriel Birke <gabriel.birke@wikimedia.de>
2016-05-09 17:08:07 +0200

    Add amount formatting class

---
+++ src/Presentation/AmountFormatter.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace WMDE\Fundraising\Frontend\Presentation;
+
+use WMDE\Fundraising\Frontend\Domain\Model\Euro;
+
+/**
+ * @license GNU GPL v2+
+ * @author Gabriel Birke < gabriel.birke@wikimedia.de >
+ */
+class AmountFormatter {
+
+	private $locale;
+
+	private $localeData = [
+		'de_DE' => [2, ',', '' ],
+		'en_US' => [2, '.', '' ]
+	];
+
+	public function __construct( string $locale ) {
+		$this->locale = $locale;
+	}
+
+	public function format( Euro $amount ) {
+		if ( empty( $this->localeData[ $this->locale ] ) ) {
+			throw new \RuntimeException( 'Unknown locale' );
+		}
+		return number_format(
+			$amount->getEuroFloat(),
+			$this->localeData[ $this->locale ][0],
+			$this->localeData[ $this->locale ][1],
+			$this->localeData[ $this->locale ][2]
+		);
+	}
+
+}
---
+++ tests/Unit/Presentation/AmountFormatterTest.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace WMDE\Fundraising\Frontend\Tests\Unit\Presentation;
+
+
+use WMDE\Fundraising\Frontend\Domain\Model\Euro;
+use WMDE\Fundraising\Frontend\Presentation\AmountFormatter;
+
+class AmountFormatterTest extends \PHPUnit_Framework_TestCase {
+
+	public function testGivenGermanLocaleAndFractionalAmount_amountIsFormattedAsGermanString() {
+		$formatter = new AmountFormatter( 'de_DE' );
+		$euro = Euro::newFromCents( 2342 );
+		$this->assertEquals( '23,42', $formatter->format( $euro ) );
+	}
+
+	public function testGivenGermanLocaleAndIntegerAmount_amountIsFormattedAsGermanString() {
+		$formatter = new AmountFormatter( 'de_DE' );
+		$euro = Euro::newFromInt( 23 );
+		$this->assertEquals( '23,00', $formatter->format( $euro ) );
+	}
+
+	public function testGivenUSLocaleAndFractionalAmount_amountIsFormattedAsUSString() {
+		$formatter = new AmountFormatter( 'en_US' );
+		$euro = Euro::newFromCents( 2342 );
+		$this->assertEquals( '23.42', $formatter->format( $euro ) );
+	}
+
+	public function testGivenUSLocaleAndIntegerAmount_amountIsFormattedAsUSString() {
+		$formatter = new AmountFormatter( 'en_US' );
+		$euro = Euro::newFromInt( 23 );
+		$this->assertEquals( '23.00', $formatter->format( $euro ) );
+	}
+}
