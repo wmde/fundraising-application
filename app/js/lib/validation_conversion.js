7d6064f95f6f8606bc9a207e45a60228de31c07c
Gabriel Birke <gabriel.birke@wikimedia.de>
2016-05-10 16:16:09 +0200

    Add initial state to store
    Allow for server-generated initial state for the store.
    Generate initial state from server-side validation messages.

---
+++ app/js/lib/validation_conversion.js
@@ -0,0 +1,33 @@
+var _ = require( 'lodash' ),
+
+	/**
+	 * Convert server-side validation messages to initial state for the store
+	 *
+	 * @param {Object} violatedFields
+	 * @returns {Object}
+	 */
+	createInitialStateFromViolatedFields = function ( violatedFields ) {
+		var state = {
+			validity: {},
+			validationMessages: {}
+		};
+		if ( _.isEmpty( violatedFields ) ) {
+			return {};
+		}
+
+		if ( violatedFields.betrag ) {
+			state.validity.amount = false;
+			state.validationMessages.amount = violatedFields.betrag;
+		}
+
+		if ( violatedFields.zahlweise ) {
+			state.validity.amount = false;
+			state.validationMessages.paymentType = violatedFields.zahlweise;
+		}
+
+		return state;
+	};
+
+module.exports = {
+	createInitialStateFromViolatedFields: createInitialStateFromViolatedFields
+};
