ce862e27a3bcc54b20799e842ebd3b3b7959e6b0
jeroendedauw <jeroendedauw@gmail.com>
2016-05-16 10:34:41 +0200

    Made use of logging repo decorators in FFFactory
    https://phabricator.wikimedia.org/T135094

--- src/Infrastructure/Repositories/LoggingCommentRepository.php
+++ src/Infrastructure/Repositories/LoggingCommentRepository.php
@@ -7,6 +7,8 @@ namespace WMDE\Fundraising\Frontend\Infrastructure\Repositories;
 use Psr\Log\LoggerInterface;
 use Psr\Log\LogLevel;
 use WMDE\Fundraising\Frontend\Domain\Model\Comment;
+use WMDE\Fundraising\Frontend\Domain\ReadModel\CommentWithAmount;
+use WMDE\Fundraising\Frontend\Domain\Repositories\CommentFinder;
 use WMDE\Fundraising\Frontend\Domain\Repositories\CommentRepository;
 use WMDE\Fundraising\Frontend\Domain\Repositories\StoreCommentException;
 
@@ -14,21 +16,25 @@ use WMDE\Fundraising\Frontend\Domain\Repositories\StoreCommentException;
  * @license GNU GPL v2+
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  */
-class LoggingCommentRepository implements CommentRepository {
+class LoggingCommentRepository implements CommentRepository, CommentFinder {
 
 	const CONTEXT_EXCEPTION_KEY = 'exception';
 
 	private $repository;
+	private $commentFinder;
 	private $logger;
 	private $logLevel;
 
-	public function __construct( CommentRepository $repository, LoggerInterface $logger ) {
+	public function __construct( CommentRepository $repository, CommentFinder $commentFinder, LoggerInterface $logger ) {
 		$this->repository = $repository;
+		$this->commentFinder = $commentFinder;
 		$this->logger = $logger;
 		$this->logLevel = LogLevel::CRITICAL;
 	}
 
 	/**
+	 * @see CommentRepository::storeComment
+	 *
 	 * @param Comment $comment
 	 * @throws StoreCommentException
 	 */
@@ -42,4 +48,21 @@ class LoggingCommentRepository implements CommentRepository {
 		}
 	}
 
+	/**
+	 * @see CommentFinder::getPublicComments
+	 *
+	 * @param int $limit
+	 *
+	 * @return CommentWithAmount[]
+	 * @throws \Exception
+	 */
+	public function getPublicComments( int $limit ): array {
+		try {
+			return $this->commentFinder->getPublicComments( $limit );
+		}
+		catch ( \Exception $ex ) { // TODO: use more specific exception type
+			$this->logger->log( $this->logLevel, $ex->getMessage(), [ self::CONTEXT_EXCEPTION_KEY => $ex ] );
+			throw $ex;
+		}
+	}
 }
