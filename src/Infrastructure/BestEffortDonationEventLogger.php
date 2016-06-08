aa58bde81068d5e7aa1a2dcf830ab4cbce2dcf37
Jeroen De Dauw <jeroendedauw@gmail.com>
2016-06-08 22:21:01 +0200

    Add missing variable and fixed bracket position

--- src/Infrastructure/BestEffortDonationEventLogger.php
+++ src/Infrastructure/BestEffortDonationEventLogger.php
@@ -1,4 +1,5 @@
 <?php
+
 declare( strict_types = 1 );
 
 namespace WMDE\Fundraising\Frontend\Infrastructure;
@@ -9,26 +10,24 @@ use Psr\Log\LoggerInterface;
  * @license GNU GPL v2+
  * @author Gabriel Birke < gabriel.birke@wikimedia.de >
  */
-class BestEffortDonationEventLogger implements DonationEventLogger
-{
+class BestEffortDonationEventLogger implements DonationEventLogger {
+
 	private $donationEventLogger;
 	private $logger;
 
-	public function __construct( DonationEventLogger $donationEventLogger, LoggerInterface $logger )
-	{
+	public function __construct( DonationEventLogger $donationEventLogger, LoggerInterface $logger ) {
 		$this->donationEventLogger = $donationEventLogger;
 		$this->logger = $logger;
 	}
 
-	public function log( int $donationId, string $message )
-	{
+	public function log( int $donationId, string $message ) {
 		try {
 			$this->donationEventLogger->log( $donationId, $message );
 		} catch ( DonationEventLogException $e ) {
 			$logContext = [
 				'donationId' => $donationId,
 				'exception' => $e,
-				'message'
+				'message' => $message
 			];
 			$this->logger->error( 'Could not update donation event log', $logContext );
 		}
