e6754fc83655bc82edc1f4de98ef6d9cdf2cf490
jeroendedauw <jeroendedauw@gmail.com>
2016-07-19 21:01:11 +0200

    Added profiling decorators
    Only for CommentFinder, PageRetriever and Guzzle Client so far.
    The later two are presumably the most important ones to profile.
    
    Note that the generic decorator building approach relies on PHPUnits
    mocking API, which internally builds a string and evals it. So this
    is presumably something we do not want to use in the production code
    when it is not initialized by the dev entry point. (This PR makes
    sure that does not happen.)
    
    Follow up things to do:
    
    * Decorate ALL the interesting things with profilers
    * Do something about the global access in the factory
    * Move the decorator building code elsewhere
    
    Can already be merged as far as I'm concerned though.

---
+++ src/Infrastructure/ProfilingDecoratorBuilder.php
@@ -0,0 +1,32 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace WMDE\Fundraising\Frontend\Infrastructure;
+
+use Symfony\Component\Stopwatch\Stopwatch;
+
+/**
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+class ProfilingDecoratorBuilder {
+
+	private $stopwatch;
+
+	public function __construct( Stopwatch $stopwatch ) {
+		$this->stopwatch = $stopwatch;
+	}
+
+	public function decorate( $objectToDecorate, string $profilingLabel ) {
+		return ( new DecoratorBuilder( $objectToDecorate ) )
+			->withBefore( function () use ( $profilingLabel ) {
+				$this->stopwatch->start( $profilingLabel );
+			} )
+			->withAfter( function () use ( $profilingLabel ) {
+				$this->stopwatch->stop( $profilingLabel );
+			} )
+			->newDecorator();
+	}
+
+}
