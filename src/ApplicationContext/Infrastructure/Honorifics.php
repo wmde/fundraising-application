ed39d4586ec896bd954baa76d553218af9be9363
jeroendedauw <jeroendedauw@gmail.com>
2016-08-03 15:52:04 +0200

    Moved Honorifics to Application folder

--- src/ApplicationContext/Infrastructure/Honorifics.php
+++ src/ApplicationContext/Infrastructure/Honorifics.php
@@ -2,7 +2,7 @@
 
 declare( strict_types = 1 );
 
-namespace WMDE\Fundraising\Frontend\Infrastructure;
+namespace WMDE\Fundraising\Frontend\ApplicationContext\Infrastructure;
 
 /**
  * Encapsulates a list of honorifics for the current locale.
