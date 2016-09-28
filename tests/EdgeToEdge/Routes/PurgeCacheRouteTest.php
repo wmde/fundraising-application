e5624568993af6ab7984f6865a235526a297c7e6
Kai_WMDE <kai.nissen@wikimedia.de>
2016-09-28 13:31:02 +0200

    fix class constant names

---
+++ tests/EdgeToEdge/Routes/PurgeCacheRouteTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace WMDE\Fundraising\Frontend\Tests\EdgeToEdge\Routes;
+
+use WMDE\Fundraising\Frontend\Tests\EdgeToEdge\WebRouteTestCase;
+
+/**
+ * @licence GNU GPL v2+
+ * @author Kai Nissen < kai.nissen@wikimedia.de >
+ */
+class PurgeCacheRouteTest extends WebRouteTestCase {
+
+	const INVALID_SECRET = 'pedo mellon a minno';
+
+	public function testGivenInvalidSecret_responseIsReturned() {
+		$client = $this->createClient();
+
+		$client->request(
+			'GET',
+			'/purge-cache',
+			[
+				'secret' => self::INVALID_SECRET
+			]
+		);
+
+		$this->assertSame( 200, $client->getResponse()->getStatusCode() );
+		$this->assertSame( 'ACCESS DENIED', $client->getResponse()->getContent() );
+	}
+
+}
