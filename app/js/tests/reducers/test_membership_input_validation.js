59ade2a97d6214a89d59bb97e95512e38be220b7
Kai_WMDE <kai.nissen@wikimedia.de>
2016-09-29 15:46:38 +0200

    reset field validity for emptied optional fields

---
+++ app/js/tests/reducers/test_membership_input_validation.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var test = require( 'tape' ),
+	deepFreeze = require( 'deep-freeze' ),
+	membershipInputValidation = require( '../../lib/reducers/membership_input_validation' );
+
+function newChangeContentAction( field, newValue ) {
+	return {
+		type: 'CHANGE_CONTENT',
+		payload: {
+			contentName: field,
+			value: newValue
+		}
+	};
+}
+
+test( 'If an optional field is emptied, its validity status is reset', function ( t ) {
+	var stateBefore = {
+			phoneNumber: { dataEntered: true, isValid: true }
+		},
+		expectedState = {
+			phoneNumber: { dataEntered: false, isValid: null }
+		};
+
+	deepFreeze( stateBefore );
+	t.deepEqual( membershipInputValidation( stateBefore, newChangeContentAction( 'phoneNumber', '' ) ), expectedState );
+	t.end();
+} );
+
+test( 'If a non-optional field is emptied, its validity status remains unchanged', function ( t ) {
+	var stateBefore = {
+			street: { dataEntered: true, isValid: true }
+		},
+		expectedState = {
+			street: { dataEntered: true, isValid: true }
+		};
+
+	deepFreeze( stateBefore );
+	t.deepEqual( membershipInputValidation( stateBefore, newChangeContentAction( 'street', '' ) ), expectedState );
+	t.end();
+} );
