946fdfe9f62996e4bcff1c143a04b64d94eb2dc0
Gabriel Birke <gabriel.birke@wikimedia.de>
2016-10-04 12:30:20 +0200

    Fix amount moderation
    Change amount policy to check the values for one-time and recurring
    donations and moderate donations that are above 1000 EUR (one-time or
    yearly).

--- tests/Unit/Validation/AmountPolicyValidatorTest.php
+++ tests/Unit/Validation/AmountPolicyValidatorTest.php
@@ -14,8 +14,29 @@ use WMDE\Fundraising\Frontend\Validation\AmountPolicyValidator;
  */
 class AmountPolicyValidatorTest extends \PHPUnit_Framework_TestCase {
 
-	public function testGivenAmountWithinLimits_validationSucceeds() {
+	const INTERVAL_ONCE = 0;
+	const INTERVAL_MONTHLY = 1;
+	const INTERVAL_QUARTERLY = 3;
+	const INTERVAL_SEMIANNUAL = 6;
+	const INTERVAL_YEARLY = 12;
+
+	/**
+	 * @dataProvider smallAmountProvider
+	 * @param float $amount
+	 * @param int $interval
+	 */
+	public function testGivenAmountWithinLimits_validationSucceeds( float $amount, int $interval ) {
 		$this->assertTrue( $this->newAmountValidator()->validate( 50, 0 )->isSuccessful() );
+	}
+
+	public function smallAmountProvider() {
+		return [
+			[ 750.0, self::INTERVAL_ONCE ],
+			[ 20.0, self::INTERVAL_MONTHLY ],
+			[ 100.5, self::INTERVAL_QUARTERLY ],
+			[ 499.98, self::INTERVAL_SEMIANNUAL ],
+			[ 999.99, self::INTERVAL_YEARLY ]
+		];
 	}
 
 	/**
@@ -23,20 +44,22 @@ class AmountPolicyValidatorTest extends \PHPUnit_Framework_TestCase {
 	 * @param float $amount
 	 * @param int $interval
 	 */
-	public function testGivenAmountTooHigh_validationFails( $amount, $interval ) {
+	public function testGivenAmountTooHigh_validationFails( float $amount, int $interval ) {
 		$this->assertFalse( $this->newAmountValidator()->validate( $amount, $interval )->isSuccessful() );
 	}
 
 	public function offLimitAmountProvider() {
 		return [
-			[ 1133, 0 ],
-			[ 250, 1 ],
-			[ 500, 12 ]
+			[ 1750.0, self::INTERVAL_ONCE ],
+			[ 101.0, self::INTERVAL_MONTHLY ],
+			[ 250.5, self::INTERVAL_QUARTERLY ],
+			[ 600, self::INTERVAL_SEMIANNUAL ],
+			[ 1337, self::INTERVAL_YEARLY ]
 		];
 	}
 
 	private function newAmountValidator() {
-		return new AmountPolicyValidator( 1000, 200, 300 );
+		return new AmountPolicyValidator( 1000, 1000 );
 	}
 
 }
