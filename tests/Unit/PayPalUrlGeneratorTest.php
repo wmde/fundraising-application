06a1776ef345167f5adbf7e68faa4c8c7ec911af
Kai_WMDE <kai.nissen@wikimedia.de>
2016-10-12 14:54:34 +0200

    refactor paypal url generator test

--- tests/Unit/PayPalUrlGeneratorTest.php
+++ tests/Unit/PayPalUrlGeneratorTest.php
@@ -32,27 +32,8 @@ class PayPalUrlGeneratorTest extends \PHPUnit_Framework_TestCase {
 	}
 
 	private function assertUrlValidForSubscriptions( $generatedUrl ) {
-		$this->assertContains( 'https://www.sandbox.paypal.com/cgi-bin/webscr', $generatedUrl );
-		$this->assertContains( 'cmd=_xclick-subscriptions', $generatedUrl );
-		$this->assertContains( 'no_shipping=1', $generatedUrl );
-		$this->assertContains( 'src=1', $generatedUrl );
-		$this->assertContains( 'sra=1', $generatedUrl );
-		$this->assertContains( 'srt=0', $generatedUrl );
-		$this->assertContains( 'a3=12.34', $generatedUrl );
-		$this->assertContains( 'p3=3', $generatedUrl );
-		$this->assertContains( 't3=M', $generatedUrl );
-		$this->assertContains( 'business=foerderpp%40wikimedia.de', $generatedUrl );
-		$this->assertContains( 'currency_code=EUR', $generatedUrl );
-		$this->assertContains( 'lc=de', $generatedUrl );
-		$this->assertContains( 'item_name=Mentioning+that+awesome+organization+on+the+invoice', $generatedUrl );
-		$this->assertContains( 'item_number=1234', $generatedUrl );
-		$this->assertContains( 'notify_url=http%3A%2F%2Fmy.donation.app%2Fhandler%2Fpaypal%2F', $generatedUrl );
-		$this->assertContains( 'cancel_return=http%3A%2F%2Fmy.donation.app%2Fdonation%2Fcancel%2F', $generatedUrl );
-		$this->assertContains(
-			'return=http%3A%2F%2Fmy.donation.app%2Fdonation%2Fconfirm%2F%3Fid%3D1234%26accessToken%3Datoken',
-			$generatedUrl
-		);
-		$this->assertContains( 'custom=%7B%22sid%22%3A1234%2C%22utoken%22%3A%22utoken%22%7D', $generatedUrl );
+		$this->assertCommonParamsSet( $generatedUrl );
+		$this->assertSubscriptionRelatedParamsSet( $generatedUrl );
 	}
 
 	public function testSinglePayments() {
@@ -64,21 +45,8 @@ class PayPalUrlGeneratorTest extends \PHPUnit_Framework_TestCase {
 	}
 
 	private function assertUrlValidForSinglePayments( $generatedUrl ) {
-		$this->assertContains( 'https://www.sandbox.paypal.com/cgi-bin/webscr', $generatedUrl );
-		$this->assertContains( 'cmd=_donations', $generatedUrl );
-		$this->assertContains( 'amount=12.34', $generatedUrl );
-		$this->assertContains( 'business=foerderpp%40wikimedia.de', $generatedUrl );
-		$this->assertContains( 'currency_code=EUR', $generatedUrl );
-		$this->assertContains( 'lc=de', $generatedUrl );
-		$this->assertContains( 'item_name=Mentioning+that+awesome+organization+on+the+invoice', $generatedUrl );
-		$this->assertContains( 'item_number=1234', $generatedUrl );
-		$this->assertContains( 'notify_url=http%3A%2F%2Fmy.donation.app%2Fhandler%2Fpaypal%2F', $generatedUrl );
-		$this->assertContains( 'cancel_return=http%3A%2F%2Fmy.donation.app%2Fdonation%2Fcancel%2F', $generatedUrl );
-		$this->assertContains(
-			'return=http%3A%2F%2Fmy.donation.app%2Fdonation%2Fconfirm%2F%3Fid%3D1234%26accessToken%3Datoken',
-			$generatedUrl
-		);
-		$this->assertContains( 'custom=%7B%22sid%22%3A1234%2C%22utoken%22%3A%22utoken%22%7D', $generatedUrl );
+		$this->assertCommonParamsSet( $generatedUrl );
+		$this->assertSinglePaymentRelatedParamsSet( $generatedUrl );
 	}
 
 	private function newPayPalUrlConfig(): PayPalUrlConfig {
@@ -117,18 +85,25 @@ class PayPalUrlGeneratorTest extends \PHPUnit_Framework_TestCase {
 	}
 
 	private function assertUrlValidForDelayedSubscriptions( $generatedUrl ) {
+		$this->assertCommonParamsSet( $generatedUrl );
+		$this->assertSubscriptionRelatedParamsSet( $generatedUrl );
+		$this->assertTrialPeriodRelatedParametersSet( $generatedUrl );
+	}
+
+	private function newPayPalUrlConfigWithDelayedPayment(): PayPalUrlConfig {
+		return PayPalUrlConfig::newFromConfig( [
+			'base-url' => self::BASE_URL,
+			'account-address' => self::ACCOUNT_ADDRESS,
+			'notify-url' => self::NOTIFY_URL,
+			'return-url' => self::RETURN_URL,
+			'cancel-url' => self::CANCEL_URL,
+			'item-name' => self::ITEM_NAME,
+			'delay-in-days' => 90
+		] );
+	}
+
+	private function assertCommonParamsSet( $generatedUrl ) {
 		$this->assertContains( 'https://www.sandbox.paypal.com/cgi-bin/webscr', $generatedUrl );
-		$this->assertContains( 'cmd=_xclick-subscriptions', $generatedUrl );
-		$this->assertContains( 'no_shipping=1', $generatedUrl );
-		$this->assertContains( 'src=1', $generatedUrl );
-		$this->assertContains( 'sra=1', $generatedUrl );
-		$this->assertContains( 'srt=0', $generatedUrl );
-		$this->assertContains( 'a1=0', $generatedUrl );
-		$this->assertContains( 'p1=90', $generatedUrl );
-		$this->assertContains( 't1=D', $generatedUrl );
-		$this->assertContains( 'a3=12.34', $generatedUrl );
-		$this->assertContains( 'p3=3', $generatedUrl );
-		$this->assertContains( 't3=M', $generatedUrl );
 		$this->assertContains( 'business=foerderpp%40wikimedia.de', $generatedUrl );
 		$this->assertContains( 'currency_code=EUR', $generatedUrl );
 		$this->assertContains( 'lc=de', $generatedUrl );
@@ -143,16 +118,26 @@ class PayPalUrlGeneratorTest extends \PHPUnit_Framework_TestCase {
 		$this->assertContains( 'custom=%7B%22sid%22%3A1234%2C%22utoken%22%3A%22utoken%22%7D', $generatedUrl );
 	}
 
-	private function newPayPalUrlConfigWithDelayedPayment(): PayPalUrlConfig {
-		return PayPalUrlConfig::newFromConfig( [
-			'base-url' => self::BASE_URL,
-			'account-address' => self::ACCOUNT_ADDRESS,
-			'notify-url' => self::NOTIFY_URL,
-			'return-url' => self::RETURN_URL,
-			'cancel-url' => self::CANCEL_URL,
-			'item-name' => self::ITEM_NAME,
-			'delay-in-days' => 90
-		] );
+	private function assertSinglePaymentRelatedParamsSet( string $generatedUrl ) {
+		$this->assertContains( 'cmd=_donations', $generatedUrl );
+		$this->assertContains( 'amount=12.34', $generatedUrl );
+	}
+
+	private function assertTrialPeriodRelatedParametersSet( string $generatedUrl ) {
+		$this->assertContains( 'a1=0', $generatedUrl );
+		$this->assertContains( 'p1=90', $generatedUrl );
+		$this->assertContains( 't1=D', $generatedUrl );
+	}
+
+	private function assertSubscriptionRelatedParamsSet( string $generatedUrl ) {
+		$this->assertContains( 'cmd=_xclick-subscriptions', $generatedUrl );
+		$this->assertContains( 'no_shipping=1', $generatedUrl );
+		$this->assertContains( 'src=1', $generatedUrl );
+		$this->assertContains( 'sra=1', $generatedUrl );
+		$this->assertContains( 'srt=0', $generatedUrl );
+		$this->assertContains( 'a3=12.34', $generatedUrl );
+		$this->assertContains( 'p3=3', $generatedUrl );
+		$this->assertContains( 't3=M', $generatedUrl );
 	}
 
 }
