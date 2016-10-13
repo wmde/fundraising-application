b3805101d14f9c6c535211594533b9af8ddb63f2
Kai_WMDE <kai.nissen@wikimedia.de>
2016-10-13 14:07:15 +0200

    consider both amount selection and amount input

---
+++ tests/EdgeToEdge/Routes/NewDonationRouteTest.php
@@ -0,0 +1,67 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace WMDE\Fundraising\Frontend\Tests\EdgeToEdge\Routes;
+
+use WMDE\Fundraising\Frontend\Tests\EdgeToEdge\WebRouteTestCase;
+
+/**
+ * @licence GNU GPL v2+
+ * @author Kai Nissen < kai.nissen@wikimedia.de >
+ */
+class NewDonationRouteTest extends WebRouteTestCase {
+
+	/** @dataProvider paymentInputProvider */
+	public function testGivenPaymentInput_paymentDataIsInitiallyValidated( $validPaymentInput, $expectedValidity ) {
+		$client = $this->createClient();
+		$client->request(
+			'POST',
+			'/donation/new',
+			$validPaymentInput
+		);
+
+		$this->assertContains(
+			'Payment data: ' . $expectedValidity,
+			$client->getResponse()->getContent()
+		);
+	}
+
+	public function paymentInputProvider() {
+		return [
+			[
+				[
+					'betrag_auswahl' => '100',
+					'zahlweise' => 'BEZ',
+					'periode' => '0'
+				],
+				'valid'
+			],
+			[
+				[
+					'amountGiven' => '123,45',
+					'zahlweise' => 'PPL',
+					'periode' => 6
+				],
+				'valid'
+			],
+			[
+				[
+					'betrag_auswahl' => '0',
+					'zahlweise' => 'PPL',
+					'periode' => 6
+				],
+				'invalid'
+			],
+			[
+				[
+					'betrag_auswahl' => '100',
+					'zahlweise' => 'BTC',
+					'periode' => 6
+				],
+				'invalid'
+			]
+		];
+	}
+
+}
