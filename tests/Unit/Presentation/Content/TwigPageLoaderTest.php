329bad617710dddb5a1b599c5aee306dfea1b544
jeroendedauw <jeroendedauw@gmail.com>
2016-10-18 13:42:45 +0200

    Switched to PageRetriever component
    Depends on https://github.com/wmde/PageRetriever/pull/3

--- tests/Unit/Presentation/Content/TwigPageLoaderTest.php
+++ tests/Unit/Presentation/Content/TwigPageLoaderTest.php
@@ -4,13 +4,17 @@ declare( strict_types = 1 );
 
 namespace WMDE\Fundraising\Frontend\Tests\Unit\Presentation\Content;
 
-use WMDE\Fundraising\Frontend\Infrastructure\PageRetriever;
 use WMDE\Fundraising\Frontend\Presentation\Content\TwigPageLoader;
+use WMDE\PageRetriever\PageRetriever;
 
 class TwigPageLoaderTest extends \PHPUnit_Framework_TestCase {
 
 	public function testGivenAPageText_getSourceReturnsPageText() {
-		$loader = new TwigPageLoader( $this->newPageRetrieverThatWillReturn( 'template text' ) );
+		$loader = new TwigPageLoader(
+			$this->createMock( PageRetriever::class ),
+			$this->newPageRetrieverThatWillReturn( 'template text' )
+		);
+
 		$this->assertSame( 'template text', $loader->getSource( 'Felis silvestris' ) );
 	}
 
@@ -25,12 +29,20 @@ class TwigPageLoaderTest extends \PHPUnit_Framework_TestCase {
 	}
 
 	public function testGivenAPageText_getCacheKeyReturnsPageName() {
-		$loader = new TwigPageLoader( $this->newPageRetrieverThatWillReturn( 'template text' ) );
+		$loader = new TwigPageLoader(
+			$this->createMock( PageRetriever::class ),
+			$this->newPageRetrieverThatWillReturn( 'template text' )
+		);
+
 		$this->assertSame( 'Felis silvestris', $loader->getCacheKey( 'Felis silvestris' ) );
 	}
 
 	public function testGivenAPageText_isFreshKeyReturnsTrue() {
-		$loader = new TwigPageLoader( $this->newPageRetrieverThatWillReturn( 'template text' ) );
+		$loader = new TwigPageLoader(
+			$this->createMock( PageRetriever::class ),
+			$this->newPageRetrieverThatWillReturn( 'template text' )
+		);
+
 		$this->assertTrue( $loader->isFresh( 'Felis silvestris', 0 ) );
 	}
 
@@ -38,7 +50,11 @@ class TwigPageLoaderTest extends \PHPUnit_Framework_TestCase {
 	 * @expectedException \Twig_Error_Loader
 	 */
 	public function testGivenMissingPage_getSourceThrowsException() {
-		$loader = new TwigPageLoader( $this->newPageRetrieverThatWillReturn( '' ) );
+		$loader = new TwigPageLoader(
+			$this->createMock( PageRetriever::class ),
+			$this->newPageRetrieverThatWillReturn( '' )
+		);
+
 		$loader->getSource( 'Felis silvestris' );
 	}
 
@@ -46,35 +62,41 @@ class TwigPageLoaderTest extends \PHPUnit_Framework_TestCase {
 	 * @expectedException \Twig_Error_Loader
 	 */
 	public function testGivenMissingPage_getCacheKeyThrowsException() {
-		$loader = new TwigPageLoader( $this->newPageRetrieverThatWillReturn( '' ) );
+		$loader = new TwigPageLoader(
+			$this->createMock( PageRetriever::class ),
+			$this->newPageRetrieverThatWillReturn( '' )
+		);
+
 		$loader->getCacheKey( 'Felis silvestris' );
 	}
 
 	public function testGivenMissingPage_isFreshKeyStaysTrue() {
-		$loader = new TwigPageLoader( $this->newPageRetrieverThatWillReturn( 'template text' ) );
+		$loader = new TwigPageLoader(
+			$this->createMock( PageRetriever::class ),
+			$this->newPageRetrieverThatWillReturn( 'template text' )
+		);
+
 		$this->assertTrue( $loader->isFresh( 'Felis silvestris', 0 ) );
 	}
 
 	public function testPageTitleConfiguredAsRawContent_pageRetrieverFetchesInRawMode() {
-		$pageRetriever = $this->newPageRetrieverThatWillReturn( 'template text' );
+		$renderedRetriever = $this->newPageRetrieverThatWillReturn( 'rendered' );
+		$renderedRetriever->expects( $this->never() )->method( $this->anything() );
 
-		$pageRetriever->expects( $this->once() )
-			->method( 'fetchPage' )
-			->with( 'FetchMeInRawMode', 'raw' );
+		$rawRetriever = $this->newPageRetrieverThatWillReturn( 'raw' );
 
-		$loader = new TwigPageLoader( $pageRetriever, [ 'FetchMeInRawMode' ] );
-		$loader->getSource( 'FetchMeInRawMode' );
+		$loader = new TwigPageLoader( $rawRetriever, $renderedRetriever, [ 'FetchMeInRawMode' ] );
+		$this->assertSame( 'raw', $loader->getSource( 'FetchMeInRawMode' ) );
 	}
 
 	public function testPageTitleNotConfiguredAsRawContent_pageRetrieverFetchesInRenderMode() {
-		$pageRetriever = $this->newPageRetrieverThatWillReturn( 'template text' );
+		$renderedRetriever = $this->newPageRetrieverThatWillReturn( 'rendered' );
 
-		$pageRetriever->expects( $this->once() )
-			->method( 'fetchPage' )
-			->with( 'FetchMeInRenderMode', 'render' );
+		$rawRetriever = $this->newPageRetrieverThatWillReturn( 'raw' );
+		$rawRetriever->expects( $this->never() )->method( $this->anything() );
 
-		$loader = new TwigPageLoader( $pageRetriever, [ 'FetchMeInRawMode' ] );
-		$loader->getSource( 'FetchMeInRenderMode' );
+		$loader = new TwigPageLoader( $rawRetriever, $renderedRetriever, [ 'FetchMeInRawMode' ] );
+		$this->assertSame( 'rendered', $loader->getSource( 'FetchMeInRenderMode' ) );
 	}
 
 }
