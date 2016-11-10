81bfb67ce37c8ea7075c07d8865823c8ce084471
jeroendedauw <jeroendedauw@gmail.com>
2016-11-11 00:51:46 +0100

    Use PSR Log Test Doubles

--- tests/Fixtures/LoggerSpy.php
+++ tests/Fixtures/LoggerSpy.php
@@ -4,59 +4,22 @@ declare( strict_types = 1 );
 
 namespace WMDE\Fundraising\Frontend\Tests\Fixtures;
 
-use Psr\Log\AbstractLogger;
-
 /**
  * @licence GNU GPL v2+
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  */
-class LoggerSpy extends AbstractLogger {
-
-	const LEVEL_INDEX = 0;
-	const MESSAGE_INDEX = 1;
-	const CONTEXT_INDEX = 2;
-
-	private $logCalls = [];
-
-	public function log( $level, $message, array $context = [] ) {
-		$this->logCalls[] = [ $level, $message, $context ];
-	}
-
-	public function getLogCalls(): array {
-		return $this->logCalls;
-	}
-
-	public function assertNoCalls() {
-		if ( !empty( $this->logCalls ) ) {
-			throw new \RuntimeException(
-				'Logger calls where made while non where expected: ' . var_export( $this->logCalls, true )
-			);
-		}
-	}
+class LoggerSpy extends \WMDE\PsrLogTestDoubles\LoggerSpy {
 
 	public function assertCalledOnceWithMessage( string $expectedMessage, \PHPUnit_Framework_TestCase $testCase ) {
 		$testCase->assertEquals(
 			[ $expectedMessage ],
 			array_map(
 				function( array $logCall ) {
-					return $logCall[self::MESSAGE_INDEX];
+					return $logCall['message'];
 				},
-				$this->logCalls
+				$this->getLogCalls()
 			),
 			'Should be called once with expected message'
-		);
-	}
-
-	public function assertCalledWithMessage( string $expectedMessage, \PHPUnit_Framework_TestCase $testCase ) {
-		$testCase->assertContains(
-			$expectedMessage,
-			array_map(
-				function( array $logCall ) {
-					return $logCall[self::MESSAGE_INDEX];
-				},
-				$this->logCalls
-			),
-			'Should be called with expected message'
 		);
 	}
 
