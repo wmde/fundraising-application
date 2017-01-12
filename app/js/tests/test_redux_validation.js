74e044548586d42985c64b482b7f76bc3192637e
Gabriel Birke <gabriel.birke@wikimedia.de>
2017-01-12 17:19:34 +0100

    Refactor ValidationDispatcher test
    Split a test that tested 3 things into 3 test methods

--- app/js/tests/test_redux_validation.js
+++ app/js/tests/test_redux_validation.js
@@ -29,10 +29,33 @@ test( 'ValidationDispatcher calls validationFunction and dispatches action', fun
 	t.end();
 } );
 
+test( 'ValidationDispatcher calls validationFunction and dispatches action every time the data changes', function ( t ) {
+	var successResult = { status: 'OK' },
+		dummyAction = { type: 'TEST_VALIDATION' },
+		initialData = {},
+		validationFunction = sinon.stub().returns( successResult ),
+		actionCreationFunction = sinon.stub().returns( dummyAction ),
+		dispatcher = reduxValidation.createValidationDispatcher(
+			validationFunction,
+			actionCreationFunction,
+			[ 'importantField' ],
+			initialData
+		),
+		testStore = { dispatch: sinon.spy() };
+
+	dispatcher.dispatchIfChanged( { importantField: 'just some data' }, testStore );
+	dispatcher.dispatchIfChanged( { importantField: 'data changed again' }, testStore );
+
+	t.ok( validationFunction.calledTwice, 'validation function is called for every change' );
+	t.ok( testStore.dispatch.calledTwice, 'action is dispatched for every change' );
+
+	t.end();
+} );
+
 test( 'ValidationDispatcher calls validationFunction and dispatches action only if data changes', function ( t ) {
 	var successResult = { status: 'OK' },
 		dummyAction = { type: 'TEST_VALIDATION' },
-		testData = { importantField: 'just some data', ignoredData: 'this won\'t be validated' },
+		testData = { importantField: 'just some data' },
 		initialData = {},
 		validationFunction = sinon.stub().returns( successResult ),
 		actionCreationFunction = sinon.stub().returns( dummyAction ),
@@ -50,18 +73,31 @@ test( 'ValidationDispatcher calls validationFunction and dispatches action only 
 	t.ok( validationFunction.calledOnce, 'validation function is called once' );
 	t.ok( testStore.dispatch.calledOnce, 'action is dispatched once' );
 
+	t.end();
+} );
+
+test( 'ValidationDispatcher does nothing if ignored data changes', function ( t ) {
+	var successResult = { status: 'OK' },
+		dummyAction = { type: 'TEST_VALIDATION' },
+		testData = { importantField: 'just some data', ignoredData: 'this won\'t be validated' },
+		initialData = {},
+		validationFunction = sinon.stub().returns( successResult ),
+		actionCreationFunction = sinon.stub().returns( dummyAction ),
+		dispatcher = reduxValidation.createValidationDispatcher(
+			validationFunction,
+			actionCreationFunction,
+			[ 'importantField' ],
+			initialData
+		),
+		testStore = { dispatch: sinon.spy() };
+
+	dispatcher.dispatchIfChanged( testData, testStore );
+
 	testData.ignoredData = 'data changed, but in ignored field';
 	dispatcher.dispatchIfChanged( testData, testStore );
 
 	t.ok( validationFunction.calledOnce, 'validation function is not called when ignored field change' );
 	t.ok( testStore.dispatch.calledOnce, 'action is is not dispatched when ignored field change' );
-
-	testData.importantField = 'data changed';
-	dispatcher.dispatchIfChanged( testData, testStore );
-
-	t.ok( validationFunction.calledTwice, 'validation function is called once' );
-	t.ok( actionCreationFunction.calledTwice, 'new action is created' );
-	t.ok( testStore.dispatch.calledTwice, 'action is dispatched again' );
 
 	t.end();
 } );
