983c92b4b0fde7077b44f09eae262f5811bc5b83
Gabriel Birke <gabriel.birke@wikimedia.de>
2017-01-16 15:14:04 +0100

    Add node.js install to vagrant

--- build/vagrant/install_packages.sh
+++ build/vagrant/install_packages.sh
@@ -1,9 +1,13 @@
 #!/bin/sh -x
 
-sudo add-apt-repository ppa:ondrej/php -y
+# Add PHP repo
+add-apt-repository ppa:ondrej/php -y
+
+# Add node.js repo
+curl -sL https://deb.nodesource.com/setup_6.x | bash -
 
 apt-get update
-apt-get upgrade
+apt-get upgrade -y
 
 # Avoid MySQL password prompt
 #debconf-set-selections <<< 'mysql-server mysql-server/root_password password PASSWORD_HERE'
@@ -11,3 +15,4 @@ apt-get upgrade
 
 apt-get install -y mysql-client unzip build-essential # mysql-server
 apt-get install -y php7.1 php7.1-dev php7.1-intl php7.1-sqlite3 php7.1-curl php7.1-xml php7.1-mysql
+apt-get install -y nodejs
