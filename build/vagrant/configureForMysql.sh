b30a86f0423ea6a9668a87b2384d21c6f7f702f4
Gabriel Birke <gabriel.birke@wikimedia.de>
2017-01-17 15:08:12 +0100

    Improve MySQL provisioning
    Make config file paths absolute

--- build/vagrant/configureForMysql.sh
+++ build/vagrant/configureForMysql.sh
@@ -4,6 +4,6 @@ mysql -pPASSWORD_HERE -u root -e 'CREATE DATABASE fundraising;'
 mysql -pPASSWORD_HERE -u root -e "CREATE USER 'fundraising'@'localhost' IDENTIFIED BY 'INSECURE PASSWORD';"
 mysql -pPASSWORD_HERE -u root -e "GRANT ALL ON fundraising.* TO 'fundraising'@'localhost';"
 
-cp build/vagrant/config.prod.json app/config/config.prod.json
+cp /vagrant/build/vagrant/config.prod.json /vagrant/app/config/config.prod.json
 
 /vagrant/vendor/bin/doctrine orm:schema-tool:create
