49363b10ef6a8f671c610fbc72e1bccc391b1120
Jeroen De Dauw <jeroendedauw@gmail.com>
2017-03-16 13:11:21 +0100

    Only create MySQL stuff if not exists
    Same as in https://github.com/wmde/fundraising-backend/pull/247

--- build/vagrant/configureForMysql.sh
+++ build/vagrant/configureForMysql.sh
@@ -4,8 +4,8 @@ if [[ -z "$DB_PASSWD" ]]; then
     DB_PASSWD="INSECURE PASSWORD"
 fi
 
-mysql -pPASSWORD_HERE -u root -e 'CREATE DATABASE fundraising;'
-mysql -pPASSWORD_HERE -u root -e "CREATE USER 'fundraising'@'localhost' IDENTIFIED BY '$DB_PASSWD';"
+mysql -pPASSWORD_HERE -u root -e 'CREATE DATABASE IF NOT EXISTS fundraising;'
+mysql -pPASSWORD_HERE -u root -e "CREATE USER IF NOT EXISTS 'fundraising'@'localhost' IDENTIFIED BY '$DB_PASSWD';"
 mysql -pPASSWORD_HERE -u root -e "GRANT ALL ON fundraising.* TO 'fundraising'@'localhost';"
 
 sed -i -e "s/__DB_PASSWORD__/$DB_PASSWD/" /vagrant/app/config/config.prod.json
