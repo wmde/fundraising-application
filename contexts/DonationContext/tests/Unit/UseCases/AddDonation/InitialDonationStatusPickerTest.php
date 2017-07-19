9755243b6efe9ce23c33b7b08a2cfaea7f8985a1
Pablo Grass <pablo.grass@wikimedia.de>
2017-07-19 11:15:05 +0200

    fixed a new namespace inconsistency in context test

--- contexts/DonationContext/tests/Unit/UseCases/AddDonation/InitialDonationStatusPickerTest.php
+++ contexts/DonationContext/tests/Unit/UseCases/AddDonation/InitialDonationStatusPickerTest.php
@@ -2,7 +2,7 @@
 
 declare( strict_types = 1 );
 
-namespace WMDE\Fundraising\Frontend\Tests\Unit\DonationContext\UseCases\AddDonation;
+namespace WMDE\Fundraising\Frontend\DonationContext\Tests\Unit\UseCases\AddDonation;
 
 use PHPUnit\Framework\TestCase;
 use WMDE\Fundraising\Frontend\DonationContext\UseCases\AddDonation\InitialDonationStatusPicker;
