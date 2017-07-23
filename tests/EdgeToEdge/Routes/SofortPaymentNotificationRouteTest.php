0a109727d4bd09d40a8db9bf8819e824cf333c4d
Jeroen De Dauw <jeroendedauw@gmail.com>
2017-07-23 05:09:45 +0200

    Extracted assertions in SofortPaymentNotificationRouteTest

--- tests/EdgeToEdge/Routes/SofortPaymentNotificationRouteTest.php
+++ tests/EdgeToEdge/Routes/SofortPaymentNotificationRouteTest.php
@@ -9,7 +9,6 @@ use Psr\Log\LogLevel;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Client;
-use WMDE\Fundraising\Frontend\DonationContext\Domain\Model\Donation;
 use WMDE\Fundraising\Frontend\DonationContext\Tests\Data\ValidDonation;
 use WMDE\Fundraising\Frontend\Factories\FunFunFactory;
 use WMDE\Fundraising\Frontend\Tests\EdgeToEdge\WebRouteTestCase;
@@ -138,24 +137,33 @@ class SofortPaymentNotificationRouteTest extends WebRouteTestCase {
 			);
 
 			$this->assertIsBadRequestResponse( $client->getResponse() );
-
-			$this->assertSame(
-				[ 'Duplicate notification' ],
-				$logger->getLogCalls()->getMessages()
-			);
-
-			$this->assertSame(
-				self::VALID_TRANSACTION_ID,
-				$logger->getLogCalls()->getFirstCall()->getContext()['post_vars']['transaction']
-			);
-
-			$this->assertEquals(
-				$donation->getId(),
-				$logger->getLogCalls()->getFirstCall()->getContext()['query_vars']['id']
-			);
-
-			$this->assertSame( LogLevel::INFO, $logger->getLogCalls()->getFirstCall()->getLevel() );
+			$this->assertErrorCauseIsLogged( $logger, 'Duplicate notification' );
+			$this->assertRequestVarsAreLogged( $logger );
+			$this->assertLogLevel( $logger, LogLevel::INFO );
 		} );
+	}
+
+	private function assertErrorCauseIsLogged( LoggerSpy $logger, string $expectedMessage ): void {
+		$this->assertSame(
+			[ $expectedMessage ],
+			$logger->getLogCalls()->getMessages()
+		);
+	}
+
+	private function assertRequestVarsAreLogged( LoggerSpy $logger ): void {
+		$this->assertSame(
+			self::VALID_TRANSACTION_ID,
+			$logger->getLogCalls()->getFirstCall()->getContext()['post_vars']['transaction']
+		);
+
+		$this->assertEquals(
+			self::VALID_TOKEN,
+			$logger->getLogCalls()->getFirstCall()->getContext()['query_vars']['updateToken']
+		);
+	}
+
+	private function assertLogLevel( LoggerSpy $logger, string $expectedLevel ): void {
+		$this->assertSame( $expectedLevel, $logger->getLogCalls()->getFirstCall()->getLevel() );
 	}
 
 	public function testGivenUnknownDonation_requestDataIsLogged(): void {
@@ -176,23 +184,9 @@ class SofortPaymentNotificationRouteTest extends WebRouteTestCase {
 			);
 
 			$this->assertIsErrorResponse( $client->getResponse() );
-
-			$this->assertSame(
-				[ 'Donation not found' ],
-				$logger->getLogCalls()->getMessages()
-			);
-
-			$this->assertSame(
-				self::VALID_TRANSACTION_ID,
-				$logger->getLogCalls()->getFirstCall()->getContext()['post_vars']['transaction']
-			);
-
-			$this->assertEquals(
-				$donation->getId() + 1,
-				$logger->getLogCalls()->getFirstCall()->getContext()['query_vars']['id']
-			);
-
-			$this->assertSame( LogLevel::ERROR, $logger->getLogCalls()->getFirstCall()->getLevel() );
+			$this->assertErrorCauseIsLogged( $logger, 'Donation not found' );
+			$this->assertRequestVarsAreLogged( $logger );
+			$this->assertLogLevel( $logger, LogLevel::ERROR );
 		} );
 	}
 
