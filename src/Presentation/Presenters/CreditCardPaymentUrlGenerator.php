922cb924ed4ef81926c63fc343aaf8942f2f0d27
Jeroen De Dauw <jeroendedauw@gmail.com>
2017-08-07 19:00:20 +0200

    Make CC payment redirect based instead of iframe
    For https://phabricator.wikimedia.org/T170171
    
    This should probably be manually tested by someone that has the
    system level integration with the CC thing set up before merging.

---
+++ src/Presentation/Presenters/CreditCardPaymentUrlGenerator.php
@@ -0,0 +1,41 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace WMDE\Fundraising\Frontend\Presentation\Presenters;
+
+use Symfony\Component\Translation\TranslatorInterface;
+use WMDE\Fundraising\Frontend\DonationContext\UseCases\AddDonation\AddDonationResponse;
+use WMDE\Fundraising\Frontend\PaymentContext\Domain\PaymentUrlGenerator\CreditCard;
+
+/**
+ * Render the credit card payment page embedding an iframe
+ *
+ * @licence GNU GPL v2+
+ */
+class CreditCardPaymentUrlGenerator {
+
+	private $translator;
+	private $urlGenerator;
+
+	public function __construct( TranslatorInterface $translator, CreditCard $urlGenerator ) {
+		$this->translator = $translator;
+		$this->urlGenerator = $urlGenerator;
+	}
+
+	public function buildUrl( AddDonationResponse $response ): string {
+		$personalInfo = $response->getDonation()->getDonor();
+
+		return $this->urlGenerator->generateUrl(
+			$personalInfo ? $personalInfo->getName()->getFirstName() : '',
+			$personalInfo ? $personalInfo->getName()->getLastName() : '',
+			$this->translator->trans( 'paytext_cc', [], 'paymentIntervals' ) . ' ' .
+			$this->translator->trans( $response->getDonation()->getPaymentIntervalInMonths(), [], 'paymentIntervals' ),
+			$response->getDonation()->getId(),
+			$response->getAccessToken(),
+			$response->getUpdateToken(),
+			$response->getDonation()->getAmount()
+		);
+	}
+
+}
