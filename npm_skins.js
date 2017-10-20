7cfb389c5e46c6c00de93f05c78011c4eb347eb7
Pablo Grass <pablo.grass@wikimedia.de>
2017-10-20 16:50:15 +0200

    build and copy assets from all skins

---
+++ npm_skins.js
@@ -0,0 +1,21 @@
+/**
+ * Invoke an npm command in all skin folders
+ */
+
+const fs = require( 'fs' ),
+	resolve = require( 'path' ).resolve,
+	join = require( 'path' ).join,
+	cp = require( 'child_process' ),
+	skins = resolve( __dirname, './skins' ),
+	commands = process.argv.slice( 2 ); // this script is invoked via `node npm_skins ...`, so lose the first two arguments
+
+fs.readdirSync( skins ).forEach( function ( skin ) {
+	var skinPath = join( skins, skin );
+
+	// ensure path has package.json
+	if ( !fs.existsSync( join( skinPath, 'package.json' ) ) ) {
+		return;
+	}
+
+	cp.spawn( 'npm', commands, { env: process.env, cwd: skinPath, stdio: 'inherit' } );
+} );
