0f1649f17136341b7340bc41eec0442b2e0a4063
Pablo Grass <pablo.grass@wikimedia.de>
2017-11-20 18:45:23 +0100

    add basic test for SuboptionDisplayHandler

---
+++ skins/cat17/src/app/tests/view_handlers/test_display_field_suboptions.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var test = require( 'tape' ),
+	sinon = require( 'sinon' ),
+	SuboptionDisplayHandler = require( '../../lib/view_handler/display_field_suboptions' )
+;
+
+test( 'Current field is selected and infotext opened, others unselected and not opened', function ( t ) {
+	var allWrappers = {
+			removeClass: sinon.spy()
+		},
+		allInfoTexts = {
+			removeClass: sinon.spy()
+		},
+		activeInfoText = {
+			addClass: sinon.spy(),
+			prop: sinon.stub().withArgs( 'scrollHeight' ).returns( 472 )
+		},
+		activeWrapper = {
+			find: sinon.stub().withArgs( '.info-text' ).returns( activeInfoText ),
+			addClass: sinon.spy()
+		},
+		activeField = {
+			parents: sinon.stub().withArgs( '.wrap-field' ).returns( activeWrapper )
+		},
+		fieldset = {
+			find: sinon.stub(),
+			css: sinon.spy()
+		}
+	;
+
+	fieldset.find.withArgs( '.wrap-field' ).returns( allWrappers );
+	fieldset.find.withArgs( '.info-text' ).returns( allInfoTexts );
+	fieldset.find.withArgs( '.wrap-input [value="PPL"]:not(.hidden)' ).returns( activeField );
+
+	var handler = SuboptionDisplayHandler.createSuboptionDisplayHandler( fieldset );
+	handler.update( 'PPL' );
+
+	t.ok( allWrappers.removeClass.withArgs( 'selected notselected' ).calledOnce, 'all wappers unstyled' );
+	t.ok( allInfoTexts.removeClass.withArgs( 'opened' ).calledOnce, 'all infotexts closed' );
+
+	t.ok( activeWrapper.addClass.withArgs( 'selected' ).calledOnce, 'active wrapper styled' );
+	t.ok( activeInfoText.addClass.withArgs( 'opened' ).calledOnce, 'active infotext opened' );
+	t.ok( fieldset.css.withArgs( 'min-height', '472px' ).calledOnce, 'height of fieldset adjusted per active infotext' );
+
+	t.end();
+} );
