fed5672eb7cf1a7df137801d79852d5e77713bcd
Pablo Grass <pablo.grass@wikimedia.de>
2017-11-22 11:06:38 +0100

    avoid using jquery, some tests and todo

--- skins/cat17/src/app/tests/test_numeric_input_handler.js
+++ skins/cat17/src/app/tests/test_numeric_input_handler.js
@@ -2,7 +2,7 @@
 
 var test = require( 'tape' ),
 	sinon = require( 'sinon' ),
-	inputHandler = require( '../lib/numeric_input_handler' )
+	NumericInputHandler = require( '../lib/numeric_input_handler' )
 ;
 
 test( 'Attaching adds listener on keypress', function ( t ) {
@@ -11,8 +11,41 @@ test( 'Attaching adds listener on keypress', function ( t ) {
 		}
 	;
 
-	inputHandler.attach( element );
+	NumericInputHandler.attach( element );
 
 	t.ok( element.on.withArgs( 'keypress' ).calledOnce );
+	t.end();
+} );
+
+test( 'Handler lets number presses pass', function ( t ) {
+	var numberKeys = {
+			0: 48,
+			1: 49,
+			2: 50,
+			3: 51,
+			4: 52,
+			5: 53,
+			6: 54,
+			7: 55,
+			8: 56,
+			9: 57
+		},
+		event, keyCode, number
+	;
+
+	for (number in numberKeys) {
+		keyCode = numberKeys[ number ];
+		event = {
+			preventDefault: sinon.stub(),
+			keyCode: keyCode,
+			ctrlKey: false
+		};
+
+		NumericInputHandler.Handler.handle( event );
+
+		t.ok( event.preventDefault.notCalled, 'not preventing default behavior' );
+		t.equal( event.keyCode, keyCode, 'keycode unmodified' );
+	}
+
 	t.end();
 } );
