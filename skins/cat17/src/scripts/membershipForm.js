43f771c89a8990743d717dce083d34021b5c3d6c
Pablo Grass <pablo.grass@wikimedia.de>
2017-12-01 20:13:26 +0100

    rm redundant code. amount typed is guarded by NumericInputHandler via FormComponents

--- skins/cat17/src/scripts/membershipForm.js
+++ skins/cat17/src/scripts/membershipForm.js
@@ -482,29 +482,6 @@ $( function () {
     return false;
   });
 
-  $("#amount-typed").on('keypress', function (event) {
-    var _element = $(this),
-      keyCode = event.keyCode || event.which,
-      keysAllowed = [44, 46, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 0, 8, 9, 13];
-
-    if ($.inArray(keyCode, keysAllowed) === -1 && event.ctrlKey === false) {
-      event.preventDefault();
-    }
-
-    if ((keyCode == 44 || keyCode == 46) && $('#amount-typed').val().indexOf('.') > 0) {
-      event.preventDefault();
-    }
-
-    if (keyCode == 44) {
-      setTimeout(
-        function () {
-          $('#amount-typed').val(
-            $('#amount-typed').val().replace(',','.')
-          );
-        }, 10);
-    }
-  });
-
   // Set initial form values
     // TODO use IntegerCurrency to parse amount
   store.dispatch( actions.newInitializeContentAction( initData.data( 'initial-form-values' ) ) );
