50cb4e1029c142fc203038c224e907f8dfd02d64
Pablo Grass <pablo.grass@wikimedia.de>
2017-12-05 16:45:48 +0100

    test donation's amount_and_frequency_are_valid

--- skins/cat17/src/app/lib/state_aggregation/donation/amount_and_frequency_are_valid.js
+++ skins/cat17/src/app/lib/state_aggregation/donation/amount_and_frequency_are_valid.js
@@ -6,10 +6,9 @@ var _ = require( 'underscore' ),
 
 module.exports = function ( state ) {
 	var result = _.clone( validationResult );
-	// todo Why is amount.isValid always bogous?
-	result.dataEntered = state.donationFormContent.paymentIntervalInMonths >= 0 || state.donationFormContent.amount != 0;
+	result.dataEntered = state.donationFormContent.paymentIntervalInMonths >= 0 || state.donationFormContent.amount !== 0;
 
-	if ( state.donationFormContent.paymentIntervalInMonths >= 0 && state.donationFormContent.amount != 0 ) {
+	if ( state.donationFormContent.paymentIntervalInMonths >= 0 && state.donationFormContent.amount !== 0 && state.donationInputValidation.amount.isValid ) {
 		result.isValid = true;
 	} else if ( state.donationInputValidation.amount.isValid === null ) {
 		result.isValid = null;
