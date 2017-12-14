e9362ff0ebe3c0f4fdc2048ee197f043561327c1
Jeroen De Dauw <jeroendedauw@gmail.com>
2017-12-14 03:14:35 +0100

    Split JS into more functions

--- skins/cat17/src/scripts/donationCommentForm.js
+++ skins/cat17/src/scripts/donationCommentForm.js
@@ -1,32 +1,31 @@
 (function ($) {
 
-	$(document).ready(function () {
-		commentForm();
-	});
+	function updateElementValidationState() {
+		if ($(this).val() === "" || !this.checkValidity()) {
+			$(this).addClass('invalid');
+			$(this).parent().addClass('invalid');
+		}
+		else {
+			$(this).removeClass('invalid');
+			$(this).parent().removeClass('invalid');
+			$(this).addClass('valid');
+			$(this).parent().addClass('valid');
+		}
+	}
 
-	// TODO move into view handlers
-	var submitValidation = function () {
+	function submitValidation() {
 		var isValid = true;
 		$('form').find('input, textarea').each(function () {
+			updateElementValidationState.apply( this );
+
 			if ($(this).val() === "" || !this.checkValidity()) {
-				$(this).addClass('invalid');
-				$(this).parent().addClass('invalid');
 				isValid = false;
-			}
-			else {
-				$(this).removeClass('invalid');
-				$(this).parent().removeClass('invalid');
-				$(this).addClass('valid');
-				$(this).parent().addClass('valid');
 			}
 		});
 		return isValid;
-	};
+	}
 
-	var commentForm = function () {
-		var form = $('#comment-form');
-		if (form.length === 0) return;
-
+	function setupFormValidation( form ) {
 		var submitButton = form.find('input[type="submit"]');
 		form.submit(submitValidation);
 		submitButton.click(submitValidation);
@@ -36,50 +35,50 @@
 		});
 
 		form.find('input, textarea').blur(function () {
-			if (!$(this).data('data-entered')) return;
-
-			if ($(this).val() === "" || !this.checkValidity()) {
-				$(this).addClass('invalid');
-				$(this).parent().addClass('invalid');
-			}
-			else {
-				$(this).removeClass('invalid');
-				$(this).parent().removeClass('invalid');
-				$(this).addClass('valid');
-				$(this).parent().addClass('valid');
+			if ($(this).data('data-entered')) {
+				updateElementValidationState.apply( this );
 			}
 		});
+	}
 
-		form.bind( 'submit', function ( event ) {
-			event.preventDefault();
+	function handleFormSubmission( event ) {
+		event.preventDefault();
 
-			$.ajax( '../add-comment', {
-				data: $( this ).serialize(),
-				dataType: 'json',
-				type: 'POST',
-				success: function( response ) {
-					// TODO: fix feedback display
-					var $feedback = $( '#comment-form' );
-					$feedback.find( '.message' ).remove();
-					$feedback.append(
-						$( '<div />' )
-							.addClass( 'message' )
-							.addClass( response.status === 'ERR' ? 'error' : 'success' )
-							.text( response.message || 'Vielen Dank! Die Nachricht wurde verschickt!' )
-					);
-				},
-				error: function ( e ){
-					var $feedback = $( '#comment-form' );
-					$feedback.find( '.message' ).remove();
-					$feedback.append(
-						$( '<div />' )
-							.addClass( 'message' )
-							.addClass( 'error' )
-							.text( 'Die Nachricht konnte auf Grund eines Fehlers nicht verschickt werden.' )
-					);
-				}
-			});
+		$.ajax( '../add-comment', {
+			data: $( this ).serialize(),
+			dataType: 'json',
+			type: 'POST',
+			success: function( response ) {
+				// TODO: fix feedback display
+				var $feedback = $( '#comment-form' );
+				$feedback.find( '.message' ).remove();
+				$feedback.append(
+					$( '<div />' )
+						.addClass( 'message' )
+						.addClass( response.status === 'ERR' ? 'error' : 'success' )
+						.text( response.message || 'Vielen Dank! Die Nachricht wurde verschickt!' )
+				);
+			},
+			error: function ( e ){
+				var $feedback = $( '#comment-form' );
+				$feedback.find( '.message' ).remove();
+				$feedback.append(
+					$( '<div />' )
+						.addClass( 'message' )
+						.addClass( 'error' )
+						.text( 'Die Nachricht konnte auf Grund eines Fehlers nicht verschickt werden.' )
+				);
+			}
 		});
-	};
+	}
+
+	$(document).ready(function () {
+		var form = $('#comment-form');
+		if (form.length === 0) return;
+
+		setupFormValidation( form );
+
+		form.bind( 'submit', handleFormSubmission );
+	});
 
 })(jQuery);
