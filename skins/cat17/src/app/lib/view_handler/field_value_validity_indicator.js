535da7c1cc8c2d06f5e4585a60325b78d469818f
Gabriel Birke <gabriel.birke@wikimedia.de>
2017-12-19 14:04:13 +0100

    Simplify FieldValidityIndicator
    Adding an icon is now done with CSS instead of JavaScript, so we can
    lose the JS code and test code that manipulates it.

--- skins/cat17/src/app/lib/view_handler/field_value_validity_indicator.js
+++ skins/cat17/src/app/lib/view_handler/field_value_validity_indicator.js
@@ -11,16 +11,13 @@ var objectAssign = require( 'object-assign' ),
 
 		update: function ( validationState ) {
 			if ( validationState.isValid === true ) {
-				this.element.addClass( 'valid' ).removeClass( 'invalid' )
-					.next( 'span' ).addClass( 'icon-ok' ).removeClass( 'icon-placeholder' );
+				this.element.addClass( 'valid' ).removeClass( 'invalid' );
 				this.element.parent().addClass( 'valid' ).removeClass( 'invalid' );
 			} else if( validationState.isValid === false ) {
-				this.element.addClass( 'invalid' ).removeClass( 'valid' )
-					.next( 'span' ).removeClass( 'icon-ok icon-placeholder' );
+				this.element.addClass( 'invalid' ).removeClass( 'valid' );
 				this.element.parent().addClass( 'invalid' ).removeClass( 'valid' );
 			} else if( validationState.isValid === null ) {
-				this.element.removeClass( 'valid invalid' )
-					.next( 'span' ).addClass( 'icon-placeholder' ).removeClass( 'icon-ok' );
+				this.element.removeClass( 'valid invalid' );
 				this.element.parent().removeClass( 'invalid valid' );
 			}
 		}
--- skins/cat17/src/app/tests/view_handlers/test_field_value_validity_indicator.js
+++ skins/cat17/src/app/tests/view_handlers/test_field_value_validity_indicator.js
@@ -5,10 +5,9 @@ var test = require( 'tape-catch' ),
 	fieldValueValidityIndicator = require( '../../lib/view_handler/field_value_validity_indicator' ),
 	createValidityIndicator = fieldValueValidityIndicator.createFieldValueValidityIndicator;
 
-function ElementStub( sibling, parent ) {
+function ElementStub( parent ) {
 	this.addClassSpy = sinon.spy();
 	this.removeClassSpy = sinon.spy();
-	this.nextElement = sibling;
 	this.parentElement = parent;
 }
 
@@ -23,19 +22,14 @@ ElementStub.prototype = {
 		return this;
 	},
 
-	next: function () {
-		return this.nextElement;
-	},
-
 	parent: function () {
 		return this.parentElement;
 	}
 };
 
 test( 'When validation state has initial status, nothing is indicated', function ( t ) {
-	var indicatorElement = new ElementStub(),
-		parentElement = new ElementStub(),
-		inputElement = new ElementStub( indicatorElement, parentElement ),
+	var parentElement = new ElementStub(),
+		inputElement = new ElementStub( parentElement ),
 		validationState = { dataEntered: false, isValid: null },
 		handler = createValidityIndicator( inputElement );
 
@@ -43,16 +37,14 @@ test( 'When validation state has initial status, nothing is indicated', function
 
 	t.ok( inputElement.addClassSpy.notCalled, 'validity indication keeps initial status' );
 	t.ok( inputElement.removeClassSpy.withArgs( 'valid invalid' ), 'validity indication is set to neutral' );
-	t.ok( indicatorElement.addClassSpy.withArgs( 'icon-placeholder' ), 'indicator is set to neutral placeholder' );
-	t.ok( indicatorElement.removeClassSpy.withArgs( 'icon-ok icon-bug' ), 'icons are hidden' );
 	t.ok( parentElement.removeClassSpy.withArgs( 'invalid valid' ).calledOnce, 'parent validity indication is set to neutral' );
 	t.end();
 } );
 
 test( 'When validation state has valid status, it is indicated', function ( t ) {
-	var indicatorElement = new ElementStub(),
-		parentElement = new ElementStub(),
-		inputElement = new ElementStub( indicatorElement, parentElement ),
+	var parentElement = new ElementStub(),
+		inputElement = new ElementStub(
+			parentElement ),
 		validationState = { dataEntered: true, isValid: true },
 		handler = createValidityIndicator( inputElement );
 
@@ -60,17 +52,14 @@ test( 'When validation state has valid status, it is indicated', function ( t ) 
 
 	t.ok( inputElement.addClassSpy.withArgs( 'valid' ).calledOnce, 'input field set to valid' );
 	t.ok( inputElement.removeClassSpy.withArgs( 'invalid' ).calledOnce, 'input field set to not invalid' );
-	t.ok( indicatorElement.addClassSpy.withArgs( 'icon-ok' ).calledOnce, 'indicator set to valid' );
-	t.ok( indicatorElement.removeClassSpy.withArgs( 'icon-bug icon-placeholder' ).calledOnce, 'indicator set to not invalid' );
 	t.ok( parentElement.removeClassSpy.withArgs( 'invalid' ).calledOnce, 'parent invalidity indication removed' );
 	t.ok( parentElement.addClassSpy.withArgs( 'valid' ).calledOnce, 'parent validity indication set' );
 	t.end();
 } );
 
 test( 'When validation state has invalid status, it is indicated', function ( t ) {
-	var indicatorElement = new ElementStub(),
-		parentElement = new ElementStub(),
-		inputElement = new ElementStub( indicatorElement, parentElement ),
+	var parentElement = new ElementStub(),
+		inputElement = new ElementStub( parentElement ),
 		validationState = { dataEntered: true, isValid: false },
 		handler = createValidityIndicator( inputElement );
 
@@ -78,8 +67,6 @@ test( 'When validation state has invalid status, it is indicated', function ( t 
 
 	t.ok( inputElement.addClassSpy.withArgs( 'invalid' ).calledOnce, 'input field set to invalid' );
 	t.ok( inputElement.removeClassSpy.withArgs( 'valid' ).calledOnce, 'input field set to not valid' );
-	t.ok( indicatorElement.addClassSpy.withArgs( 'icon-bug' ).calledOnce, 'indicator set to invalid' );
-	t.ok( indicatorElement.removeClassSpy.withArgs( 'icon-ok icon-placeholder' ).calledOnce, 'indicator set to not valid' );
 	t.ok( parentElement.removeClassSpy.withArgs( 'valid' ).calledOnce, 'parent validity indication removed' );
 	t.ok( parentElement.addClassSpy.withArgs( 'invalid' ).calledOnce, 'parent invalidity indication set' );
 	t.end();
