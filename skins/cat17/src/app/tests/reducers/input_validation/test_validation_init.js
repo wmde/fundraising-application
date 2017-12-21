f2c356568270a9c4ff019dc3231e78c89342a55a
Gabriel Birke <gabriel.birke@wikimedia.de>
2017-12-21 16:28:59 +0100

    Finish validation initialization
    Handle case when initial value is given.
    Check if unrelated values in initial state are ignored.

--- skins/cat17/src/app/tests/reducers/input_validation/test_validation_init.js
+++ skins/cat17/src/app/tests/reducers/input_validation/test_validation_init.js
@@ -4,12 +4,11 @@ var test = require( 'tape-catch' ),
 	deepFreeze = require( 'deep-freeze' ),
 	inputValidation = require( '../../../lib/reducers/input_validation' ).inputValidation;
 
-function newInitializeValidationAction( violatedFields, initialValidationResult, initialValues ) {
+function newInitializeValidationAction( violatedFields, initialValues ) {
 	return {
 		type: 'INITIALIZE_VALIDATION',
 		payload: {
 			violatedFields: violatedFields,
-			initialValidationResult: initialValidationResult,
 			initialValues: initialValues
 		}
 	}
@@ -27,7 +26,51 @@ test( 'Given violated fields, their validation state should change', function ( 
 
 	deepFreeze( stateBefore );
 	t.deepEqual(
-		inputValidation( stateBefore, newInitializeValidationAction( { email: 'Not valid' }, {}, {} ) ),
+		inputValidation( stateBefore, newInitializeValidationAction( { email: 'Not valid' }, {} ) ),
+		expectedState
+	);
+	t.end();
+} );
+
+test( 'Given initial values, their state should change, based on violated fields', function ( t ) {
+	var stateBefore = {
+			firstName: { dataEntered: false, isValid: null },
+			lastName: { dataEntered: false, isValid: null },
+			email: { dataEntered: false, isValid: null }
+		},
+		expectedState = {
+			firstName: { dataEntered: false, isValid: null },
+			lastName: { dataEntered: true, isValid: true },
+			email: { dataEntered: true, isValid: false }
+		};
+
+	deepFreeze( stateBefore );
+	t.deepEqual(
+		inputValidation( stateBefore, newInitializeValidationAction(
+			{ email: 'Wrong animal provider' },
+			{ lastName: 'McGoatface', email: 'maaah@yougoatmail.com' }
+			) ),
+		expectedState
+	);
+	t.end();
+} );
+
+test( 'Unknown initial values are ignored', function ( t ) {
+	var stateBefore = {
+			firstName: { dataEntered: false, isValid: null },
+			email: { dataEntered: false, isValid: null }
+		},
+		expectedState = {
+			firstName: { dataEntered: false, isValid: null },
+			email: { dataEntered: false, isValid: null }
+		};
+
+	deepFreeze( stateBefore );
+	t.deepEqual(
+		inputValidation( stateBefore, newInitializeValidationAction(
+			{ unrelated: "Not enough sprockets"},
+			{ unrelated: '1', superfluous: "Floo" }
+			) ),
 		expectedState
 	);
 	t.end();
