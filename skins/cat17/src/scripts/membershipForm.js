84007239930032f3f35f4f5d91c0a86184843a51
Gabriel Birke <gabriel.birke@wikimedia.de>
2017-12-22 13:46:52 +0100

    Fix wrong field name

--- skins/cat17/src/scripts/membershipForm.js
+++ skins/cat17/src/scripts/membershipForm.js
@@ -354,8 +354,8 @@ $( function () {
 	store.dispatch( actions.newInitializeContentAction( initSetup ) );
 
 	// Set initial validation state
-	if ( initSetup.value === 0 ) {
-		delete initSetup.value;
+	if ( initSetup.amount === 0 ) {
+		delete initSetup.amount;
 	}
 	store.dispatch( actions.newInitializeValidationStateAction(
 		initData.data( 'violatedFields' ),
