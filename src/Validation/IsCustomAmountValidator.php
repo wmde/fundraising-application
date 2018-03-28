222cbc656295b789668747e33527d1566dbc45f1
tzhelyazkova <tonina.zhelyazkova@wikimedia.de>
2018-03-28 13:38:58 +0200

    Fix linting errors

--- src/Validation/IsCustomAmountValidator.php
+++ src/Validation/IsCustomAmountValidator.php
@@ -1,24 +1,24 @@
 <?php
 
+declare( strict_types = 1 );
+
 namespace WMDE\Fundraising\Frontend\Validation;
 
 use WMDE\Euro\Euro;
 
-class IsCustomAmountValidator
-{
+class IsCustomAmountValidator {
 	private $validAmounts;
 	/**
 	 * IsCustomAmountValidator constructor.
 	 * @param int[] $validAmounts
 	 */
-	public function __construct( array $validAmounts )
-	{
+	public function __construct( array $validAmounts ) {
 		$this->validAmounts = $validAmounts;
 	}
 
 	public function validate( Euro $amount ):bool {
 		if ( $amount->getEuroCents() === 0 ) {
-			return false;
+			return false ;
 		}
 		return !in_array( $amount->getEuroCents(), $this->validAmounts );
 	}
