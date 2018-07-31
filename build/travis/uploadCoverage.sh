599acb82d62eba5665ccdf9e47c536002987a902
Tim Eulitz <tim.eulitz@wikimedia.de>
2018-07-31 15:42:57 +0200

    Adapt Scrutinizer settings
    T198705

--- build/travis/uploadCoverage.sh
+++ build/travis/uploadCoverage.sh
@@ -2,6 +2,5 @@
 
 set -x
 
-vendor/bin/phpunit --coverage-clover coverage.clover
 wget https://scrutinizer-ci.com/ocular.phar
 php ocular.phar code-coverage:upload --format=php-clover coverage.clover
