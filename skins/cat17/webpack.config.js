ee27b0a44975cdfdb92994ba84aa26c5fe813827
Gabriel Birke <gabriel.birke@gmail.com>
2018-10-05 23:26:53 +0200

    Moudularize webpack config
    Use development and production config, sharing a common base.
    As introduced at
    https://survivejs.com/webpack/developing/composing-configuration/
    
    Add sourcemap configuration.
    
    Add TODO list to configuration to check what needs to be done until Gulp
    can browserify can be replaced.

--- skins/cat17/webpack.config.js
+++ skins/cat17/webpack.config.js
@@ -1,11 +1,47 @@
 const path = require( 'path' );
+const merge = require( 'webpack-merge' )
 
-module.exports = {
-	entry: {
-		'scripts/wmde': './src/app/main.js'
-	},
-	output: {
-		filename: '[name].js',
-		path: path.resolve( __dirname, 'web' )
+// TODO
+// - Generate styles
+// - Concatenate script assets
+// - Copy fonts, pdfs etc
+// - use environment configurations (dev, prod)
+// - minify images
+// - build faq (+ vue)
+// - replace npm build scripts
+// - delete gulpfile & browserify
+// - Add dev server & integrate HMR into rest of application
+
+const commonConfig = merge( [
+	{
+		entry: {
+			'scripts/wmde': './src/app/main.js'
+		},
+		output: {
+			filename: '[name].js',
+			path: path.resolve( __dirname, 'web' )
+		}
 	}
+] );
+
+const productionConfig = merge( [
+	{
+		devtool: 'source-map'
+	}
+] );
+
+const developmentConfig = merge( [
+	{
+		devtool: 'eval'
+	}
+] );
+
+module.exports = mode => {
+	mode = mode || 'development';
+
+	if (mode === "production") {
+		return merge( commonConfig, productionConfig, { mode } );
+	}
+
+	return merge( commonConfig, developmentConfig, { mode } );
 };
