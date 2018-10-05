f13f4a7ed78bfa4fd3591df00ae8e65ff5a2a5aa
Gabriel Birke <gabriel.birke@wikimedia.de>
2018-10-05 19:14:21 +0200

    Use separate entry point for Vue
    After many failed attempts to export a Vue component inside
    src/app/main.js and use it with skins/cat17/src/scripts/donationForm.js,
    we decided to create a separate entry point and output file.
    
    This is a first working demo of a Vue component that's integrated into
    the Redux store. It's unfortunate that it has to use a gloabl variable
    for the store, but we could not make any other solution work.

--- skins/cat17/src/app/components/BankData.vue
+++ skins/cat17/src/app/components/BankData.vue
@@ -1,14 +1,16 @@
 <template>
-	<div id="bank-data">
+	<div id="bank-data" @click="justAnExample">
 		<span>Hey</span>
+		<!-- TODO: Use IBAN and stuff -->
+		<span>{{ formContent.firstName }}</span>
 	</div>
 </template>
 
 <script>
 	export default {
 		name: 'bank-data',
-		mounted: function( ) {
-			console.log('I was mounted!!!!!');
-		}
+		props: ['formContent', 'justAnExample'] // TODO use different props and use defaults for the right store cross-section
+		// add methods if needed (e.g. for passing values from input fields), see https://www.npmjs.com/package/redux-vue
 	}
+
 </script>
