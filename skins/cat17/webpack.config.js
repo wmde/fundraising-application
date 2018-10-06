08a8c4d21909316bb4757d6b44f185e737399bb1
Gabriel Birke <gabriel.birke@gmail.com>
2018-10-06 12:07:13 +0200

    Add SASS support to webpack
    Add Sass/CSS and font loaders
    
    CSS is output as Javascript though, the next step is to use an
    extraction plugin.

--- skins/cat17/webpack.config.js
+++ skins/cat17/webpack.config.js
@@ -15,11 +15,54 @@ const merge = require( 'webpack-merge' )
 const commonConfig = merge( [
 	{
 		entry: {
-			'scripts/wmde': './src/app/main.js'
+			'scripts/wmde': './src/app/main.js',
+			'styles/main': './src/sass/main.scss'
 		},
 		output: {
 			filename: '[name].js',
 			path: path.resolve( __dirname, 'web' )
+		},
+		module: {
+			rules: [
+				{
+					test: /\.scss$/,
+					use: [
+						{ loader: "style-loader" },
+						{ loader:  "css-loader" }, // translates CSS into CommonJS
+						{
+							// Loader for webpack to process CSS with PostCSS
+							loader: 'postcss-loader',
+							options: {
+								plugins: function () {
+									return [
+										require('autoprefixer')
+									];
+								}
+							}
+						},
+						{
+							loader: "sass-loader",
+							options: {
+								outputStyle: 'nested', // libsass doesn't support expanded yet
+								precision: 10,
+								includePaths: [ './node_modules' ],
+								sourceMap: true,
+								sourceMapContents: false
+							}
+						} // compiles Sass to CSS, using Node Sass by default
+					]
+				},
+				{
+					test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+					use: [{
+						loader: 'file-loader',
+						options: {
+							name: '[name].[ext]',
+							outputPath: 'assets/fonts/'
+						}
+					}]
+				}
+			]
 		}
 	}
 ] );
