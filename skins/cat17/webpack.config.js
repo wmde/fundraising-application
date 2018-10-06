545be11f24caae4f6b10182b3743b7addb25cd08
Gabriel Birke <gabriel.birke@gmail.com>
2018-10-06 23:27:41 +0200

    Build css from npm
    Remove CSS parsing from webpack, put it into npm tasks
    Run webpack and css task in parallel

--- skins/cat17/webpack.config.js
+++ skins/cat17/webpack.config.js
@@ -1,17 +1,13 @@
 const path = require( 'path' );
 const glob = require( 'glob' );
-const { getIfUtils, removeEmpty } = require( 'webpack-config-utils' );
-const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
+const { getIfUtils } = require( 'webpack-config-utils' );
 const CleanWebpackPlugin = require( 'clean-webpack-plugin' );
 const ConcatPlugin = require( 'webpack-concat-plugin' );
 const CopyWebpackPlugin = require( 'copy-webpack-plugin' );
 const ImageminPlugin = require( 'imagemin-webpack-plugin' ).default;
 
 // TODO
-// - Copy fonts, pdfs etc
-// - minify images (and make font regex matcher more specific to put the logo SVGs into images folder)
 // - build faq (+ vue, with extra CSS file)
-// - replace npm build scripts
 // - delete gulpfile & browserify
 // - Add dev server & integrate HMR into rest of application
 
@@ -23,8 +19,7 @@ module.exports = mode => {
 	return 	{
 		mode: ifProduction( 'production', 'development' ),
 		entry: {
-			'scripts/wmde': './src/app/main.js',
-			'css/main': './src/sass/main.scss'
+			'scripts/wmde': './src/app/main.js'
 		},
 		output: {
 			filename: '[name].js',
@@ -32,10 +27,10 @@ module.exports = mode => {
 			library: 'WMDE'
 		},
 		plugins: [
-			new CleanWebpackPlugin( OUTPUT_PATH, { exclude:  ['.gitkeep'], verbose: true } ),
-			new MiniCssExtractPlugin({
-				filename: "[name].css"
-			}),
+			new CleanWebpackPlugin( OUTPUT_PATH, {
+				exclude:  [ '.gitkeep', 'css' ],
+				verbose: true
+			} ),
 			new ConcatPlugin( {
 				uglify: true,
 				sourceMap: true,
@@ -53,8 +48,9 @@ module.exports = mode => {
 				}
 			} ),
 			new CopyWebpackPlugin( [
-				{ from: 'src/scripts/*.js', to: 'scripts/', flatten: true  } // TODO transform with uglify
-			] ),
+				{ from: 'src/scripts/*.js', to: 'scripts/', flatten: true  }, // TODO transform with uglify
+				{ from: 'assets/fonts/**/*', context: 'src'  }
+			], { debug: false } ),
 			new ImageminPlugin( {
 				externalImages: {
 					//context: 'src',
@@ -67,59 +63,6 @@ module.exports = mode => {
 				}
 			} )
 		],
-		module: {
-			rules: [
-				{
-					test: /\.scss$/,
-					use: [
-						{
-							// Extract CSS modules into files
-							loader: MiniCssExtractPlugin.loader,
-							options: {
-								publicPath: '../'
-							}
-						},
-						{ loader:  "css-loader" }, // translates CSS into CommonJS
-						{
-							// Loader for webpack to process CSS with PostCSS
-							loader: 'postcss-loader',
-							options: {
-								plugins: function () {
-									return removeEmpty( [
-										require( 'autoprefixer' ),
-										ifProduction(
-											require( 'postcss-clean' )( {
-												compatibility: 'ie10'
-											} )
-										)
-									] );
-								}
-							}
-						},
-						{
-							loader: "sass-loader",
-							options: {
-								outputStyle: 'nested', // libsass doesn't support expanded yet
-								precision: 10,
-								includePaths: [ './node_modules' ],
-								sourceMap: true,
-								sourceMapContents: false
-							}
-						} // compiles Sass to CSS, using Node Sass by default
-					]
-				},
-				{
-					test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-					use: [{
-						loader: 'file-loader',
-						options: {
-							name: '[name].[ext]',
-							outputPath: 'assets/fonts/'
-						}
-					}]
-				}
-			]
-		},
 		devtool: ifProduction( 'source-map', 'eval' )
 	}
 };
