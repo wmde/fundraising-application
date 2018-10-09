fc3c314f57cb86d8cb4e978d81ecbf29c433c483
Tim Eulitz <tim.eulitz@wikimedia.de>
2018-10-09 11:15:13 +0200

    Get Webpack to work in combination with Vue

--- skins/cat17/webpack.config.js
+++ skins/cat17/webpack.config.js
@@ -4,6 +4,7 @@ const { getIfUtils } = require( 'webpack-config-utils' );
 const ConcatPlugin = require( 'webpack-concat-plugin' );
 const CopyWebpackPlugin = require( 'copy-webpack-plugin' );
 const ImageminPlugin = require( 'imagemin-webpack-plugin' ).default;
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 // TODO
 // - build faq (+ vue, with extra CSS file)
@@ -56,8 +57,17 @@ module.exports = mode => {
 					gifsicle: { interlaced: true },
 					svgo: { plugins: [ { removeUnknownsAndDefaults: false }, { cleanupIDs: false } ] }
 				}
-			} )
+			} ),
+			new VueLoaderPlugin()
 		],
+		module: {
+			rules: [
+				{
+					test: /\.vue$/,
+					loader: 'vue-loader'
+				}
+			]
+		},
 		devtool: ifProduction( 'source-map', 'eval' ),
 		stats: {
 			all: false,
@@ -66,6 +76,11 @@ module.exports = mode => {
 			errors: true,
 			warnings: true,
 			warningsFilter: [/font/]
+		},
+		resolve: {
+			alias: {
+				'vue$': 'vue/dist/vue.esm.js'
+			}
 		}
 	}
 };
