363ddd109dda53e0396cc5093de688efb9f80b1e
Gabriel Birke <gabriel.birke@wikimedia.de>
2018-10-10 10:38:26 +0200

    Move FAQ data initialization to entry point

--- skins/cat17/faq-page/src/main.js
+++ skins/cat17/faq-page/src/main.js
@@ -1,10 +1,21 @@
 // The following line loads the standalone build of Vue instead of the runtime-only build,
 // so you don't have to do: import Vue from 'vue/dist/vue'
 // This is done with the browser options. For the config, see package.json
-var Vue = require( 'vue' ),
-	Faq = require( './Faq.vue' );
 
-new Vue( { // eslint-disable-line no-new
+var Vue = require( 'vue' );
+var Faq = require( './Faq.vue' );
+
+var faqDataElement = document.getElementById( 'faq' );
+
+new Vue({ // eslint-disable-line no-new
 	el: '#faq',
-	render: ( h ) => h( Faq )
+	render: ( h ) => h(
+		Faq,
+	    {
+			props: {
+				messages: JSON.parse( faqDataElement.getAttribute( 'data-messages' ) ),
+				content: JSON.parse( faqDataElement.getAttribute( 'data-content' ) )
+			}
+		}
+	)
 } );
