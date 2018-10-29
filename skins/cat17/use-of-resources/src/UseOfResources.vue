9f2d4d243af2e8cccd3de52725dcfcdbdfbd11e8
tzhelyazkova <tonina.zhelyazkova@wikimedia.de>
2018-10-29 18:45:45 +0100

    Set Vue infrastructure

---
+++ skins/cat17/use-of-resources/src/UseOfResources.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="app">
+        <h2>{{ message }}</h2>
+    </div>
+</template>
+
+<script>
+
+export default {
+	name: 'app',
+	data() {
+		return {
+			message: 'New & shiny Application Of Funds page'
+		}
+	}
+}
+</script>
