135d2f8c58262607f9694e282c3499b18f008cba
tzhelyazkova <tonina.zhelyazkova@wikimedia.de>
2019-01-08 18:16:50 +0100

    Structure address form layout

--- skins/cat17/src/app/components/AddressForm.vue
+++
@@ -1,4 +0,0 @@
-<template>
-	<p>TODO</p>
-</template>
-
---
+++ skins/cat17/src/pages/AddressForm.vue
@@ -0,0 +1,21 @@
+<template>
+	<name></name>
+	<postal></postal>
+	<email></email>
+</template>
+
+<script>
+	import Name from '../app/components/Name.vue';
+	import Email from '../app/components/Email.vue';
+	import Postal from '../app/components/Postal.vue';
+
+	export default {
+		name: 'addressForm',
+		components: {
+			Name,
+			Email,
+			Postal
+		},
+		props: [ 'addressType' ],
+	};
+</script>
