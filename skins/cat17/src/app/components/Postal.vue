bb8e18fe112dad9e61c4fc175206c2c74aeb20ee
tzhelyazkova <tonina.zhelyazkova@wikimedia.de>
2019-01-10 10:45:06 +0100

    Adjust code to typescript requirements

--- skins/cat17/src/app/components/Postal.vue
+++ skins/cat17/src/app/components/Postal.vue
@@ -1,9 +1,32 @@
 <template>
-
+	<div class="field-grp field-street">
+		<input type="text" id="street" placeholder="{$ 'street_label' | trans $}" data-pattern="^.+$">
+		<label for="street" class="sr-only">{$ 'street_label' | trans $}</label>
+		<span class="error-text">{$ 'form_street_error' | trans $}</span>
+		<span class="warning-text">{$ "form_street_number_warning" | trans $}</span>
+	</div>
+	<div class="field-grp field-postcode">
+		<input type="text" id="post-code" placeholder="{$ 'zip_label' | trans $}" data-pattern="^[0-9]{4,5}$">
+		<label for="post-code" class="sr-only">{$ 'zip_label' | trans $}</label>
+		<span class="error-text">{$ 'form_zip_error' | trans $}</span>
+	</div>
+	<div class="field-grp field-city">
+		<input type="text" id="city" placeholder="{$ 'city_label' | trans $}" data-pattern="^.+$">
+		<label for="city" class="sr-only">{$ 'city_label' | trans $}</label>
+		<span class="error-text">{$ 'form_city_error' | trans $}</span>
+	</div>
+	<select id="country" title="{$ 'country_label' | trans | e( 'html_attr' ) $}"
+			data-jcf='{"wrapNative": false, "wrapNativeOnMobile": true, "flipDropToFit": true,  "maxVisibleItems": 6}'
+			class="no-outline country-select">
+		{% for countryCode in COUNTRIES %}
+		<option value="{$ countryCode $}">{$ ( 'country_option_' ~ countryCode ) | trans $}</option>
+		{% endfor %}
+	</select>
 </template>
 
-<script>
-	export default {
+<script lang="ts">
+    import Vue from "vue";
+	export default Vue.extend({
 		name: "postal"
-	}
+	});
 </script>
