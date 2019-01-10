bc853aa1bf0345d962f999c091497dbe127873f6
tzhelyazkova <tonina.zhelyazkova@wikimedia.de>
2019-01-10 10:45:06 +0100

    Adjust code to typescript requirements

--- skins/cat17/src/app/components/Name.vue
+++ skins/cat17/src/app/components/Name.vue
@@ -1,9 +1,19 @@
 <template>
-
+	<div class="field-grp field-firstname">
+		<input type="text" id="first-name" name="firstName" placeholder="{$ 'firstname_label' | trans $}" data-pattern="^.+$">
+		<label for="first-name" class="sr-only">{$ 'firstname_label' | trans $}</label>
+		<span class="error-text">{$ 'form_firstname_error' | trans $}</span>
+	</div>
+	<div class="field-grp field-lastname">
+		<input type="text" id="last-name" name="lastName" placeholder="{$ 'lastname_label' | trans $}" data-pattern="^.+$">
+		<label for="last-name" class="sr-only">{$ 'lastname_label' | trans $}</label>
+		<span class="error-text">{$ 'form_lastname_error' | trans $}</span>
+	</div>
 </template>
 
-<script>
-	export default {
+<script lang="ts">
+    import Vue from "vue";
+	export default Vue.extend({
 		name: "name"
-	}
+	});
 </script>
