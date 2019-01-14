1c5a8f9e54724c4bf4eef716c6e50831ab6c9370
tzhelyazkova <tonina.zhelyazkova@wikimedia.de>
2019-01-14 16:50:08 +0100

    Develop the address form according to specs - Add css rules specific to the address form - Pass all data from .twig to vue components as props

--- skins/cat17/address-form/src/AddressForm.vue
+++ skins/cat17/address-form/src/AddressForm.vue
@@ -1,38 +1,65 @@
 <template>
-<div>
-	<select class="no-outline salutation" id="salutation" name="salutation"
-			data-jcf='{"wrapNative": false,  "wrapNativeOnMobile": true  }'>
-		<option hidden class="hideme" value="">{$ 'salutation_label' | trans $}</option>
-		<option value="Herr">{$ 'salutation_option_mr' | trans $}</option>
-		<option value="Frau">{$ 'salutation_option_mrs' | trans $}</option>
-	</select>
-	<select class="no-outline personal-title" id="title" name="title"
-			data-jcf='{"wrapNative": false, "wrapNativeOnMobile": true}'>
-		<option value="">{$ 'title_option_none' | trans $}</option>
-		<option value="Dr.">Dr.</option>
-		<option value="Prof.">Prof.</option>
-		<option value="Prof. Dr.">Prof. Dr.</option>
-	</select>
-	<span class="error-text">{$ 'form_salutation_error' | trans $}</span>
-	<name></name>
-	<postal></postal>
-	<email></email>
+<div id="addressForm" class="container">
+    <!--will remove the hardcoded text when composer update is fixed-->
+ 	<h2>Anschrift ändern {{ messages.address_form_title }}</h2>
+ 	<h5>Die bisherigen Daten sind schon eingetragen, bitte passen Sie diese an. {{ messages.address_form_subtitle }}</h5>
+ 	<div class="row">
+        <div class="col-xs-12 col-md-9">
+            <div class="form-shadow-wrap">
+                <div v-if="!isCompany">
+                    <div class="align-block">
+                        <label for="salutation">{{ messages.salutation_label }}</label>
+                        <select class="salutation col-xs-12 col-md-6" id="salutation" name="salutation" data-jcf='{"wrapNative": false,  "wrapNativeOnMobile": true  }'>
+                            <option hidden class="hideme" value="">{{ messages.salutation_label }}</option>
+                            <option value="Herr">{{ messages.salutation_option_mr }}</option>
+                            <option value="Frau">{{ messages.salutation_option_mrs }}</option>
+                        </select>
+                    </div>
+                    <div class="align-block">
+                        <!--will remove the hardcoded text when composer update is fixed-->
+                        <label for="title">Akademischer Titel {{ messages.academic_title_label }}</label>
+                        <select class="personal-title col-xs-12 col-md-6" id="title" name="title" data-jcf='{"wrapNative": false, "wrapNativeOnMobile": true}'>
+                            <option value="">{{ messages.title_option_none }}</option>
+                            <option value="Dr.">Dr.</option>
+                            <option value="Prof.">Prof.</option>
+                            <option value="Prof. Dr.">Prof. Dr.</option>
+                        </select>
+                    </div>
+                    <span class="error-text"> {{ messages.form_salutation_error }}</span>
+                </div>
+                <name :is-company="isCompany" :messages="messages"></name>
+                <postal :messages="messages" :countries="countries"></postal>
+            </div>
+        </div>
+        <div class="col-xs-12 col-md-3 submit">
+            <input type="submit" value="Kontaktdaten ändern" class="btn btn-address-change">
+            <span class="info-text">Im folgenden Schritt können Sie die Daten noch einmal prüfen.</span>
+        </div>
+ 	</div>
 </div>
 </template>
 
 <script lang="ts">
   	import Vue from 'vue';
 	import Name from './components/Name.vue';
-	import Email from './components/Email.vue';
 	import Postal from './components/Postal.vue';
 
-    export default Vue.extend ({
+    export default Vue.extend ( {
 		name: 'addressForm',
 		components: {
 			Name,
-			Email,
 			Postal
 		},
-		props: ['addressType']
-	});
+		props: {
+            addressToken: String,
+            isCompany: Boolean,
+            messages: Object,
+            countries: {
+                type: Array,
+                default: function() {
+                    return [ 'DE', 'AT', 'CH', 'BE', 'IT', 'LI', 'LU' ];
+                }
+            }
+        }
+	} );
 </script>
--- skins/cat17/address-form/src/components/Name.vue
+++ skins/cat17/address-form/src/components/Name.vue
@@ -1,21 +1,29 @@
 <template>
-<div>
-	<div class="field-grp field-firstname">
-		<input type="text" id="first-name" name="firstName" placeholder="{$ 'firstname_label' | trans $}" data-pattern="^.+$">
-		<label for="first-name" class="sr-only">{$ 'firstname_label' | trans $}</label>
-		<span class="error-text">{$ 'form_firstname_error' | trans $}</span>
-	</div>
-	<div class="field-grp field-lastname">
-		<input type="text" id="last-name" name="lastName" placeholder="{$ 'lastname_label' | trans $}" data-pattern="^.+$">
-		<label for="last-name" class="sr-only">{$ 'lastname_label' | trans $}</label>
-		<span class="error-text">{$ 'form_lastname_error' | trans $}</span>
-	</div>
+<div class="row">
+    <div v-if="isCompany" class="field-grp field-company col-xs-12 col-md-6">
+        <label for="company-name">{{ messages.companyname_label }}</label>
+        <input type="text" id="company-name" name="companyName" :placeholder="messages.companyname_label" data-pattern="^.+$">
+        <span class="error-text">{{ messages.form_companyname_error }}</span>
+    </div>
+    <div v-else>
+        <div class="field-grp field-firstname col-xs-12 col-md-6">
+            <label for="first-name">{{ messages.firstname_label }}</label>
+            <input type="text" id="first-name" name="firstName" :placeholder="messages.firstname_label" data-pattern="^.+$">
+            <span class="error-text">{{ messages.form_firstname_error }}</span>
+        </div>
+        <div class="field-grp field-lastname col-xs-12 col-md-6">
+            <label for="last-name">{{ messages.lastname_label }}</label>
+            <input type="text" id="last-name" name="lastName" :placeholder="messages.lastname_label" data-pattern="^.+$">
+            <span class="error-text">{{ messages.form_lastname_error }}</span>
+        </div>
+    </div>
 </div>
 </template>
 
 <script lang="ts">
-    import Vue from "vue";
-	export default Vue.extend({
-		name: "name"
-	});
+    import Vue from 'vue';
+	export default Vue.extend( {
+        name: "name",
+        props: [ 'isCompany', 'messages' ]
+	} );
 </script>
