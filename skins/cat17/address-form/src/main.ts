754ecf2142140772933b8c6004565b8131570677
tzhelyazkova <tonina.zhelyazkova@wikimedia.de>
2019-01-14 16:50:08 +0100

    Develop the address form according to specs - Add css rules specific to the address form - Pass all data from .twig to vue components as props

--- skins/cat17/address-form/src/main.ts
+++ skins/cat17/address-form/src/main.ts
@@ -3,8 +3,17 @@ import AddressForm from "./AddressForm.vue";
 import store from "./store";
 
 Vue.config.productionTip = false;
-
-new Vue({
+let addressElement: any = document.getElementById( 'updateAddress' );
+new Vue( {
   store,
-  render: h => h(AddressForm)
-}).$mount("#updateAddress");
+  render: h => h(
+      AddressForm,
+      {
+          props: {
+              addressToken: addressElement.getAttribute( 'data-address-token' ),
+              isCompany: addressElement.getAttribute( 'data-is-company' ),
+              messages: JSON.parse( addressElement.getAttribute( 'data-messages' ) )
+          }
+      }
+    )
+} ).$mount( "#updateAddress" );
