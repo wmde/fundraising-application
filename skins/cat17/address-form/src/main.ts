579695d22628d6c341f7682e3f0d52820ba6ef6c
tzhelyazkova <tonina.zhelyazkova@wikimedia.de>
2019-01-22 00:18:48 +0100

    Squash actions and adapt state

--- skins/cat17/address-form/src/main.ts
+++ skins/cat17/address-form/src/main.ts
@@ -5,18 +5,19 @@ import store from "./store";
 Vue.config.productionTip = false;
 let addressElement: any = document.getElementById( 'updateAddress' );
 let initAddressForm: any = document.getElementById( 'address-form' );
-
 new Vue( {
-  store,
-  render: h => h(
-      AddressForm,
-      {
-          props: {
-              addressToken: addressElement.getAttribute( 'data-address-token' ),
-              isCompany: addressElement.getAttribute( 'data-is-company' ),
-              messages: JSON.parse( addressElement.getAttribute( 'data-messages' ) ),
-              initAddressForm: initAddressForm
-          }
-      }
-    )
+	store,
+	render: h => h(
+		AddressForm,
+		{
+			props: {
+				addressToken: addressElement.getAttribute('data-address-token'),
+				isCompany: addressElement.getAttribute('data-is-company'),
+				messages: JSON.parse(addressElement.getAttribute('data-messages')),
+				initAddressForm: {
+					validateAddressURL: initAddressForm.getAttribute('data-validate-address-url'),
+				}
+			}
+		}
+	)
 } ).$mount( "#updateAddress" );
