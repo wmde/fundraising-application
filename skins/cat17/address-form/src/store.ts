3b7e8a319b23106adee56697398501a2d9066fc0
tzhelyazkova <tonina.zhelyazkova@wikimedia.de>
2019-01-22 16:58:08 +0100

    Create and assign types

--- skins/cat17/address-form/src/store.ts
+++ skins/cat17/address-form/src/store.ts
@@ -1,23 +1,25 @@
 import Vue from "vue";
-import Vuex from "vuex";
-import { Validity } from '../../src/app/lib/validation/validation_states';
-import { Helper } from 'mixins/helper'
+import Vuex, { StoreOptions } from "vuex";
+import { Validity } from './lib/validation_states';
+import { Helper } from './mixins/helper';
+import { AddressState, Payload, Form, FormData, ValidationResult } from './types';
 
-const VALIDATE_INPUT = 'VALIDATE_INPUT';
-const MARK_EMPTY_FIELD_INVALID = 'MARK_EMPTY_FIELD_INVALID';
-const BEGIN_ADDRESS_VALIDATION = 'BEGIN_ADDRESS_VALIDATION';
-const FINISH_ADDRESS_VALIDATION = 'FINISH_ADDRESS_VALIDATION';
-const STORE_VALUES = 'STORE_VALUES';
-const ADDRESS_FIELD_VALIDATOR_NAMES = [
+const VALIDATE_INPUT: string = 'VALIDATE_INPUT';
+const MARK_EMPTY_FIELD_INVALID: string = 'MARK_EMPTY_FIELD_INVALID';
+const BEGIN_ADDRESS_VALIDATION: string = 'BEGIN_ADDRESS_VALIDATION';
+const FINISH_ADDRESS_VALIDATION: string = 'FINISH_ADDRESS_VALIDATION';
+const STORE_VALUES: string = 'STORE_VALUES';
+const ADDRESS_FIELD_VALIDATOR_NAMES: Array<string> = [
 	'addressType',
 	'salutation', 'title', 'firstName', 'lastName',
 	'companyName',
 	'street', 'postcode', 'city', 'country'
 ]
 let transport = new WMDE.JQueryTransport();
+
 Vue.use(Vuex);
 
-export default new Vuex.Store({
+const store: StoreOptions<AddressState> = {
 	state: {
         isValidating: false,
         form: {
@@ -66,16 +68,16 @@ export default new Vuex.Store({
                 value: '',
                 isValid: Validity.INCOMPLETE
             }
-    }
+        }  
 	},
 	mutations: {
-        [STORE_VALUES](state, payload) {
-           payload.forEach(field => {
+        [STORE_VALUES](state, payload): void {
+            payload.forEach((field: FormData) => {
                state.form[field.name].value = field.value;
            });
         },
-		[VALIDATE_INPUT](state, payload) {
-            payload.forEach(field => {
+        [VALIDATE_INPUT](state, payload): void {
+            payload.forEach((field: FormData) => {
                 if (field.value === '' && field.optionalField === true) {
                     state.form[field.name] = {
                         ...state.form[field.name],
@@ -86,27 +88,27 @@ export default new Vuex.Store({
                 else {
                     state.form[field.name] = {
                         ...state.form[field.name],
-                        dataEntered: false,
-                        isValid: Validity.INCOMPLETE
+                        dataEntered: true,
+                        isValid: Helper.inputIsValid(field.value, field.pattern)
                     };
                 }
             });
 		},
-		[MARK_EMPTY_FIELD_INVALID](state, payload) {
-			payload.forEach(field => {
-                if (!field.optionalField && state.form[field].isValid === Validity.INCOMPLETE) {
-                    state.form[field].isValid = Validity.INVALID;
+		[MARK_EMPTY_FIELD_INVALID](state, payload): void {
+			payload.forEach((field: FormData) => {
+                if (!field.optionalField && state.form[field.name].isValid === Validity.INCOMPLETE) {
+                    state.form[field.name].isValid = Validity.INVALID;
 				}
 			});
 		},
-		[BEGIN_ADDRESS_VALIDATION](state) {
+		[BEGIN_ADDRESS_VALIDATION](state): void {
 			state.isValidating = true;
 		},
-		[FINISH_ADDRESS_VALIDATION](state, payload) {
+        [FINISH_ADDRESS_VALIDATION](state, payload: ValidationResult): void {
 			ADDRESS_FIELD_VALIDATOR_NAMES.forEach(name => {
-				if (name.dataEntered === true) {
+                if (state.form[name].dataEntered === true) {
                     state.form[name] = {
-						dataEntered: true,
+                        ...state.form[name],
                         isValid: state.form[name].isValid !== false && !payload.messages[name] ? Validity.VALID : Validity.INVALID
 					};
 				}
@@ -115,20 +117,22 @@ export default new Vuex.Store({
 		}
 	},
 	getters: {
-		validity(state, contentName) {
+		validity(state, contentName: string) {
             return state.form[contentName].isValid;
 		}
 	},
 	actions: {
-        storeAddressFields({ commit }, payload) {
+        storeAddressFields({ commit }, payload: Payload) {
             commit('STORE_VALUES', payload.formData);
             commit('VALIDATE_INPUT', payload.formData);
             commit('MARK_EMPTY_FIELD_INVALID', payload.formData);
             if (Object.keys(this.state.form).filter(field => this.state.form[field].isValid === Validity.INVALID ).length === 0 ) {
                 commit('BEGIN_ADDRESS_VALIDATION', payload.formData);
                 return transport.postData(payload.validateAddressURL, payload.formData)
-                    .then(validationResult => commit('FINISH_ADDRESS_VALIDATION', validationResult));
+                    .then( (validationResult: ValidationResult) => commit('FINISH_ADDRESS_VALIDATION', validationResult));
             }
         }
 	}
-});
+}
+
+export default new Vuex.Store<AddressState>(store);
