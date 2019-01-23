04c93a08b0a3ee81f6a5d9ec68b34c9693cf942c
tzhelyazkova <tonina.zhelyazkova@wikimedia.de>
2019-01-23 18:24:58 +0100

    Dispatch validate input action for each form field

--- skins/cat17/address-form/src/components/Postal.vue
+++ skins/cat17/address-form/src/components/Postal.vue
@@ -3,28 +3,41 @@
     <div class="row">
         <div class="field-grp field-street col-xs-12 col-md-6">
             <label for="street">{{ messages.street_label }}</label>
-            <input type="text" id="street" :placeholder="messages.street_label" data-pattern="^.+$">
-            <span class="error-text">{{ messages.form_street_error }}</span>
+            <input type="text" 
+                    id="street" 
+                    :placeholder="messages.street_label" 
+                    data-pattern="^.+$"
+                    @blur="validateInput('street')">
+            <span v-if="error('street')" class="error-text">{{ messages.form_street_error }}</span>
             <span class="warning-text">{{ messages.form_street_number_warning }}</span>
         </div>
     </div>
     <div class="row">
         <div class="field-grp field-city col-xs-12 col-md-6">
             <label for="city">{{ messages.city_label }}</label>
-            <input type="text" id="city" :placeholder="messages.city_label" data-pattern="^.+$">
-            <span class="error-text">{{ messages.form_city_error }}</span>
+            <input type="text" 
+                    id="city" 
+                    :placeholder="messages.city_label"
+                    data-pattern="^.+$"
+                    @blur="validateInput('city')">
+            <span v-if="error('city')" class="error-text">{{ messages.form_city_error }}</span>
         </div>
         <div class="field-grp field-postcode col-xs-12 col-md-6">
             <label for="post-code">{{ messages.zip_label }}</label>
-            <input type="text" id="post-code" :placeholder="messages.zip_label" data-pattern="^[0-9]{4,5}$">
-            <span class="error-text">{{ messages.form_zip_error }}</span>
+            <input type="text" 
+                    id="post-code" 
+                    :placeholder="messages.zip_label" 
+                    data-pattern="^[0-9]{4,5}$"
+                    @blur="validateInput('postCode')">
+            <span v-if="error('postCode')" class="error-text">{{ messages.form_zip_error }}</span>
         </div>
     </div>
     <div class="align-block">
         <label for="country">{{ messages.country_label }}</label>
-        <select id="country" title="{$ 'country_label' | trans | e( 'html_attr' ) $}"
+        <select id="country"
                 class="country-select col-xs-12 col-md-6" 
-                data-jcf='{"wrapNative": false, "wrapNativeOnMobile": true, "flipDropToFit": true,  "maxVisibleItems": 6}'>
+                data-jcf='{"wrapNative": false, "wrapNativeOnMobile": true, "flipDropToFit": true,  "maxVisibleItems": 6}'
+                @blur="validateInput('country')">
             <option v-for="(countryCode, index) in countries" :value="countryCode" :key="index">{{ messages[ 'country_option_' + countryCode ] }}</option>
             {% endfor %}
         </select>
