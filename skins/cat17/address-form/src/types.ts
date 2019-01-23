ca2e7f32ddfab1fd47f736d259283d67fb53e0f9
tzhelyazkova <tonina.zhelyazkova@wikimedia.de>
2019-01-23 18:24:58 +0100

    Dispatch validate input action for each form field

--- skins/cat17/address-form/src/types.ts
+++ skins/cat17/address-form/src/types.ts
@@ -21,7 +21,30 @@ export interface AddressState {
     form: Form,
 }
 
+export interface AddressMutations {
+    VALIDATE_INPUT: { field: inputField },
+    MARK_EMPTY_FIELD_INVALID: { payload: Array<FormData>},
+    BEGIN_ADDRESS_VALIDATION: { payload: Array<FormData> },
+    FINISH_ADDRESS_VALIDATION: { payload: ValidationResult }
+}
+
+export interface AddressGetters {
+    validity: boolean,
+    allFieldsAreValid: boolean
+}
+
+export interface AddressActions {
+    validateInput: { field: inputField },
+    storeAddressFields: { payload: Payload }
+}
+
 export interface FormData {
+    name: string,
+    pattern: string,
+    optionalField: boolean
+}
+
+export interface inputField {
     name: string,
     value: string,
     pattern: string,
@@ -35,9 +58,9 @@ export interface Payload {
 
 export interface ValidationResult {
     status: string,
-    messages: {}
+    messages: object
 }
 
 export interface Helper {
-    inputIsValid(value: string, pattern: string): boolean;
+    inputIsValid(value: string, pattern: string): boolean
 }
