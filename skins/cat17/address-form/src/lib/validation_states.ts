1e693f91c0c598acb69ed4fddbab1379845d58a7
tzhelyazkova <tonina.zhelyazkova@wikimedia.de>
2019-01-28 10:46:09 +0100

    Resolve TypeScript errors

---
+++ skins/cat17/address-form/src/lib/validation_states.ts
@@ -0,0 +1,11 @@
+export const ValidationStates = {
+    OK: 'OK',
+    ERR: 'ERR',
+    INCOMPLETE: 'INCOMPLETE'
+};
+
+export const Validity = {
+    VALID: true,
+    INVALID: false,
+    INCOMPLETE: null
+};
--- skins/cat17/address-form/src/types.ts
+++ skins/cat17/address-form/src/types.ts
@@ -1,66 +1,36 @@
 export interface FormValidity {
-    dataEntered: boolean,
-    value: string,
-    isValid: boolean
+	dataEntered: boolean,
+	isValid: boolean | null
 }
 
 export interface Form {
-    salutation: FormValidity,
-    title: FormValidity,
-    firstName: FormValidity,
-    lastName: FormValidity,
-    companyName: FormValidity,
-    street: FormValidity,
-    postcode: FormValidity,
-    city: FormValidity,
-    country: FormValidity
+	[key: string]: FormValidity
 }
 
 export interface AddressState {
-    isValidating: boolean
-    form: Form,
-}
-
-export interface AddressMutations {
-    VALIDATE_INPUT: { field: inputField },
-    MARK_EMPTY_FIELD_INVALID: { payload: Array<FormData>},
-    BEGIN_ADDRESS_VALIDATION: { payload: Array<FormData> },
-    FINISH_ADDRESS_VALIDATION: { payload: ValidationResult }
-}
-
-export interface AddressGetters {
-    validity: boolean,
-    allFieldsAreValid: boolean
-}
-
-export interface AddressActions {
-    validateInput: { field: inputField },
-    storeAddressFields: { payload: Payload }
-}
-
-export interface FormData {
-    name: string,
-    pattern: string,
-    optionalField: boolean
-}
-
-export interface inputField {
-    name: string,
-    value: string,
-    pattern: string,
-    optionalField: boolean
-}
-
-export interface Payload {
-    formData: Array<FormData>,
-    validateAddressURL: string
+	isValidating: boolean
+	form: Form,
 }
 
 export interface ValidationResult {
-    status: string,
-    messages: object
+	status: string,
+	messages: object
 }
 
 export interface Helper {
-    inputIsValid(value: string, pattern: string): boolean
+	inputIsValid(value: string, pattern: string): boolean
 }
+
+export interface inputField {
+	name: string,
+	value: string,
+	pattern: string,
+	optionalField: boolean
+}
+
+export interface Payload {
+	validateAddressURL: string,
+    formData: Array<inputField>
+}
+
+
