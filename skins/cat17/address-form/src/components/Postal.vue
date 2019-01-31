2cbbecf5fd1f800f70f589bff180a05a15fbeace
tzhelyazkova <tonina.zhelyazkova@wikimedia.de>
2019-01-31 10:19:11 +0100

    Submit form

--- skins/cat17/address-form/src/components/Postal.vue
+++ skins/cat17/address-form/src/components/Postal.vue
@@ -5,6 +5,7 @@
 			<label for="street">{{ messages.street_label }}</label>
 			<input type="text"
 					id="street"
+                    name="street"
 					:placeholder="messages.street_label"
 					v-model="formData[5].value"
 					@blur="validateInput(formData, 'street')">
@@ -17,6 +18,7 @@
 			<label for="city">{{ messages.city_label }}</label>
 			<input type="text"
 					id="city"
+                    name="city"
 					:placeholder="messages.city_label"
 					v-model="formData[6].value"
 					@blur="validateInput(formData, 'city')">
@@ -26,6 +28,7 @@
 			<label for="post-code">{{ messages.zip_label }}</label>
 			<input type="text"
 					id="post-code"
+                    name="postcode"
 					v-model="formData[7].value"
 					:placeholder="messages.zip_label"
 					@blur="validateInput(formData, 'postcode')">
@@ -33,19 +36,20 @@
 		</div>
 	</div>
 	<div class="row">
-        <div class="align-block">
-            <label for="country">{{ messages.country_label }}</label>
-            <select id="country"
-                    class="country-select col-xs-12 col-md-6"
-                    data-jcf='{"wrapNative": false, "wrapNativeOnMobile": true, "flipDropToFit": true,  "maxVisibleItems": 6}'
-                    v-model="formData[8].value"
-                    @blur="validateInput(formData, 'country')">
-                <option v-for="(countryCode, index) in countries" 
-                        :value="countryCode"
-                        :key="index">{{ messages[ 'country_option_' + countryCode ] }}
-                </option>
-            </select>
-        </div>
+	<div class="align-block">
+		<label for="country">{{ messages.country_label }}</label>
+		<select id="country"
+				name="country"
+				class="country-select col-xs-12 col-md-6"
+				data-jcf='{"wrapNative": false, "wrapNativeOnMobile": true, "flipDropToFit": true,  "maxVisibleItems": 6}'
+				v-model="formData[8].value"
+				@blur="validateInput(formData, 'country')">
+			<option v-for="(countryCode, index) in countries" 
+					:value="countryCode"
+					:key="index">{{ messages[ 'country_option_' + countryCode ] }}
+			</option>
+		</select>
+	</div>
 	</div>
 </div>
 </template>
