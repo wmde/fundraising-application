490534b465bb1fb4ff115d9c059549a0a6a5d991
tzhelyazkova <tonina.zhelyazkova@wikimedia.de>
2019-02-08 15:33:47 +0100

    Add AddressForm.vue tests

--- skins/cat17/address-form/src/__tests__/components/AddressForm.spec.ts
+++ skins/cat17/address-form/src/__tests__/components/AddressForm.spec.ts
@@ -1,7 +1,6 @@
 import { shallowMount, mount, createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
 import AddressForm from '../../AddressForm.vue'
-import {Validity} from "@/types";
 
 const localVue = createLocalVue();
 
@@ -11,7 +10,7 @@ function newTestProperties(overrides: Object) {
     return Object.assign(
         {
             addressToken: '',
-            isCompany: true,
+            isCompany: false,
             messages: {},
             validateAddressURL: '/update-address',
             updateAddressURL: '/update-address/',
@@ -21,22 +20,6 @@ function newTestProperties(overrides: Object) {
     );
 }
 
-const defaultState = {
-    form: {
-        salutation: Validity.INCOMPLETE,
-        title: Validity.INCOMPLETE,
-        firstName: Validity.INCOMPLETE,
-        lastName: Validity.INCOMPLETE,
-        companyName: Validity.INCOMPLETE,
-        street: Validity.INCOMPLETE,
-        postcode: Validity.INCOMPLETE,
-        city: Validity.INCOMPLETE,
-        country: Validity.VALID,
-        addressType: Validity.VALID
-    }
-};
-
-/* Basic sanity test */
 describe('AddressForm.vue', () => {
     let actions: any
     let store: any
@@ -48,24 +31,79 @@ describe('AddressForm.vue', () => {
             storeAddressFields: jest.fn()
         };
         getters = {
-            validity: () => jest.fn(),
+            validity: jest.fn(),
             invalidFields: jest.fn(),
             allFieldsAreValid: jest.fn()
-        };
+        }
         store = new Vuex.Store({
-            state: defaultState,
             actions,
             getters
         });
+    })
+
+    it('Form renders', () => {
+        const wrapper = mount(AddressForm, {
+            propsData: newTestProperties({ addressToken: 'bla' })
+        });
+        expect(typeof wrapper).toEqual('object')
     });
 
+    it('Property isCompany is passed', () => {
+        const wrapper = mount(AddressForm, {
+            propsData: newTestProperties({ isCompany: true })
+        });
+        expect(wrapper.props('isCompany')).toBe(true);
+    });
 
-    test('renders', () => {
+    it('Company address form is shown when isCompany is passed as true', () => {
+        const wrapper = mount(AddressForm, {
+            propsData: newTestProperties({ isCompany: true })
+        });
+        expect(wrapper.contains('#company-name')).toBe(true);
+        expect(wrapper.contains('#first-name')).toBe(false);
+        expect(wrapper.contains('#last-name')).toBe(false);
+        expect(wrapper.contains('#salutation')).toBe(false);
+        expect(wrapper.contains('#title')).toBe(false);
+    });
+
+    it('Person address form is shown when isCompany is passed as false', () => {
+        const wrapper = mount(AddressForm, {
+            propsData: newTestProperties({ isCompany: false })
+        });
+        expect(wrapper.contains('#company-name')).toBe(false);
+        expect(wrapper.contains('#first-name')).toBe(true);
+        expect(wrapper.contains('#last-name')).toBe(true);
+        expect(wrapper.contains('#salutation')).toBe(true);
+        expect(wrapper.contains('#title')).toBe(true);
+    });
+
+    it('On blur from an input field validateInput is dispatched with the correct field value', () => {
         const wrapper = mount(AddressForm, {
             store,
             localVue,
-            propsData: newTestProperties({ addressToken: 'bla' })
+            propsData: newTestProperties({ isCompany: false })
         });
-        expect(typeof wrapper).toMatch('object')
-    })
-})
+        let input = wrapper.find('#first-name');
+        input.setValue('test');
+        input.trigger('blur');
+        expect(actions.validateInput).toHaveBeenCalledWith({
+            name: 'firstName',
+            value: 'test',
+            pattern: '^.+$',
+            optionalField: true
+        });
+    });
+
+    it('showError is updated correctly according to the validation result', () => {
+        const wrapper = mount(AddressForm, {
+            store,
+            localVue,
+            propsData: newTestProperties({ isCompany: false })
+        });
+        let input = wrapper.find('#post-code');
+        input.setValue('test');
+        input.trigger('blur');
+        expect(wrapper.vm.$data.showError.postCode).toBe(false);
+    });
+
+});
