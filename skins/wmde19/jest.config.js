cf47dd9585ef9500e19a45ca076e2c602b0d7165
tzhelyazkova <tonina.zhelyazkova@wikimedia.de>
2019-03-31 21:27:08 +0200

    Setup new skin folder using vue-cli
    - Use .vue for component files
    - Add Babel as compiler
    - Add TypeScript
    - Add Vuex as store
    - Add Sass as css pre-processor
    - Add Jest as unit test runner
    - Add Nightwatch as e2e test runner

---
+++ skins/wmde19/jest.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  moduleFileExtensions: [
+    'js',
+    'json',
+    'vue',
+    'ts'
+  ],
+  transform: {
+    '^.+\\.vue$': 'vue-jest',
+    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  transformIgnorePatterns: [
+    '/node_modules/'
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  snapshotSerializers: [
+    'jest-serializer-vue'
+  ],
+  testMatch: [
+    '**/tests/unit/**/*.spec.(js|ts)|**/__tests__/*.(js|ts)'
+  ],
+  testURL: 'http://localhost/',
+  globals: {
+    'ts-jest': {
+      babelConfig: true
+    }
+  }
+}
