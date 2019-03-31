10061a8d81db0da3aa67b0a0774b0e4e63bd5b85
tzhelyazkova <tonina.zhelyazkova@wikimedia.de>
2019-03-31 21:27:08 +0200

    Setup new skin folder using vue-cli
    - Use .vue for component files
    - Add Babel as compiler
    - Add TypeScript
    - Add Vuex as store
    - Add Sass as css pre-processor
    - Add Jest as unit test runner
    - Add Nightwatch as e2e test runner

---
+++ skins/wmde19/tests/e2e/specs/test.js
@@ -0,0 +1,10 @@
+// For authoring Nightwatch tests, see
+// http://nightwatchjs.org/guide#usage
+
+module.exports = {
+  'default e2e tests': browser => {
+    browser
+      .url(process.env.VUE_DEV_SERVER_URL)
+      .waitForElementVisible('#app', 5000)
+  }
+}
