ef2474f265893603fd960d4c896e0d43dc4b33e1
moiikana <corinna.hillebrand@wikimedia.de>
2019-04-02 14:45:47 +0200

    correct validation condition for membershiptype
    https://phabricator.wikimedia.org/T214491

--- skins/cat17/src/app/lib/state_aggregation/membership/some_validation_sections_are_incomplete.js
+++ skins/cat17/src/app/lib/state_aggregation/membership/some_validation_sections_are_incomplete.js
@@ -4,10 +4,7 @@ const Validity = require( '../../validation/validation_states' ).Validity;
 
 module.exports = function ( state ) {
 	return (
-		(
-			( state.membershipFormContent.addressType === 'person' && state.membershipFormContent.membershipType !== null ) ||
-			( state.membershipFormContent.addressType === 'firma' && state.membershipFormContent.membershipType === 'sustaining' )
-		) ||
+		( state.membershipFormContent.membershipType !== 'active' && state.membershipFormContent.membershipType !== 'sustaining' ) ||
 		state.validity.paymentData === Validity.INCOMPLETE ||
 		state.membershipInputValidation.email.isValid === Validity.INCOMPLETE ||
 		state.validity.address === Validity.INCOMPLETE ||
