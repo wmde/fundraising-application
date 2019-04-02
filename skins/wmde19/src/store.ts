fc0e940728696dede6f19460b7379c59c590a77f
Gabriel Birke <gabriel.birke@wikimedia.de>
2019-04-02 16:48:58 +0200

    Use wikimedia CS in eslint

--- skins/wmde19/src/store.ts
+++ skins/wmde19/src/store.ts
@@ -1,16 +1,16 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+Vue.use( Vuex );
 
-export default new Vuex.Store({
-  state: {
+export default new Vuex.Store( {
+	state: {
 
-  },
-  mutations: {
+	},
+	mutations: {
 
-  },
-  actions: {
+	},
+	actions: {
 
-  }
-})
+	},
+} );
--- skins/wmde19/tests/e2e/specs/test.js
+++ skins/wmde19/tests/e2e/specs/test.js
@@ -2,9 +2,9 @@
 // http://nightwatchjs.org/guide#usage
 
 module.exports = {
-  'default e2e tests': browser => {
-    browser
-      .url(process.env.VUE_DEV_SERVER_URL)
-      .waitForElementVisible('#app', 5000)
-  }
-}
+	'default e2e tests': browser => {
+		browser
+			.url( process.env.VUE_DEV_SERVER_URL )
+			.waitForElementVisible( '#app', 5000 );
+	},
+};
