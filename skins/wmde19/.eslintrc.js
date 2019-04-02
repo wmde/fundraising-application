2c092ef02173547807120bd97a848636566ccf9b
Gabriel Birke <gabriel.birke@wikimedia.de>
2019-04-02 16:48:58 +0200

    Use wikimedia CS in eslint

--- skins/wmde19/.eslintrc.js
+++ skins/wmde19/.eslintrc.js
@@ -1,18 +1,41 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended',
-    '@vue/typescript'
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
-  parserOptions: {
-    parser: '@typescript-eslint/parser'
-  }
-}
+	root: true,
+	env: {
+		node: true,
+		es6: true,
+	},
+	plugins: [
+		'@typescript-eslint',
+	],
+	'extends': [
+		'wikimedia',
+
+	],
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+
+		// problematic in TypeScript / ES6
+		'no-unused-vars': 'off',
+		// TODO uncomment when https://github.com/typescript-eslint/typescript-eslint/issues/363 is fixed
+		// '@typescript-eslint/no-unused-vars': 'error',
+		'one-var': 'off',
+		'no-undef': 'off',
+
+		// diverging from Wikimedia rule set
+		'max-len': [ 'error', 120 ],
+		'comma-dangle': [ 'error', 'always-multiline' ],
+		'operator-linebreak': 'off',
+		'quote-props': 'off',
+		'valid-jsdoc': 'off',
+	},
+	'parser': 'vue-eslint-parser',
+	'parserOptions': {
+		'parser': '@typescript-eslint/parser',
+		'sourceType': 'module',
+		'ecmaVersion': 7,
+		'ecmaFeatures': {
+			'modules': true,
+		},
+	},
+};
