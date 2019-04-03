0eccac3ac3af71db07c4a3c491f382bfe2e0fb69
Gabriel Birke <gabriel.birke@wikimedia.de>
2019-04-03 19:38:01 +0200

    Add entry point for error messages
    As a first proof of concept, add an entry point for displaying error
    messages, showing the data exchange between the Twig-generated HTML
    markup and the vue entry point.
    
    This Proof-of-Concept shows the following areas where further refinement
    is needed:
    * A better way to pass the translation map directly to Vue and use a
    function in templates, instead of adding a `messages` property to each
    component.
    * A generalized way to pass data to the vue instance (avoid the
    boilerplate of checks, plus add types (interfaces) to the data model to
    be more explicit about what data is required (and optional).
    * A better way to build layouted apps. Currently, the `Error` page
    component is standalone. A better way would be to have a root `App` with
    a slot (its template consisting of layout components) and passing it the
    current "Page Component" in the render function of the entry point.
    * A convention to organize entry points, their "Page component" and
    their data model definition.

---
+++ skins/wmde19/src/Error.vue
@@ -0,0 +1,30 @@
+<template>
+	<main>
+		<!-- TODO create small i18n vue function to use instead of messages variable -->
+		<h1>{{ messages.error_page_header }}</h1>
+
+		<p>
+			{{ messages.error_page }}
+		</p>
+
+		<p v-if="errorMessage">
+			{{ errorMessage }}
+		</p>
+
+		<p>
+			<a href="/"><span v-html="messages.error_pages_return_to_donation"></span></a>
+		</p>
+	</main>
+
+</template>
+
+<script>
+export default {
+	name: 'Error',
+	props: [ 'messages', 'errorMessage' ],
+};
+</script>
+
+<style scoped>
+
+</style>
