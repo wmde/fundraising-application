9cc061ee66b819cab5115aff15889f02e0b05615
Gabriel Birke <gabriel.birke@wikimedia.de>
2019-04-05 11:09:24 +0200

    Create PageDataInitializer for Twig->Vue data
    Extract data selection into its own class.
    This also shows how to create view model. The View model can either be
    used in the Vuex store actions (for initializing the data) or directly
    in the component.

---
+++ skins/wmde19/src/page_data_initializer.ts
@@ -0,0 +1,14 @@
+export default class PageDataInitializer<T> {
+	applicationVars: T;
+	messages: object;
+
+	constructor( dataElementSelector: string = '#app' ) {
+		const dataElement: HTMLElement | null = document.querySelector( dataElementSelector );
+
+		if ( !dataElement ) {
+			throw new Error( 'No element found with selector ' + dataElementSelector );
+		}
+		this.applicationVars = JSON.parse( dataElement.dataset.applicationVars || '{}' );
+		this.messages = JSON.parse( dataElement.dataset.applicationMessages || '{}' );
+	}
+}
