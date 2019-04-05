f459c4ee2b2fede4d84274d6f3b4a61087316dc0
Gabriel Birke <gabriel.birke@wikimedia.de>
2019-04-05 11:58:32 +0200

    Use vue-i18n
    Remove `messages` property from Error and use i18n plugin instead.
    
    Until we have different locales, we're using the DEFAULT_LOCALE
    constant everywhere.

--- skins/wmde19/src/pages/error.ts
+++ skins/wmde19/src/pages/error.ts
@@ -1,9 +1,12 @@
 import Vue from 'vue';
+import VueI18n from 'vue-i18n';
 import Error from '@/components/pages/Error.vue';
 import App from '@/components/App.vue';
 import PageDataInitializer from '@/page_data_initializer';
+import { DEFAULT_LOCALE } from '@/locales';
 
 Vue.config.productionTip = false;
+Vue.use( VueI18n );
 
 interface ErrorModel {
 	message: string
@@ -11,7 +14,15 @@ interface ErrorModel {
 
 const pageData = new PageDataInitializer<ErrorModel>( '#app' );
 
+const i18n = new VueI18n( {
+	locale: DEFAULT_LOCALE,
+	messages: {
+		[ DEFAULT_LOCALE ]: pageData.messages,
+	},
+} );
+
 new Vue( {
+	i18n,
 	render: h => h( App, {}, [ h( Error, {
 		props: {
 			errorMessage: pageData.applicationVars.message,
