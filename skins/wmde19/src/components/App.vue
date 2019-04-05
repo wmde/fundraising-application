f01ef6d37d37ebfff44fbb6e9c3a0c8a24e37c63
tzhelyazkova <tonina.zhelyazkova@wikimedia.de>
2019-04-05 16:03:18 +0200

    Cleanup components and fix linting

--- skins/wmde19/src/components/App.vue
+++ skins/wmde19/src/components/App.vue
@@ -1,24 +1,27 @@
 <template>
-  <section id="app" class="section">
-    <div class="container">
-      <Header></Header>
-      <Content>
-        <slot></slot>
-      </Content>
-    </div>
-  </section>
+	<section id="app" class="section">
+		<div class="container">
+			<Header></Header>
+			<Content>
+				<slot></slot>
+			</Content>
+			<Footer></Footer>
+		</div>
+	</section>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import Header from './layout/Header.vue';
 import Content from './layout/Content.vue';
+import Footer from './layout/Footer.vue';
 
 export default Vue.extend( {
 	name: 'app',
 	components: {
 		Header,
 		Content,
+		Footer,
 	},
 } );
 </script>
--- skins/wmde19/src/components/layout/Content.vue
+++ skins/wmde19/src/components/layout/Content.vue
@@ -9,7 +9,3 @@ export default {
 	name: 'Content',
 };
 </script>
-
-<style scoped>
-
-</style>
