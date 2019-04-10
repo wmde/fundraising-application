c2f50dcba5c0df92feba98acc3638c27cb8ad261
tzhelyazkova <tonina.zhelyazkova@wikimedia.de>
2019-04-10 15:41:47 +0200

    Refactor AddressForm computed property
    Defining the computed property as a getter makes TypesSript accept it as such
    and actually compute the values, instead of just treating it as an isolated function.

--- skins/cat17/address-form/src/__tests__/components/Postal.spec.ts
+++ skins/cat17/address-form/src/__tests__/components/Postal.spec.ts
@@ -1,85 +1,92 @@
 import { shallowMount } from '@vue/test-utils'
 import Postal from '../../components/Postal.vue'
 
+function newTestProperties(overrides: Object) {
+    return Object.assign(
+        {
+            showError: {
+                salutation: false,
+                companyName: false,
+                firstName: false,
+                lastName: false,
+                street: false,
+                city: false,
+                postcode: false
+            },
+            formData: {
+                salutation: {
+                    name: 'salutation',
+                    value: '',
+                    pattern: '^(Herr|Frau)$',
+                    optionalField: false
+                },
+                title: {
+                    name: 'title',
+                    value: '',
+                    pattern: '',
+                    optionalField: true
+                },
+                companyName: {
+                    name: 'companyName',
+                    value: '',
+                    pattern: '^.+$',
+                    optionalField: true
+                },
+                firstName: {
+                    name: 'firstName',
+                    value: '',
+                    pattern: '^.+$',
+                    optionalField: false
+                },
+                lastName: {
+                    name: 'lastName',
+                    value: '',
+                    pattern: '^.+$',
+                    optionalField: false
+                },
+                street: {
+                    name: 'street',
+                    value: '',
+                    pattern: '^.+$',
+                    optionalField: false
+                },
+                city: {
+                    name: 'city',
+                    value: '',
+                    pattern: '^.+$',
+                    optionalField: false
+                },
+                postcode: {
+                    name: 'postcode',
+                    value: '',
+                    pattern: '[0-9]{4,5}$',
+                    optionalField: false
+                },
+                country: {
+                    name: 'country',
+                    value: 'DE',
+                    pattern: '',
+                    optionalField: false
+                },
+                addressType: {
+                    name: 'addressType',
+                    value: false ? 'firma' : 'person',
+                    pattern: '',
+                    optionalField: false
+                }
+            },
+            validateInput: jest.fn(),
+            messages: {},
+            countries: []
+        },
+        overrides
+    );
+}
+
 describe('AddressForm.vue', () => {
     it('shows street number warning if street field does not contain numbers', () => {
-        const wrapper = shallowMount(Postal, {
-            propsData: {
-                showError: {
-                    salutation: false,
-                    companyName: false,
-                    firstName: false,
-                    lastName: false,
-                    street: false,
-                    city: false,
-                    postcode: false
-                },
-                formData: {
-                    salutation: {
-                        name: 'salutation',
-                        value: '',
-                        pattern: '^(Herr|Frau)$',
-                        optionalField: false
-                    },
-                    title: {
-                        name: 'title',
-                        value: '',
-                        pattern: '',
-                        optionalField: true
-                    },
-                    companyName: {
-                        name: 'companyName',
-                        value: '',
-                        pattern: '^.+$',
-                        optionalField: true
-                    },
-                    firstName: {
-                        name: 'firstName',
-                        value: '',
-                        pattern: '^.+$',
-                        optionalField: false
-                    },
-                    lastName: {
-                        name: 'lastName',
-                        value: '',
-                        pattern: '^.+$',
-                        optionalField: false
-                    },
-                    street: {
-                        name: 'street',
-                        value: '',
-                        pattern: '^.+$',
-                        optionalField: false
-                    },
-                    city: {
-                        name: 'city',
-                        value: '',
-                        pattern: '^.+$',
-                        optionalField: false
-                    },
-                    postcode: {
-                        name: 'postcode',
-                        value: '',
-                        pattern: '[0-9]{4,5}$',
-                        optionalField: false
-                    },
-                    country: {
-                        name: 'country',
-                        value: 'DE',
-                        pattern: '',
-                        optionalField: false
-                    },
-                    addressType: {
-                        name: 'addressType',
-                        value: false ? 'firma' : 'person',
-                        pattern: '',
-                        optionalField: false
-                    }
-                },
-                validateInput: jest.fn(), 
-                messages: {},
-                countries: []
-            }
+        const wrapper = shallowMount( Postal, {
+            propsData: newTestProperties( {} )
         });
         let street = wrapper.find('#street');
         street.setValue('Testenhofen Ufer');
