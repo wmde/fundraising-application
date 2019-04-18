ebae431b03a469da84fa0ec15539cce082eb1cc9
Gabriel Birke <gabriel.birke@wikimedia.de>
2019-04-18 19:27:05 +0200

    Combine CSS into one file
    Configure webpack to combine the CSS from all entry points into one
    file.
    
    Unfortunately, due to a [webpack bug][1], this also generates an
    additional JS file that must be included.
    
    [1]: https://github.com/webpack/webpack/issues/7300

--- skins/wmde19/vue.config.js
+++ skins/wmde19/vue.config.js
@@ -34,6 +34,25 @@ module.exports = {
 					chunkFilename: 'css/[name].css',
 				} ] );
 			}
+			// Preliminary solution for combining all CSS into one file
+			// see https://github.com/webpack-contrib/mini-css-extract-plugin/issues/113
+			config.optimization.splitChunks( {
+				cacheGroups: {
+					commons: {
+						test: /node_modules/,
+						chunks: 'initial',
+						name: 'chunk-vendors',
+					},
+					'styles-compiled': {
+						name: 'styles',
+						test: module =>
+							module.nameForCondition &&
+							/\.(s?css|vue)$/.test( module.nameForCondition() ) && !/^javascript/.test( module.type ),
+						chunks: 'all',
+						enforce: true,
+					},
+				},
+			} );
 		}
 		config.module
 			.rule( 'vue' )
