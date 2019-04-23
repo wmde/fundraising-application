88595429a34aad8ff5c1cebb4f27a93bab0bb13c
Tim Eulitz <tim.eulitz@wikimedia.de>
2019-04-23 18:03:18 +0200

    Add all missing entry points with place holders

---
+++ skins/wmde19/src/components/pages/Placeholder.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="column is-full">
+
+		<h1 class="title">Placeholder Page</h1>
+
+		<p>
+			This page exists but it has not been implemented for the new skin yet
+			and you are seeing this placeholder page instead.
+		</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Placeholder',
+};
+</script>
