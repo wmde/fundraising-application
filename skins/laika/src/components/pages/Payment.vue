2ed28547b88b674612cc9e25057d310dc7fcb2ac
Tim Eulitz <tim.eulitz@wikimedia.de>
2019-05-02 17:40:08 +0200

    Add payment amount component
    Note that this commit is missing local updates to the wmde/Euro class
    which are still pending in a pull request at this time.

---
+++ skins/laika/src/components/pages/Payment.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="column is-full">
+		<form>
+			<payment-amount :paymentAmounts="paymentAmounts"></payment-amount>
+		</form>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import PaymentAmount from "@/components/pages/donation_form/PaymentAmount.vue";
+
+export default Vue.extend( {
+	name: 'Payment',
+	components: {
+		PaymentAmount,
+	},
+	props: [ 'paymentAmounts' ],
+} );
+</script>
--- skins/laika/src/pages/donation_form.ts
+++ skins/laika/src/pages/donation_form.ts
@@ -4,7 +4,7 @@ import PageDataInitializer from '@/page_data_initializer';
 import { DEFAULT_LOCALE } from '@/locales';
 import App from '@/components/App.vue';
 
-import Component from '@/components/pages/Placeholder.vue';
+import Component from '@/components/pages/Payment.vue';
 import Sidebar from '@/components/layout/Sidebar.vue';
 
 const PAGE_IDENTIFIER = 'donation-form';
@@ -12,11 +12,11 @@ const PAGE_IDENTIFIER = 'donation-form';
 Vue.config.productionTip = false;
 Vue.use( VueI18n );
 
-interface ErrorModel {
-	message: string
+interface PaymentAmountModel {
+	presetsAmounts: Array<string>
 }
 
-const pageData = new PageDataInitializer<ErrorModel>( '#app' );
+const pageData = new PageDataInitializer<PaymentAmountModel>( '#app' );
 
 const i18n = new VueI18n( {
 	locale: DEFAULT_LOCALE,
@@ -28,15 +28,19 @@ const i18n = new VueI18n( {
 new Vue( {
 	i18n,
 	render: h => h( App, {
-		props: {
-			assetsPath: pageData.assetsPath,
-			pageIdentifier: PAGE_IDENTIFIER,
+			props: {
+				assetsPath: pageData.assetsPath,
+				pageIdentifier: PAGE_IDENTIFIER,
+			},
 		},
-	},
-	[
-		h( Component, {} ),
-		h( Sidebar, {
-			slot: 'sidebar',
-		} ),
-	] ),
+		[
+			h( Component, {
+				props: {
+					paymentAmounts: pageData.applicationVars,
+				},
+			} ),
+			h( Sidebar, {
+				slot: 'sidebar',
+			} ),
+		] ),
 } ).$mount( '#app' );
