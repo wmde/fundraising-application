1aed09ff938b57f811e2a0c45297c9bb3a60aa37
Gabriel Birke <gabriel.birke@wikimedia.de>
2019-05-09 10:42:37 +0200

    Use sqlite instead of mysql on Travis
    This will speed up the tests on Travis

--- build/travis/createLocalTestConfig.sh
+++ build/travis/createLocalTestConfig.sh
@@ -1,8 +1,29 @@
-cat << EOF
+#!/usr/bin/env bash
+
+if [ "$MCP_ACCESS_KEY" != "" ] && [ "$MCP_PROJECT_ID" != "" ]; then
+
+cat << EOF > app/config/config.test.local.json
 {
 	"creditcard": {
 		"access-key": "$MCP_ACCESS_KEY",
 		"project-id": "$MCP_PROJECT_ID"
+	},
+	"db": {
+		"driver": "pdo_sqlite",
+		"memory": true
 	}
 }
 EOF
+
+else
+
+cat << EOF > app/config/config.test.local.json
+{
+	"db": {
+		"driver": "pdo_sqlite",
+		"memory": true
+	}
+}
+EOF
+
+fi
