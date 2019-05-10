1f0b30356f921ee89a35a76b4e8661b211b23def
Gabriel Birke <gabriel.birke@wikimedia.de>
2019-05-10 19:15:35 +0200

    Add tests for custom amounts
    Also, use HTML id instead of class name as a selector in tests.

--- skins/laika/tests/unit/components/pages/donation_form/PaymentAmount.spec.ts
+++ skins/laika/tests/unit/components/pages/donation_form/PaymentAmount.spec.ts
@@ -49,12 +49,201 @@ describe( 'PaymentAmount', () => {
 		const store = wrapper.vm.$store;
 		store.dispatch = jest.fn();
 
-		const customAmountInput = wrapper.find( '.custom-amount' );
-		( customAmountInput.element as HTMLInputElement ).value = '5';
+		const customAmountInput = wrapper.find( '#amount-custom' );
+		customAmountInput.setValue( '5' );
 		customAmountInput.trigger( 'blur' );
 		wrapper.find( '#amount-29900' ).trigger( 'click' );
 
-		expect( ( customAmountInput.element as HTMLInputElement ).value ).toBe( '' );
+		expect( wrapper.vm.$data.amountCustomValue ).toBe( '' );
+	} );
+
+	it( 'sends cent amount to store when custom amount is entered', () => {
+		const wrapper = mount( PaymentAmount, {
+			propsData: {
+				paymentAmounts: [ 5 ],
+				validateAmountURL: 'https://example.com/amount-check',
+			},
+			store: createStore(),
+			mocks: {
+				$t: () => {},
+			},
+		} );
+		const store = wrapper.vm.$store;
+		store.dispatch = jest.fn();
+
+		const customAmountInput = wrapper.find( '#amount-custom' );
+		customAmountInput.setValue( '23' );
+		customAmountInput.trigger( 'blur' );
+		const expectedAction = action( NS_PAYMENT, setAmount );
+		const expectedPayload = {
+			amountValue: '2300',
+			validateAmountURL: 'https://example.com/amount-check',
+		};
+
+		expect( store.dispatch ).toBeCalledWith( expectedAction, expectedPayload );
+	} );
+
+	it( 'converts custom amounts with decimal point to cent amounts', () => {
+		const wrapper = mount( PaymentAmount, {
+			propsData: {
+				paymentAmounts: [ 5 ],
+				validateAmountURL: 'https://example.com/amount-check',
+			},
+			store: createStore(),
+			mocks: {
+				$t: () => {},
+			},
+		} );
+		const store = wrapper.vm.$store;
+		store.dispatch = jest.fn();
+
+		const customAmountInput = wrapper.find( '#amount-custom' );
+		customAmountInput.setValue( '12.34' );
+		customAmountInput.trigger( 'blur' );
+		const expectedAction = action( NS_PAYMENT, setAmount );
+		const expectedPayload = {
+			amountValue: '1234',
+			validateAmountURL: 'https://example.com/amount-check',
+		};
+
+		expect( store.dispatch ).toBeCalledWith( expectedAction, expectedPayload );
+	} );
+
+	it( 'converts custom amounts with comma to cent amounts', () => {
+		const wrapper = mount( PaymentAmount, {
+			propsData: {
+				paymentAmounts: [ 5 ],
+				validateAmountURL: 'https://example.com/amount-check',
+			},
+			store: createStore(),
+			mocks: {
+				$t: () => {},
+			},
+		} );
+		const store = wrapper.vm.$store;
+		store.dispatch = jest.fn();
+
+		const customAmountInput = wrapper.find( '#amount-custom' );
+		customAmountInput.setValue( '23,42' );
+		customAmountInput.trigger( 'blur' );
+		const expectedAction = action( NS_PAYMENT, setAmount );
+		const expectedPayload = {
+			amountValue: '2342',
+			validateAmountURL: 'https://example.com/amount-check',
+		};
+
+		expect( store.dispatch ).toBeCalledWith( expectedAction, expectedPayload );
+	} );
+
+	it( 'cuts off cent fractions from custom amounts', () => {
+		const wrapper = mount( PaymentAmount, {
+			propsData: {
+				paymentAmounts: [ 5 ],
+				validateAmountURL: 'https://example.com/amount-check',
+			},
+			store: createStore(),
+			mocks: {
+				$t: () => {},
+			},
+		} );
+		const store = wrapper.vm.$store;
+		store.dispatch = jest.fn();
+
+		const customAmountInput = wrapper.find( '#amount-custom' );
+		customAmountInput.setValue( '23,429' );
+		customAmountInput.trigger( 'blur' );
+		const expectedAction = action( NS_PAYMENT, setAmount );
+		const expectedPayload = {
+			amountValue: '2342',
+			validateAmountURL: 'https://example.com/amount-check',
+		};
+
+		expect( store.dispatch ).toBeCalledWith( expectedAction, expectedPayload );
+	} );
+
+	it( 'sends empty string to store when custom amount is invalid', () => {
+		const wrapper = mount( PaymentAmount, {
+			propsData: {
+				paymentAmounts: [ 5 ],
+				validateAmountURL: 'https://example.com/amount-check',
+			},
+			store: createStore(),
+			mocks: {
+				$t: () => {},
+			},
+		} );
+		const store = wrapper.vm.$store;
+		store.dispatch = jest.fn();
+
+		const customAmountInput = wrapper.find( '#amount-custom' );
+		customAmountInput.setValue( 'hi mom!' );
+		customAmountInput.trigger( 'blur' );
+		const expectedAction = action( NS_PAYMENT, setAmount );
+		const expectedPayload = {
+			amountValue: '',
+			validateAmountURL: 'https://example.com/amount-check',
+		};
+
+		expect( store.dispatch ).toBeCalledWith( expectedAction, expectedPayload );
+	} );
+
+	it( 'does not send empty custom amount', () => {
+		const wrapper = mount( PaymentAmount, {
+			store: createStore(),
+			mocks: {
+				$t: () => {},
+			},
+		} );
+		const store = wrapper.vm.$store;
+		store.dispatch = jest.fn();
+
+		const customAmountInput = wrapper.find( '#amount-custom' );
+		customAmountInput.setValue( '' );
+		customAmountInput.trigger( 'blur' );
+		const forbiddenAction = action( NS_PAYMENT, setAmount );
+
+		expect( store.dispatch ).not.toBeCalledWith( forbiddenAction, expect.anything() );
+	} );
+
+	it( 'triggers an amount check in the store when custom value is empty', () => {
+		const wrapper = mount( PaymentAmount, {
+			store: createStore(),
+			mocks: {
+				$t: () => {},
+			},
+		} );
+		const store = wrapper.vm.$store;
+		store.dispatch = jest.fn();
+
+		const customAmountInput = wrapper.find( '#amount-custom' );
+		customAmountInput.setValue( '' );
+		customAmountInput.trigger( 'blur' );
+		const expectedAction = action( NS_PAYMENT, markEmptyAmountAsInvalid );
+
+		expect( store.dispatch ).toBeCalledWith( expectedAction );
+	} );
+
+	it( 'clears selected amount when custom amount is entered', () => {
+		const wrapper = mount( PaymentAmount, {
+			propsData: {
+				paymentAmounts: [ 5, 10, 100, 299 ],
+				validateAmountURL: 'https://example.com/amount-check',
+			},
+			store: createStore(),
+			mocks: {
+				$t: () => {},
+			},
+		} );
+		const store = wrapper.vm.$store;
+		store.dispatch = jest.fn();
+
+		const presetAmount = wrapper.find( '#amount-29900' );
+		const customAmountInput = wrapper.find( '#amount-custom' );
+		presetAmount.trigger( 'click' );
+		customAmountInput.setValue( '1998' );
+		customAmountInput.trigger( 'blur' );
+
+		expect( wrapper.vm.$data.amountValue ).toBe( '' );
 	} );
 
 } );
