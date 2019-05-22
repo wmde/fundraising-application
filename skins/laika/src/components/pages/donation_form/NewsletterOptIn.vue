c520731f00869ea1f29ca0d2c00d552136c086b5
tzhelyazkova <tonina.zhelyazkova@wikimedia.de>
2019-05-22 16:07:52 +0200

    Add newsletter opt in component

---
+++ skins/laika/src/components/pages/donation_form/NewsletterOptIn.vue
@@ -0,0 +1,32 @@
+<template>
+     <div>
+        <b-checkbox type="checkbox" class="subtitle has-margin-top-18"
+            id="newsletter"
+            name="newsletter"
+            v-model="newsletterOptIn"
+            @click="setNewsletterOptIn()">
+        </b-checkbox>
+        <label for="newsletter">{{ $t( 'donation_sendinfo_label' ) }}</label>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { NS_ADDRESS } from '@/store/namespaces';
+import { action } from '@/store/util';
+import { setNewsletterOptIn } from '@/store/address/actionTypes';
+
+export default Vue.extend( {
+	name: 'NewsletterOptIn',
+	data: function () {
+		return {
+			newsletterOptIn: false,
+		};
+	},
+	methods: {
+		setNewsletterOptIn: function () {
+			this.$store.dispatch( action( NS_ADDRESS, setNewsletterOptIn ), this.$data.newsletterOptIn );
+		},
+	},
+} );
+</script>
