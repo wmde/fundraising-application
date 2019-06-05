999fe33ce709acd2fbe52e686c781ae7ae0175fa
Tim Eulitz <tim.eulitz@wikimedia.de>
2019-06-05 16:30:44 +0200

    Implement minor confirmation page design details

--- skins/laika/src/components/layout/Content.vue
+++ skins/laika/src/components/layout/Content.vue
@@ -1,11 +1,9 @@
 <template>
-	<div class="container">
-		<div class="columns has-margin-top-0">
-			<div v-bind:class="[isFullWidth ? 'is-full' : 'is-two-thirds column has-background-bright']">
-				<slot></slot>
-			</div>
-			<slot name="sidebar"></slot>
+	<div class="columns has-margin-top-0">
+		<div v-bind:class="[isFullWidth ? 'is-full' : 'is-two-thirds column has-background-bright']">
+			<slot></slot>
 		</div>
+		<slot name="sidebar"></slot>
 	</div>
 </template>
 
