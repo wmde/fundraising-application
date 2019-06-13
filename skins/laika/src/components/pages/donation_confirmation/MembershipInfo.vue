8a52526cae8b67a78f2a33c8d52cffb53c474e82
timEulitz <tim.eulitz@wikimedia.de>
2019-06-13 09:44:20 +0200

    Add membership benefits to confirmation page
    https://phabricator.wikimedia.org/T223061

--- skins/laika/src/components/pages/donation_confirmation/MembershipInfo.vue
+++ skins/laika/src/components/pages/donation_confirmation/MembershipInfo.vue
@@ -3,9 +3,19 @@
         <div class="column is-half">
             <h2 class="title is-size-5">{{ $t( 'donation_confirmation_membership_call_to_action_title' ) }}</h2>
             <p>{{ $t( 'donation_confirmation_membership_call_to_action_text' ) }}</p>
-            <p><b-button type="is-primary is-main">{{ $t('donation_confirmation_membership_button') }}</b-button></p>
+            <p>
+                <a href="#">
+                    <b-button type="is-primary is-main has-margin-top-36">{{ $t('donation_confirmation_membership_button') }}</b-button>
+                </a>
+            </p>
         </div>
-        <div class="column is-half">TODO</div>
+        <div class="column is-half">
+            <p>{{ $t( 'donation_confirmation_bottombox_membership_benefit_1' ) }}</p>
+            <p class="has-margin-top-18">{{ $t( 'donation_confirmation_bottombox_membership_benefit_2' ) }}</p>
+            <p class="has-margin-top-18">{{ $t( 'donation_confirmation_bottombox_membership_benefit_3' ) }}</p>
+            <p class="has-margin-top-18">{{ $t( 'donation_confirmation_bottombox_membership_benefit_4' ) }}</p>
+            <p class="has-margin-top-18"><a href="https://wikimedia.de/de/mitglied-werden">{{ $t( 'donation_confirmation_bottombox_membership_link' ) }}</a></p>
+        </div>
 
     </div>
 </template>
