7ea99bbab5063bd114bf20bafe29a8e9b44a2668
tzhelyazkova <tonina.zhelyazkova@wikimedia.de>
2019-07-02 17:17:40 +0200

    Add address components to membership address page

---
+++ skins/laika/src/components/pages/membership_form/Address.vue
@@ -0,0 +1,154 @@
+<template>
+	<div id="addressForm" class="column is-full">
+		<div class="has-margin-top-18">
+			<h1 class="title is-size-1">{{ $t( 'donation_form_section_address_title' ) }}</h1>
+			<address-type></address-type>
+		</div>
+		<name :show-error="fieldErrors" :form-data="formData" :address-type="addressType" v-on:field-changed="onFieldChange"></name>
+		<postal :show-error="fieldErrors" :form-data="formData" :countries="countries" v-on:field-changed="onFieldChange"></postal>
+		<date-of-birth></date-of-birth>
+        <receipt-opt-out></receipt-opt-out>
+        <div class="has-margin-top-36">
+			<h1 class="title is-size-1">{{ $t( 'donation_form_section_email_title' ) }}</h1>
+			<email></email>
+		</div>
+		<newsletter-opt-in></newsletter-opt-in>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { mapGetters } from 'vuex';
+import AddressType from '@/components/pages/donation_form/AddressType.vue';
+import Name from '@/components/pages/donation_form/Name.vue';
+import Postal from '@/components/pages/donation_form/Postal.vue';
+import DateOfBirth from '@/components/pages/donation_form/DateOfBirth.vue';
+import ReceiptOptOut from '@/components/pages/donation_form/ReceiptOptOut.vue';
+import Email from '@/components/pages/donation_form/Email.vue';
+import NewsletterOptIn from '@/components/pages/donation_form/NewsletterOptIn.vue';
+import { AddressValidity, FormData, ValidationResult } from '@/view_models/Address';
+import { Validity } from '@/view_models/Validity';
+import { NS_ADDRESS } from '@/store/namespaces';
+import { setAddressField, validateAddress } from '@/store/address/actionTypes';
+import { action } from '@/store/util';
+
+export default Vue.extend( {
+	name: 'Address',
+	components: {
+		Name,
+        Postal,
+        DateOfBirth,
+        ReceiptOptOut,
+		AddressType,
+		Email,
+		NewsletterOptIn,
+	},
+	data: function (): { formData: FormData } {
+		return {
+			formData: {
+				salutation: {
+					name: 'salutation',
+					value: '',
+					pattern: '^(Herr|Frau)$',
+					optionalField: false,
+				},
+				title: {
+					name: 'title',
+					value: '',
+					pattern: '',
+					optionalField: true,
+				},
+				companyName: {
+					name: 'companyName',
+					value: '',
+					pattern: '^.+$',
+					optionalField: false,
+				},
+				firstName: {
+					name: 'firstName',
+					value: '',
+					pattern: '^.+$',
+					optionalField: false,
+				},
+				lastName: {
+					name: 'lastName',
+					value: '',
+					pattern: '^.+$',
+					optionalField: false,
+				},
+				street: {
+					name: 'street',
+					value: '',
+					pattern: '^.+$',
+					optionalField: false,
+				},
+				city: {
+					name: 'city',
+					value: '',
+					pattern: '^.+$',
+					optionalField: false,
+				},
+				postcode: {
+					name: 'postcode',
+					value: '',
+					pattern: '^[0-9]{4,5}$',
+					optionalField: false,
+				},
+				country: {
+					name: 'country',
+					value: 'DE',
+					pattern: '',
+					optionalField: false,
+				},
+			},
+		};
+	},
+	props: {
+		validateAddressUrl: String,
+		countries: Array as () => Array<String>,
+	},
+	computed: {
+		fieldErrors: {
+			get: function (): AddressValidity {
+                /*uncomment when membership store is developed
+				return Object.keys( this.formData ).reduce( ( validity: AddressValidity, fieldName: string ) => {
+					if ( !this.formData[ fieldName ].optionalField ) {
+						validity[ fieldName ] = this.$store.state.address.validity[ fieldName ] === Validity.INVALID;
+					}
+					return validity;
+                }, ( {} as AddressValidity ) );
+                */
+			},
+        },
+        ...mapGetters( NS_ADDRESS, [
+			'addressType',
+		] ),
+	},
+	methods: {
+        /* uncomment when membership store is developed
+		validateForm(): Promise<ValidationResult> {
+			return this.$store.dispatch( action( NS_ADDRESS, validateAddress ), this.$props.validateAddressUrl );
+		},
+		onFieldChange( fieldName: string ): void {
+			//this.$store.dispatch( action( NS_ADDRESS, setAddressField ), this.formData[ fieldName ] );
+        },
+        */
+	},
+} );
+</script>
+<style lang="scss" scoped>
+    @import "../../../scss/custom";
+
+    button.is-main {
+        height: 54px;
+        font-size: 1em;
+        font-weight: bold;
+        width: 250px;
+        border-radius: 0;
+    }
+    @include until($tablet) {
+        button.is-main {
+            width: 100%;
+        }
+    }
+</style>
--- skins/laika/src/components/pages/membership_form/subpages/AddressPage.vue
+++ skins/laika/src/components/pages/membership_form/subpages/AddressPage.vue
@@ -1,17 +1,26 @@
 <template>
     <div class="column is-full">
         <membership-type></membership-type>
+		<address-fields v-bind="$props" ref="address"></address-fields>
     </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import MembershipType from '@/components/pages/membership_form//MembershipType.vue';
+import AddressFields from '@/components/pages/membership_form/Address.vue';
+import { TrackingData } from '@/view_models/SubmitValues';
 
 export default Vue.extend( {
 	name: 'AddressPage',
 	components: {
 		MembershipType,
+		AddressFields
+	},
+	props: {
+		validateAddressUrl: String,
+		countries: Array as () => Array<String>,
+		trackingData: Object as () => TrackingData,
 	},
 } );
 </script>
