81aae22a38985ef9fb636f347d80e22614dc7995
tzhelyazkova <tonina.zhelyazkova@wikimedia.de>
2019-07-02 11:53:29 +0200

    Connect membership type to membership form page

--- skins/laika/src/components/pages/membership_form/subpages/AddressPage.vue
+++ skins/laika/src/components/pages/membership_form/subpages/AddressPage.vue
@@ -1,17 +1,17 @@
 <template>
-    <div>
-
+    <div class="column is-full">
+        <membership-type></membership-type>
     </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import MembershipType from '@/components/pages/membership_form//MembershipType.vue';
 
 export default Vue.extend( {
-
+    name: 'AddressPage',
+	components: {
+		MembershipType,
+	},
 } );
 </script>
-
-<style scoped>
-
-</style>
