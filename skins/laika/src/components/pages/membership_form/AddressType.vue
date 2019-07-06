f1453970fbc680ce10512b1449837bad878265ae
timEulitz <tim.eulitz@wikimedia.de>
2019-07-06 22:38:04 +0200

    Fix membership application section design

--- skins/laika/src/components/pages/membership_form/AddressType.vue
+++ skins/laika/src/components/pages/membership_form/AddressType.vue
@@ -1,6 +1,6 @@
 <template>
 	<fieldset>
-		<legend class="subtitle">{{ $t( 'membership_section_address_header_type' ) }}</legend>
+		<legend class="title is-size-5">{{ $t( 'membership_section_address_header_type' ) }}</legend>
 		<div>
 			<b-radio type="radio"
 					id="personal"
