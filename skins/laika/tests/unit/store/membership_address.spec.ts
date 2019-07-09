606ba032df6cb6ef760e074da853e51dac77be49
tzhelyazkova <tonina.zhelyazkova@wikimedia.de>
2019-07-09 17:16:54 +0200

    Add membership address store actions unit tests

--- skins/laika/tests/unit/store/membership_address.spec.ts
+++ skins/laika/tests/unit/store/membership_address.spec.ts
@@ -23,7 +23,7 @@ import {
 	SET_RECEIPT_OPTOUT,
 	SET_MEMBERSHIP_TYPE,
 } from '@/store/membership_address/mutationTypes';
-import { AddressTypeModel } from '@/view_models/AddressTypeModel';
+import { AddressTypeModel, addressTypeName } from '@/view_models/AddressTypeModel';
 import { MembershipTypeModel } from '@/view_models/MembershipTypeModel';
 import { MembershipAddressState } from '@/view_models/Address';
 import { Validity } from '@/view_models/Validity';
@@ -202,6 +202,207 @@ describe( 'MembershipAddress', () => {
 				null,
 				null
 			) ).toBe( 'Testina Testingson' );
+		} );
+	} );
+
+	describe( 'Actions/setAddressField', () => {
+		it( 'commits to mutation [SET_ADDRESS_FIELD] and [VALIDATE_INPUT] with the correct field', () => {
+			const commit = jest.fn(),
+				action = actions.setAddressField as any,
+				field = {
+					name: 'postcode',
+					value: '',
+					pattern: '^[0-9]{4,5}$',
+					optionalField: false,
+				};
+			action( { commit }, field );
+			expect( commit ).toBeCalledWith(
+				'SET_ADDRESS_FIELD',
+				field
+			);
+			expect( commit ).toBeCalledWith(
+				'VALIDATE_INPUT',
+				field
+			);
+		} );
+	} );
+
+	describe( 'Actions/validateAddress', () => {
+		beforeEach( function () {
+			moxios.install();
+		} );
+
+		afterEach( function () {
+			moxios.uninstall();
+		} );
+		it( 'commits to mutation [MARK_EMPTY_FIELDS_INVALID] and [BEGIN_ADDRESS_VALIDATION]', () => {
+			const context = {
+					commit: jest.fn(),
+					getters: {
+						requiredFieldsAreValid: true,
+					},
+					state: newMinimalStore( {
+						addressType: AddressTypeModel.PERSON,
+						values: {
+							salutation: 'Mrs',
+							title: 'Pro. Dr.',
+							firstName: 'Testina',
+							lastName: 'Testington',
+							street: 'Testenhofen Ufer 23-24',
+							postcode: '12345',
+							city: 'Testlin',
+							country: 'DE',
+							email: 'test@testmedia.de',
+							date: '01.01.1942',
+						},
+					} ),
+				},
+				validationUrl = '/check-address',
+				action = actions.validateAddress as any;
+			action( context, validationUrl );
+			expect( context.commit ).toBeCalledWith(
+				'MARK_EMPTY_FIELDS_INVALID'
+			);
+			expect( context.commit ).toBeCalledWith(
+				'BEGIN_ADDRESS_VALIDATION'
+			);
+		} );
+
+		it( 'sends post request for validation when required fields are valid and commits to mutation [FINISH_ADDRESS_VALIDATION]', ( done ) => {
+			const context = {
+					commit: jest.fn(),
+					getters: {
+						requiredFieldsAreValid: true,
+					},
+					state: newMinimalStore( {
+						addressType: AddressTypeModel.PERSON,
+						values: {
+							salutation: 'Mrs',
+							title: 'Pro. Dr.',
+							firstName: 'Testina',
+							lastName: 'Testington',
+							street: 'Testenhofen Ufer 23-24',
+							postcode: '12345',
+							city: 'Testlin',
+							country: 'DE',
+							email: 'test@testmedia.de',
+							date: '01.01.1942',
+						},
+					} ),
+				},
+				validationUrl = '/check-address',
+				action = actions.validateAddress as any;
+
+			action( context, validationUrl );
+			moxios.wait( function () {
+				let request = moxios.requests.mostRecent();
+				request.respondWith( {
+					status: 200,
+					response: {
+						status: 'OK',
+					} as any,
+				} ).then( function () {
+					expect( context.commit ).toHaveBeenCalledWith( 'FINISH_ADDRESS_VALIDATION', {
+						status: 'OK',
+					} );
+					done();
+				} );
+			} );
+		} );
+
+		it( 'does not send a post request when required fields are invalid and returns an error', ( done ) => {
+			const context = {
+					commit: jest.fn(),
+					getters: {
+						requiredFieldsAreValid: false,
+					},
+					state: newMinimalStore( {
+						addressType: AddressTypeModel.PERSON,
+						values: {
+							salutation: '',
+							title: '',
+							firstName: '',
+							lastName: '',
+							street: '',
+							postcode: 'I AM DEFINITELY INVALID',
+							city: '',
+							country: '',
+							email: '',
+							date: '',
+						},
+					} ),
+				},
+				validationUrl = '/check-address',
+				action = actions.validateAddress as any;
+			action( context, validationUrl ).then( function ( resp: any ) {
+				expect( resp ).toStrictEqual( { status: 'ERR', messages: [] } );
+				done();
+			} );
+		} );
+	} );
+
+	describe( 'Actions/setAddressType', () => {
+		it( 'commits to mutation [SET_ADDRESS_TYPE] with the chosen type', () => {
+			const commit = jest.fn(),
+				action = actions.setAddressType as any,
+				type = AddressTypeModel.COMPANY;
+			action( { commit }, type );
+			expect( commit ).toBeCalledWith(
+				'SET_ADDRESS_TYPE',
+				type
+			);
+		} );
+	} );
+
+	describe( 'Actions/setEmail', () => {
+		it( 'commits to mutation [SET_EMAIL] with the entered email', () => {
+			const commit = jest.fn(),
+				action = actions.setEmail as any,
+				email = 'email@email.com';
+			action( { commit }, email );
+			expect( commit ).toBeCalledWith(
+				'SET_EMAIL',
+				email
+			);
+		} );
+	} );
+
+	describe( 'Actions/setDate', () => {
+		it( 'commits to mutation [SET_DATE] with the entered birth date', () => {
+			const commit = jest.fn(),
+				action = actions.setDate as any,
+				date = '19.19.1919';
+			action( { commit }, date );
+			expect( commit ).toBeCalledWith(
+				'SET_DATE',
+				date
+			);
+		} );
+	} );
+
+	describe( 'Actions/setReceiptOptOut', () => {
+		it( 'commits to mutation [SET_RECEIPT_OPTOUT] with the entered choice', () => {
+			const commit = jest.fn(),
+				action = actions.setReceiptOptOut as any,
+				choice = true;
+			action( { commit }, choice );
+			expect( commit ).toBeCalledWith(
+				'SET_RECEIPT_OPTOUT',
+				choice
+			);
+		} );
+	} );
+
+	describe( 'Actions/setMembershipType', () => {
+		it( 'commits to mutation [SET_MEMBERSHIP_TYPE] with the chosen membership type', () => {
+			const commit = jest.fn(),
+				action = actions.setMembershipType as any,
+				choice = MembershipTypeModel.ACTIVE;
+			action( { commit }, choice );
+			expect( commit ).toBeCalledWith(
+				'SET_MEMBERSHIP_TYPE',
+				choice
+			);
 		} );
 	} );
 
