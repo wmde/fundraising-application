77cfd5115160b194975a51a8b423cafdb63666f8
tzhelyazkova <tonina.zhelyazkova@wikimedia.de>
2019-07-10 23:05:12 +0200

    Assert events submit the right data

--- skins/laika/tests/unit/components/pages/donation_form/AddressType.spec.ts
+++ skins/laika/tests/unit/components/pages/donation_form/AddressType.spec.ts
@@ -1,6 +1,7 @@
 import { mount, createLocalVue } from '@vue/test-utils';
 import AddressType from '@/components/pages/donation_form/AddressType.vue';
 import Buefy from 'buefy';
+import { AddressTypeModel } from '@/view_models/AddressTypeModel';
 
 const localVue = createLocalVue();
 localVue.use( Buefy );
@@ -16,11 +17,14 @@ describe( 'AddressType.vue', () => {
 			} ),
 			event = 'address-type',
 			company = wrapper.find( '#company' );
-		company.trigger( 'change' );
+		company.trigger( 'click' );
 		const person = wrapper.find( '#personal' );
-		person.trigger( 'change' );
+		person.trigger( 'click' );
 		const anon = wrapper.find( '#anonymous' );
-		anon.trigger( 'change' );
+		anon.trigger( 'click' );
 		expect( wrapper.emitted( event ) ).toHaveLength( 3 );
+		expect( wrapper.emitted( event )[ 0 ] ).toEqual( [ AddressTypeModel.COMPANY ] );
+		expect( wrapper.emitted( event )[ 1 ] ).toEqual( [ AddressTypeModel.PERSON ] );
+		expect( wrapper.emitted( event )[ 2 ] ).toEqual( [ AddressTypeModel.ANON ] );
 	} );
 } );
--- skins/laika/tests/unit/components/pages/membership_form/AddressType.spec.ts
+++ skins/laika/tests/unit/components/pages/membership_form/AddressType.spec.ts
@@ -1,5 +1,6 @@
 import { mount, createLocalVue } from '@vue/test-utils';
 import AddressType from '@/components/pages/membership_form/AddressType.vue';
+import { AddressTypeModel } from '@/view_models/AddressTypeModel';
 import Buefy from 'buefy';
 
 const localVue = createLocalVue();
@@ -16,9 +17,11 @@ describe( 'AddressType.vue', () => {
 			} ),
 			event = 'address-type',
 			company = wrapper.find( '#company' );
-		company.trigger( 'change' );
+		company.trigger( 'click' );
 		const person = wrapper.find( '#personal' );
-		person.trigger( 'change' );
+		person.trigger( 'click' );
 		expect( wrapper.emitted( event ) ).toHaveLength( 2 );
+		expect( wrapper.emitted( event )[ 0 ] ).toEqual( [ AddressTypeModel.COMPANY ] );
+		expect( wrapper.emitted( event )[ 1 ] ).toEqual( [ AddressTypeModel.PERSON ] );
 	} );
 } );
