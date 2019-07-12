b466a6c3a8318d9844a6cea6aca57cf2de161d47
tzhelyazkova <tonina.zhelyazkova@wikimedia.de>
2019-07-12 14:17:19 +0200

    Preset email and address type if the data exists on render

--- skins/laika/src/view_models/AddressTypeModel.ts
+++ skins/laika/src/view_models/AddressTypeModel.ts
@@ -10,6 +10,12 @@ export const AddressTypeNames = new Map<number, string>( [
 	[ AddressTypeModel.COMPANY, 'firma' ],
 ] );
 
+export const AddressTypes = new Map<string, number>( [
+	[ 'anonym', AddressTypeModel.ANON ],
+	[ 'person', AddressTypeModel.PERSON ],
+	[ 'firma', AddressTypeModel.COMPANY ],
+] );
+
 export function addressTypeName( t: AddressTypeModel ): string {
 	const name = AddressTypeNames.get( t );
 	// poor man's type check to protect against future extensions of AddressTypeModel, e.g. https://phabricator.wikimedia.org/T220367
@@ -17,4 +23,13 @@ export function addressTypeName( t: AddressTypeModel ): string {
 		throw new Error( 'Unknown address type: ' + t );
 	}
 	return name;
+}
+
+export function addressTypeFromName( n: string ): AddressTypeModel {
+	const type = AddressTypes.get( n );
+	// poor man's type check to protect against future extensions of AddressTypeModel, e.g. https://phabricator.wikimedia.org/T220367
+	if ( typeof type === 'undefined' ) {
+		throw new Error( 'Unknown address type: ' + n );
+	}
+	return type;
 }
