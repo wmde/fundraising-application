a2f4fe801d58e3ecba8b730726f986bf688465f2
Tim Eulitz <tim.eulitz@wikimedia.de>
2019-07-30 17:52:58 +0200

    Implement donation / membership cancellation page

---
+++ skins/laika/src/components/pages/DonationCancellation.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="column is-full">
+		<h1 class="title">{{ $t('donation_cancellation_title') }}</h1>
+		<span v-html="$t( 'donation_cancellation_text', { donation_id: cancellationData.donationId, } )"></span>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend( {
+	name: 'DonationCancellation',
+	props: [
+		'cancellationData',
+	],
+} );
+</script>
+
+<style lang="scss">
+	@import "../../scss/custom";
+
+</style>
---
+++ skins/laika/src/components/pages/MembershipCancellation.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="column is-full">
+		<h1 class="title">{{ $t('membership_cancellation_title') }}</h1>
+		<span v-html="$t( 'membership_cancellation_text', { membership_id: cancellationData.membershipId, } )"></span>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend( {
+	name: 'DonationCancellation',
+	props: [
+		'cancellationData',
+	],
+} );
+</script>
+
+<style lang="scss">
+	@import "../../scss/custom";
+
+</style>
