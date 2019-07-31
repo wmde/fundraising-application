5c23232f79dd34c7ded40af39448b420ca43ad24
tzhelyazkova <tonina.zhelyazkova@wikimedia.de>
2019-07-31 11:44:17 +0200

    Fix linting issues

--- skins/laika/src/components/pages/PrivacyProtection.vue
+++ skins/laika/src/components/pages/PrivacyProtection.vue
@@ -1,26 +1,26 @@
 <template>
-    <div class="column is-full privacy-selection has-padding-36">
-        <h2 class="title is-size-2">{{ $t( 'privacy_protection_title' ) }}</h2>
-        <p class="legend">
-            {{ $t('privacy_optout_description') }}
-        </p>
-            <b-radio id="tracking-opt-in"
-                    name="matomo_choice"
-                    :native-value="0"
-                    v-model="optOut"
-                    @input="changeTracking">
-                {{ $t('privacy_optout_tracking_permit') }}
-            </b-radio>
-            <b-radio id="tracking-opt-out"
-                    name="matomo_choice"
-                    :native-value="1"
-                    v-model="optOut"
-                    @input="changeTracking">
-                {{ $t('privacy_optout_tracking_deny') }}
-            </b-radio>
-            <p v-if="optOut === 0" class="has-text-dark-lighter has-margin-top-18">{{ $t( 'privacy_optout_tracking_state' ) }}</p>
-            <p v-else class="has-text-dark-lighter has-margin-top-18" v-html="$t( 'privacy_optout_tracking_state_no' )"></p>
-    </div>
+	<div class="column is-full privacy-selection has-padding-36">
+		<h2 class="title is-size-2">{{ $t( 'privacy_protection_title' ) }}</h2>
+		<p class="legend">
+			{{ $t('privacy_optout_description') }}
+		</p>
+			<b-radio id="tracking-opt-in"
+					name="matomo_choice"
+					:native-value="0"
+					v-model="optOut"
+					@input="changeTracking">
+				{{ $t('privacy_optout_tracking_permit') }}
+			</b-radio>
+			<b-radio id="tracking-opt-out"
+					name="matomo_choice"
+					:native-value="1"
+					v-model="optOut"
+					@input="changeTracking">
+				{{ $t('privacy_optout_tracking_deny') }}
+			</b-radio>
+			<p v-if="optOut === 0" class="has-text-dark-lighter has-margin-top-18">{{ $t( 'privacy_optout_tracking_state' ) }}</p>
+			<p v-else class="has-text-dark-lighter has-margin-top-18" v-html="$t( 'privacy_optout_tracking_state_no' )"></p>
+	</div>
 </template>
 
 <script lang="ts">
@@ -34,10 +34,10 @@ export default Vue.extend( {
 		return {
 			optOut: 0,
 		};
-    },
-    beforeMount: function() {
-       this.getInitialTrackingState();
-    },
+	},
+	beforeMount: function () {
+		this.getInitialTrackingState();
+	},
 	methods: {
 		changeTracking: function (): void {
 			if ( this.$data.optOut === 0 ) {
@@ -51,27 +51,27 @@ export default Vue.extend( {
 					function () {}
 				);
 			}
-        },
-        getInitialTrackingState: function(): void {
-            jsonp( TRACKING_URL + 'index.php?module=API&method=AjaxOptOut.isTracked&format=json',
-					undefined,
-					( error, data ) => {
-                        this.$data.optOut = data.value ? 0 : 1;
-                    }
-				);
-        },
+		},
+		getInitialTrackingState: function (): void {
+			jsonp( TRACKING_URL + 'index.php?module=API&method=AjaxOptOut.isTracked&format=json',
+				undefined,
+				( error, data ) => {
+					this.$data.optOut = data.value ? 0 : 1;
+				}
+			);
+		},
 	},
 } );
 </script>
 <style lang="scss" scoped>
 @import "../../scss/custom.scss";
 	.b-radio.radio {
-		&+.radio{
-            margin-left: 0;
+		&+.radio {
+			margin-left: 0;
 		}
-    }
-    .privacy-selection {
-        border: 1px solid $fun-color-gray-mid;
+	}
+	.privacy-selection {
+		border: 1px solid $fun-color-gray-mid;
 		border-radius: 2px;
-    }
+	}
 </style>
