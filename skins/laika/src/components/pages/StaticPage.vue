0bf841d39084b5cdd707638237c18efeafcd0a61
Tim Eulitz <tim.eulitz@wikimedia.de>
2019-08-12 16:39:57 +0200

    Remove column class from static text / rename CSS

--- skins/laika/src/components/pages/StaticPage.vue
+++ skins/laika/src/components/pages/StaticPage.vue
@@ -1,9 +1,9 @@
 <template>
 <div class="column is-full">
 	<h2 class="title is-size-2">{{ pageTitle }}</h2>
-	<div v-html="partialContentFirstHalf" class="has-margin-top-18 column is-full privacy"></div>
+	<div v-html="partialContentFirstHalf" class="has-margin-top-18 is-full static-content"></div>
 	<privacy-protection v-if="pageId === 'privacy_protection'"></privacy-protection>
-	<div v-html="partialContentSecondHalf" class="has-margin-top-18 column is-full privacy"></div>
+	<div v-html="partialContentSecondHalf" class="has-margin-top-18 is-full static-content"></div>
 </div>
 </template>
 
@@ -30,3 +30,19 @@ export default Vue.extend( {
 	},
 } );
 </script>
+
+<style lang="scss">
+	@import "../../scss/custom.scss";
+	.static-content {
+		ol {
+			margin-left: 1.5em;
+		}
+		& > ol {
+			padding-left: 10px;
+			list-style-type: upper-roman;
+		}
+		p {
+			margin: 0 0 11px;
+		}
+	}
+</style>
