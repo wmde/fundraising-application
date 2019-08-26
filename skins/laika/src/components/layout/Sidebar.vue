9edac9667fc429539bc2a81ef9fa7e1fac363854
Gabriel Birke <gabriel.birke@wikimedia.de>
2019-08-26 19:47:28 +0200

    Add contact form link
    This fixes https://phabricator.wikimedia.org/T230872

--- skins/laika/src/components/layout/Sidebar.vue
+++ skins/laika/src/components/layout/Sidebar.vue
@@ -3,8 +3,8 @@
 		<p>{{ $t('sidebar_data_encryption_notice') }}</p>
 		<br>
 		<p><strong>{{ $t('sidebar_getintouch_headline') }}</strong></p>
-		<p>{{ $t('sidebar_getintouch_text') }}</p>
-		<p>{{ $t('sidebar_getintouch_text_phone') }}</p>
+		<p v-html="$t('sidebar_getintouch_text')"></p>
+		<p class="has-margin-top-18">{{ $t('sidebar_getintouch_text_phone') }}</p>
 	</aside>
 </template>
 
@@ -13,14 +13,3 @@ export default {
 	name: 'Sidebar',
 };
 </script>
-
-<style lang="scss">
-	@import "../../scss/variables";
-	.phone-number {
-		color: $fun-color-dark;
-		cursor: default;
-		&:hover {
-			color: $fun-color-dark;
-		}
-	}
-</style>
