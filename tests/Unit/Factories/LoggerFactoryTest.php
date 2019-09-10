29f9e95e992fa608afe314fc539cdfb04e7b14f1
Gabriel Birke <gabriel.birke@wikimedia.de>
2019-09-10 16:30:48 +0200

    Remove GELF logging
    We no longer use Graylog and have switched all configuration files to
    file-based logging.

--- tests/Unit/Factories/LoggerFactoryTest.php
+++ tests/Unit/Factories/LoggerFactoryTest.php
@@ -5,7 +5,6 @@ declare( strict_types = 1 );
 namespace WMDE\Fundraising\Frontend\Tests\Unit\Factories;
 
 use Monolog\Handler\ErrorLogHandler;
-use Monolog\Handler\GelfHandler;
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 use org\bovigo\vfs\vfsStream;
@@ -38,19 +37,6 @@ class LoggerFactoryTest extends TestCase {
 		$this->assertInstanceOf( StreamHandler::class, $firstHandler );
 		$this->assertSame( Logger::ERROR, $firstHandler->getLevel() );
 		$this->assertSame( vfsStream::url( 'logs/error.log' ), $firstHandler->getUrl() );
-	}
-
-	public function testGivenGraylogConfiguration_itReturnsStreamHandler() {
-		$factory = new LoggerFactory( [
-			'method' => 'gelf',
-			'url' => 'example.com:12201',
-			'level' => 'NOTICE'
-		]);
-		$logger = $factory->getLogger();
-		$this->assertCount( 1, $logger->getHandlers() );
-		$firstHandler = $logger->getHandlers()[0]; /** @var GelfHandler $firstHandler */
-		$this->assertInstanceOf( GelfHandler::class, $firstHandler );
-		$this->assertSame( Logger::NOTICE, $firstHandler->getLevel() );
 	}
 
 	public function testGivenUnknownLogType_exeptionIsThrown() {
