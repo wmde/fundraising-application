bbdd0ecb96ccc1fd0d891d761e148860f4b11ff0
Gabriel Birke <gabriel.birke@wikimedia.de>
2019-09-24 19:11:25 +0200

    Remove GELF logging (#1634)
    We no longer use Graylog and have switched all configuration files to
    file-based logging.

--- src/Factories/LoggerFactory.php
+++ src/Factories/LoggerFactory.php
@@ -4,10 +4,7 @@ declare( strict_types = 1 );
 
 namespace WMDE\Fundraising\Frontend\Factories;
 
-use Gelf\Publisher;
-use Gelf\Transport\UdpTransport;
 use Monolog\Handler\ErrorLogHandler;
-use Monolog\Handler\GelfHandler;
 use Monolog\Handler\HandlerInterface;
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
@@ -19,7 +16,6 @@ class LoggerFactory {
 
 	private const TYPE_ERROR_LOG = 'error_log';
 	private const TYPE_FILE = 'file';
-	private const TYPE_GELF = 'gelf';
 
 	private $config;
 
@@ -38,14 +34,6 @@ class LoggerFactory {
 				return new ErrorLogHandler( ErrorLogHandler::OPERATING_SYSTEM, $this->config['level'] );
 			case self::TYPE_FILE:
 				return new StreamHandler( $this->config['url'], $this->config['level'] );
-			case self::TYPE_GELF:
-				$url = parse_url( $this->config['url'] );
-				return new GelfHandler(
-					new Publisher(
-						new UdpTransport( $url['host'], $url['port'] ?? UdpTransport::DEFAULT_PORT )
-					),
-					$this->config['level']
-				);
 			default:
 				throw new \InvalidArgumentException( 'Unknown logging method - ' . $this->config['method'] );
 		}
