543336b8753b4e5977ff43c73f5ab125cd5e50de
Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
2020-03-03 18:13:50 +0100

    Extract deprecated param logger to static function
    ... and remove deprecated class AmountParser.

--- tests/Fixtures/SucceedingEmailValidator.php
+++ tests/Fixtures/SucceedingEmailValidator.php
@@ -4,7 +4,7 @@ declare( strict_types = 1 );
 
 namespace WMDE\Fundraising\Frontend\Tests\Fixtures;
 
-use WMDE\Fundraising\Frontend\Infrastructure\NullDomainNameValidator;
+use WMDE\Fundraising\Frontend\Infrastructure\Validation\NullDomainNameValidator;
 use WMDE\FunValidators\ValidationResult;
 use WMDE\FunValidators\Validators\EmailValidator;
 
