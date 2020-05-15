7900e158ee7e7d55420cf2cffe79d691c96f6c94
Gabriel Birke <gabriel.birke@gmail.com>
2020-05-15 19:01:10 +0200

    Add crypto key for user data
    Add master key to configuration
    Add UserDataKeyGenerator class
    Pass generated daily key to donation form
    
    This is for https://phabricator.wikimedia.org/T249803

---
+++ tests/Unit/Infrastructure/UserDataKeyGeneratorTest.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace WMDE\Fundraising\Frontend\Tests\Unit\Infrastructure;
+
+use WMDE\Clock\StubClock;
+use WMDE\Fundraising\Frontend\Infrastructure\UserDataKeyGenerator;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \WMDE\Fundraising\Frontend\Infrastructure\UserDataKeyGenerator
+ */
+class UserDataKeyGeneratorTest extends TestCase {
+
+	private string $masterKey;
+
+	protected function setUp(): void {
+		$this->masterKey = 'DiF8rUjXa2/pePFhQqB5ylDWfH/W5rlEXnrvf5/tNnk=';
+	}
+
+	public function testDailyKeyGeneratesSameKeyOnEachCall() {
+		$generator = new UserDataKeyGenerator( $this->masterKey, new StubClock( new \DateTimeImmutable( '2021-02-21' ) ) );
+
+		$this->assertSame( $generator->getDailyKey(), $generator->getDailyKey() );
+	}
+
+	public function testDailyKeyGeneratesSameKeyForEachDay() {
+		$generator1 = new UserDataKeyGenerator( $this->masterKey, new StubClock( new \DateTimeImmutable( '2021-02-20 12:21:00' ) ) );
+		$generator2 = new UserDataKeyGenerator( $this->masterKey, new StubClock( new \DateTimeImmutable( '2021-02-20 22:11:00' ) ) );
+
+		$this->assertSame( $generator1->getDailyKey(), $generator2->getDailyKey() );
+	}
+
+	public function testDailyKeyGeneratesDifferentKeyForEachDay() {
+		$generator1 = new UserDataKeyGenerator( $this->masterKey, new StubClock( new \DateTimeImmutable( '2021-02-21' ) ) );
+		$generator2 = new UserDataKeyGenerator( $this->masterKey, new StubClock( new \DateTimeImmutable( '2021-02-22' ) ) );
+
+		$this->assertNotSame( $generator1->getDailyKey(), $generator2->getDailyKey() );
+	}
+}
