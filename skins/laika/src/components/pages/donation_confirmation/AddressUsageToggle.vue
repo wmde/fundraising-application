562a5aaa3fe39bcbe01a032fa77ee40158ca2c18
Abban Dunne <abban.dunne@wikimedia.de>
2020-09-25 15:28:43 +0200

    Add address usage component

---
+++ skins/laika/src/components/pages/donation_confirmation/AddressUsageToggle.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="address-usage-toggle" :class="{ 'open': contentOpen }">
+		<a href="#" v-on:click="toggleContent" class="address-usage-toggle-link">{{ $t('donation_confirmation_address_usage_link') }}</a>
+		<div class="address-usage-toggle-content">{{ $t('donation_confirmation_address_usage_content') }}</div>
+    </div>
+</template>
+
+<script>
+import Vue from 'vue';
+
+export default Vue.extend( {
+	name: 'AddressUsageToggle',
+	data: function () {
+		return {
+			contentOpen: false,
+		};
+	},
+	methods: {
+		toggleContent: function () {
+			this.$data.contentOpen = !this.$data.contentOpen;
+		},
+	},
+} );
+</script>
