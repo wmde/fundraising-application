9b57346edb209bc13c2d135aa609d4b49b5e9cd5
Gabriel Birke <gabriel.birke@wikimedia.de>
2020-11-03 15:25:48 +0100

    Fix Twig cache creation (#1987)
    Fix error where we passed a boolean `true` instead of
    string|CacheInterface.
    
    Add test for cache creation.

---
+++ tests/Unit/Factories/TwigFactoryTestImplementation.php
@@ -0,0 +1,16 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace WMDE\Fundraising\Frontend\Tests\Unit\Factories;
+
+use WMDE\Fundraising\Frontend\Factories\TwigFactory;
+
+/**
+ * Implementation for testing common functionality
+ *
+ * @codeCoverageIgnore
+ */
+class TwigFactoryTestImplementation extends TwigFactory {
+
+}
