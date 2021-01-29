53b737ebdc36366162ede9899b47eabdfee38e99
Gabriel Birke <gabriel.birke@posteo.net>
2021-01-29 18:19:14 +0100

    Fix Redirects in web route test cases
    The request browser was throwing away the environment, causing tests
    with redirection to fail because the in-memory SQLite database was gone.

--- tests/EdgeToEdge/WebRouteTestCase.php
+++ tests/EdgeToEdge/WebRouteTestCase.php
@@ -83,6 +83,10 @@ abstract class WebRouteTestCase extends KernelTestCase {
 	 */
 	protected function createEnvironment( callable $onEnvironmentCreated ): void {
 		$client = static::createClient();
+		// Don't throw away the environment between http requests, otherwise the in-memory SQLite database would be gone
+		if ( $client instanceof KernelBrowser) {
+			$client->disableReboot();
+		}
 		call_user_func(
 			$onEnvironmentCreated,
 			$client,
