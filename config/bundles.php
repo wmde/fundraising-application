2e811e48dd7e3a3fe38fbc9eed6016a6a6f26af3
Gabriel Birke <gabriel.birke@posteo.net>
2021-02-02 15:27:45 +0100

    Modify types of expected values
    Symfony converts every request parameter into a string, so the expected
    values need to be strings.

--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,5 @@ declare( strict_types = 1 );
 
 return [
 	Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+	Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
