59bd7857730fae17279dc74c18caa88ef1b1f15a
Gabriel Birke <gabriel.birke@posteo.net>
2021-02-08 16:37:09 +0100

    Remove unnecessary proxy handling
    The page works without the proxy handling on the production server and
    in the dev environment.
    
    Should we ever need it back, we should configure it in the framework
    configuration file instead of using index.php. See
    https://symfony.com/doc/current/deployment/proxies.html

--- web/index.php
+++ web/index.php
@@ -20,17 +20,6 @@ if ( $debug ) {
 	Debug::enable();
 }
 
-if ( $trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false ) {
-	Request::setTrustedProxies(
-		explode( ',', $trustedProxies ),
-		Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST
-	);
-}
-
-if ( $trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false ) {
-	Request::setTrustedHosts( [ $trustedHosts ] );
-}
-
 $kernel = new Kernel( $env, $debug );
 $request = Request::createFromGlobals();
 $response = $kernel->handle( $request );
