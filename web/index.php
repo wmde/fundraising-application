50123d4381878d4007edc4fba12f3e2b4034cb0e
Gabriel Birke <gabriel.birke@posteo.net>
2021-02-08 15:31:18 +0100

    Read environment from .env in web entry point
    We don't use the recommended way (setting it from Apache/PHP-FPM config)
    to force the environment to be in sync on console and web and to be be
    independent from our managed hosting.

--- web/index.php
+++ web/index.php
@@ -2,11 +2,15 @@
 
 declare( strict_types = 1 );
 
+use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 use WMDE\Fundraising\Frontend\App\Kernel;
 
 require_once __DIR__ . '/../vendor/autoload.php';
+
+// We set the env from the .env file instead of web server settings because we want to be independent from the hosting provider
+( new Dotenv() )->bootEnv( __DIR__ . '/../.env' );
 
 $env = $_SERVER['APP_ENV'] ?? 'dev';
 $debug = $_SERVER['APP_DEBUG'] ? (bool)$_SERVER['APP_DEBUG'] : false;
