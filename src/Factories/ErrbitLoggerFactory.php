be91f890de4f569de8293fe2e3d69940fe2e3dfe
Gabriel Birke <gabriel.birke@posteo.net>
2021-02-19 17:08:06 +0100

    Improve logging configuration
    The Logger now uses a `fingers_crossed` handler for uat and production
    to avoid logging unneccessary information. When the handler triggers, it
    will log both to errbit and a local file. The local file will contain
    also all "info" level messages (can be useful to detect which route a
    request came from). Errbit will log only notice level and up (because
    it's our "alerting" and error aggregation system).
    
    Removed SupportHandler and unified ErrbitLoggerFactory: The Monolog
    FallbackGroupHandler does the same job as the SupportHandler and the
    monolog configuration is suited better to define nesting and log levels.

--- src/Factories/ErrbitLoggerFactory.php
+++ src/Factories/ErrbitLoggerFactory.php
@@ -5,11 +5,9 @@ namespace WMDE\Fundraising\Frontend\Factories;
 
 use Airbrake\MonologHandler as AirbrakeHandler;
 use Airbrake\Notifier;
-use Monolog\Handler\ErrorLogHandler;
 use Monolog\Handler\HandlerInterface;
 use Monolog\Logger;
 use Psr\Log\LogLevel;
-use WMDE\Fundraising\Frontend\Infrastructure\SupportHandler;
 
 class ErrbitLoggerFactory {
 	public static function createErrbitHandler( string $projectId, string $projectKey, string $host, string $environment = 'dev', ?string $level = LogLevel::DEBUG, bool $bubble = true ): HandlerInterface {
@@ -20,11 +18,7 @@ class ErrbitLoggerFactory {
 			'environment' => $environment
 		] );
 
-		// Wrap errbit handler in SupportHandler to avoid logception (logger trying to log logging errors) when errbit throws an error
-		return new SupportHandler(
-			new AirbrakeHandler( $notifier, Logger::toMonologLevel( $level ?? LogLevel::DEBUG ), $bubble ),
-			new Logger( 'errbit errors', [ new ErrorLogHandler( ErrorLogHandler::OPERATING_SYSTEM, LogLevel::ERROR ), ] ),
-		);
+		return new AirbrakeHandler( $notifier, Logger::toMonologLevel( $level ?? LogLevel::DEBUG ), $bubble );
 	}
 
 }
