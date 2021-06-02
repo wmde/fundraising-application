b2786555ca3344f29417604fceec5578c33bab5b
Abban Dunne <abban.dunne@wikimedia.de>
2021-06-02 11:34:27 +0200

    Add ErrorHandlingTemplateBasedMailer
    https://phabricator.wikimedia.org/T277023

--- tests/Fixtures/TemplateBasedMailerSpy.php
+++ tests/Fixtures/TemplateBasedMailerSpy.php
@@ -6,12 +6,13 @@ namespace WMDE\Fundraising\Frontend\Tests\Fixtures;
 
 use PHPUnit\Framework\TestCase;
 use WMDE\EmailAddress\EmailAddress;
+use WMDE\Fundraising\DonationContext\Infrastructure\TemplateMailerInterface as DonationTemplateMailerInterface;
 use WMDE\Fundraising\Frontend\Infrastructure\Mail\GetInTouchMailerInterface;
 
 /**
  * @license GPL-2.0-or-later
  */
-class TemplateBasedMailerSpy implements GetInTouchMailerInterface {
+class TemplateBasedMailerSpy implements GetInTouchMailerInterface, DonationTemplateMailerInterface {
 
 	private $testCase;
 	private $sendMailCalls = [];
---
+++ tests/Unit/Infrastructure/Mail/ErrorHandlingTemplateBasedMailerTest.php
@@ -0,0 +1,51 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace WMDE\Fundraising\Frontend\Tests\Unit\Infrastructure\Mail;
+
+use PHPUnit\Framework\TestCase;
+use Psr\Log\LogLevel;
+use WMDE\EmailAddress\EmailAddress;
+use WMDE\Fundraising\Frontend\Infrastructure\Mail\ErrorHandlingTemplateBasedMailer;
+use WMDE\Fundraising\Frontend\Tests\Fixtures\ErrorThrowingTemplateBasedMailer;
+use WMDE\Fundraising\Frontend\Tests\Fixtures\TemplateBasedMailerSpy;
+use WMDE\PsrLogTestDoubles\LoggerSpy;
+
+/**
+ * @covers \WMDE\Fundraising\Frontend\Infrastructure\Mail\ErrorHandlingTemplateBasedMailer
+ */
+class ErrorHandlingTemplateBasedMailerTest extends TestCase {
+
+	public function testOnSendMail_sendsMail(): void {
+		$mailerSpy = new TemplateBasedMailerSpy( $this );
+		$loggerSpy = new LoggerSpy();
+		$email = new EmailAddress( 'happy@bunny.carrot' );
+		$arguments = [ 'gotta get up' => 'to get down' ];
+
+		$errorHandlingMailer = new ErrorHandlingTemplateBasedMailer(
+			$mailerSpy,
+			$loggerSpy
+		);
+
+		$errorHandlingMailer->sendMail( $email, $arguments );
+
+		$mailerSpy->assertCalledOnceWith( $email, $arguments );
+		$this->assertSame( 0, $loggerSpy->getLogCalls()->count() );
+	}
+
+	public function testOnSendMailWithError_logsError(): void {
+		$loggerSpy = new LoggerSpy();
+
+		$errorHandlingMailer = new ErrorHandlingTemplateBasedMailer(
+			new ErrorThrowingTemplateBasedMailer(),
+			$loggerSpy
+		);
+
+		$errorHandlingMailer->sendMail( new EmailAddress( 'happy@bunny.carrot' ) );
+
+		$this->assertSame( 1, $loggerSpy->getLogCalls()->count() );
+		$this->assertSame( [ ErrorThrowingTemplateBasedMailer::ERROR_MESSAGE ], $loggerSpy->getLogCalls()->getMessages() );
+		$this->assertSame( LogLevel::ERROR, $loggerSpy->getLogCalls()->getFirstCall()->getLevel() );
+	}
+}
