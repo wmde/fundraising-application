381f491eaaab4d2a267cb33965cfcf96b00c0f04
Abban Dunne <abban.dunne@wikimedia.de>
2021-09-29 13:10:54 +0200

    Add salutations
    This adds a salutations object that initialises
    using a new json file in fundraising content
    
    Tickets:
    * https://phabricator.wikimedia.org/T291356
    * https://phabricator.wikimedia.org/T291357

---
+++ src/Presentation/Salutations.php
@@ -0,0 +1,32 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace WMDE\Fundraising\Frontend\Presentation;
+
+class Salutations {
+
+	/**
+	 * @var array
+	 */
+	private array $salutations;
+
+	public function __construct( array $salutations ) {
+		$this->salutations = $salutations;
+	}
+
+	public function getList(): array {
+		return $this->salutations;
+	}
+
+	public function getSalutation( string $value ): ?array {
+		$data = array_filter( $this->salutations, fn( $salutation ) => $salutation['value'] == $value );
+
+		if ( count( $data ) === 0 ) {
+			return null;
+		}
+
+		return array_values( $data )[0];
+	}
+
+}
---
+++ tests/Unit/Presentation/SalutationsTest.php
@@ -0,0 +1,61 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace WMDE\Fundraising\Frontend\Tests\Unit\Presentation;
+
+use PHPUnit\Framework\TestCase;
+use WMDE\Fundraising\Frontend\Presentation\Salutations;
+
+/**
+ * @covers \WMDE\Fundraising\Frontend\Presentation\Salutations
+ */
+class SalutationsTest extends TestCase {
+	public function testGetListReturnsSalutations(): void {
+		$salutationsArray = [
+			[
+				'label' => 'Herr',
+				'value' => 'Herr',
+				'display' => '',
+				'greetings' => [
+					'formal' => 'formal',
+					'informal' => 'informal',
+					'lastNameInformal' => 'lastNameInformal',
+				],
+			]
+		];
+
+		$salutations = new Salutations( $salutationsArray );
+
+		$this->assertEquals( $salutationsArray, $salutations->getList() );
+	}
+
+	public function testGetSalutationReturnsSalutation(): void {
+		$salutationsArray = [
+			[
+				'label' => 'Herr',
+				'value' => 'Herr',
+				'display' => 'Herr',
+				'greetings' => [
+					'formal' => 'formal',
+					'informal' => 'informal',
+					'lastNameInformal' => 'lastNameInformal',
+				],
+			],
+			[
+				'label' => 'Frau',
+				'value' => 'Frau',
+				'display' => 'Frau',
+				'greetings' => [
+					'formal' => 'formal',
+					'informal' => 'informal',
+					'lastNameInformal' => 'lastNameInformal',
+				],
+			]
+		];
+
+		$salutations = new Salutations( $salutationsArray );
+
+		$this->assertEquals( $salutationsArray[1], $salutations->getSalutation( 'Frau' ) );
+	}
+}
