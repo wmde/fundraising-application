a5a42abba98429b4c270ffab47490b01f8534f87
Gabriel Birke <gabriel.birke@posteo.net>
2021-12-09 16:25:57 +0100

    Add API platform
    Add composer package and basic configuration:
    
    - Bundle
    - package config
    - route config

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ declare( strict_types = 1 );
 return [
 	Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
 	Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+	\ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true]
 ];
