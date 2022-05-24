151f6dd837c41dd493803775e01e98df15253c9a
Gabriel Birke <gabriel.birke@posteo.net>
2022-05-24 13:53:15 +0200

    Document bucket testing (#2474)
    Document bucket testing
    
    Bucket testing has a lot of classes related to it. This document
    explains how they fit together.
    
    Also remove a comment from BucketTestingContextFactory that no longer applies
    
    Co-authored-by: Co <corinna.hillebrand@wikimedia.de>

--- src/BucketTesting/BucketTestingContextFactory.php
+++ src/BucketTesting/BucketTestingContextFactory.php
@@ -11,9 +11,6 @@ class BucketTestingContextFactory {
 	private const DOCTRINE_CLASS_MAPPING_DIRECTORY = __DIR__ . '/config/DoctrineClassMapping';
 
 	public function newMappingDriver(): MappingDriver {
-		// We're only calling this for the side effect of adding Mapping/Driver/DoctrineAnnotations.php
-		// to the AnnotationRegistry. When AnnotationRegistry is deprecated with Doctrine Annotations 2.0,
-		// use $this->annotationReader instead
 		return new XmlDriver( self::DOCTRINE_CLASS_MAPPING_DIRECTORY );
 	}
 }
