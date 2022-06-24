307306bf485355bc49832d65bbadc59d25f1ca9a
Gabriel Birke <gabriel.birke@wikimedia.de>
2022-06-24 15:43:31 +0200

    Adapt validation routes for new payment
    This is for https://phabricator.wikimedia.org/T310994

--- app/Controllers/Validation/ValidateIbanController.php
+++ app/Controllers/Validation/ValidateIbanController.php
@@ -8,13 +8,12 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use WMDE\Fundraising\Frontend\Factories\FunFunFactory;
-use WMDE\Fundraising\PaymentContext\Domain\Model\Iban;
 
 class ValidateIbanController {
 
 	public function index( Request $request, FunFunFactory $ffFactory ): Response {
 		$useCase = $ffFactory->newCheckIbanUseCase();
-		$checkIbanResponse = $useCase->checkIban( new Iban( $request->query->get( 'iban', '' ) ) );
+		$checkIbanResponse = $useCase->ibanIsValid( $request->query->get( 'iban', '' ) );
 		return new JsonResponse( $ffFactory->newIbanPresenter()->present( $checkIbanResponse ) );
 	}
 }
