97239f698fe4fd98a4942db5c981c63ff636f52b
Gabriel Birke <gabriel.birke@wikimedia.de>
2022-08-10 17:52:06 +0200

    Change table name in initial data
    After we renamed the last_generated_payment_id in the payments context
    we need to change it for the initial data of the development
    environment.

--- .docker/database/02.Default_Data.sql
+++ .docker/database/02.Default_Data.sql
@@ -1,4 +1,4 @@
-INSERT INTO payment_id (payment_id) VALUES (0);
+INSERT INTO last_generated_payment_id (payment_id) VALUES (0);
 
 -- Only needed until https://phabricator.wikimedia.org/T270721 is done
 INSERT INTO incentive (`id`, `name`) VALUES (1, 'tote_bag');
