021b29cc37e13625b824cb4f0decb418bb5633e1
Gabriel Birke <gabriel.birke@wikimedia.de>
2022-10-21 11:59:54 +0200

    Check for non-string values

--- src/Infrastructure/ParameterBagEncodingConverter.php
+++ src/Infrastructure/ParameterBagEncodingConverter.php
@@ -1,14 +1,17 @@
 <?php
-declare(strict_types=1);
+declare( strict_types=1 );
 
 namespace WMDE\Fundraising\Frontend\Infrastructure;
 
 use Symfony\Component\HttpFoundation\ParameterBag;
 
-class ParameterBagEncodingConverter
-{
-	public static function convert(ParameterBag $input, string $fromEncoding, string $toEncoding = 'UTF-8' ): ParameterBag
-	{
-		return $input;
+class ParameterBagEncodingConverter {
+	public static function convert( ParameterBag $input, string $fromEncoding, string $toEncoding = 'UTF-8' ): ParameterBag {
+		return new ParameterBag(
+			array_map(
+				fn( $value ) => iconv( $fromEncoding, $toEncoding, $value ),
+				$input->all()
+			),
+		);
 	}
 }
--- tests/Unit/Infrastructure/ParameterBagEncodingConverterTest.php
+++ tests/Unit/Infrastructure/ParameterBagEncodingConverterTest.php
@@ -1,32 +1,51 @@
 <?php
-declare(strict_types=1);
+declare( strict_types=1 );
 
 namespace WMDE\Fundraising\Frontend\Tests\Unit\Infrastructure;
 
+use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpFoundation\ParameterBag;
 use WMDE\Fundraising\Frontend\Infrastructure\ParameterBagEncodingConverter;
-use PHPUnit\Framework\TestCase;
 
 /**
  * @covers \WMDE\Fundraising\Frontend\Infrastructure\ParameterBagEncodingConverter
  */
-class ParameterBagEncodingConverterTest extends TestCase
-{
-	public function testGivenEmptyParameterBagResultIsEmpty(): void {
+class ParameterBagEncodingConverterTest extends TestCase {
+	public function testGivenEmptyParameterBagThenResultIsEmpty(): void {
 		$input = new ParameterBag();
 
-		$result = ParameterBagEncodingConverter::convert( $input,'ISO-8859-1' );
+		$result = ParameterBagEncodingConverter::convert( $input, 'ISO-8859-1' );
 
 		$this->assertSame( [], $result->all() );
 	}
-	public function testGivenCorrectParameterBAgResultIsConverted():void
-	{
-		$input = new ParameterBag(['Encoded_Value'=>"Bitte \xe4ndern"]);
 
-		// $input="Bitte \xe4ndern";
-		
-		$result = ParameterBagEncodingConverter::convert( $input,'ISO-8859-1');
+	public function testGivenIsoLatin1EncodedParameterBagThenResultIsConverted(): void {
+		$input = new ParameterBag( [ 'Encoded_Value' => "Bitte \xe4ndern" ] );
 
-		$this->assertSame( 'Bitte ändern', $result->get('Encoded_Value') );
+		$result = ParameterBagEncodingConverter::convert( $input, 'ISO-8859-1' );
+
+		$this->assertSame( 'Bitte ändern', $result->get( 'Encoded_Value' ) );
+	}
+
+	/**
+	 * @dataProvider typedValues
+	 */
+	public function testGivenNonStringInParameterBagThenValueShouldBeUntouched( mixed $value ): void {
+		$input = new ParameterBag( ['some_value' => $value] );
+
+		$result = ParameterBagEncodingConverter::convert( $input, 'ISO-8859-1' );
+
+		$this->assertSame( $value, $result->get('some_value') );
+	}
+
+	/**
+	 * @return iterable<string,mixed>
+	 */
+	public function typedValues(): iterable {
+		yield 'Numeric String' => [ '44' ];
+		yield 'Int' => [ 27 ];
+		yield 'Float' => [ 3.14 ];
+		yield 'Boolean' => [ false ];
+		yield 'null' => [ null ];
 	}
 }
