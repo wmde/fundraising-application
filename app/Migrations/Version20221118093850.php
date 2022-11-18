dd400a6a26cc2ff3957c2fb770d9f4ff9809847b
Abban Dunne <abban.dunne@wikimedia.de>
2022-11-18 10:55:20 +0100

    Increase bucket_log event_name length
    Ticket: https://phabricator.wikimedia.org/T323353

---
+++ app/Migrations/Version20221118093850.php
@@ -0,0 +1,27 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace WMDE\Fundraising\Frontend\App\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20221118093850 extends AbstractMigration {
+
+	public function getDescription(): string {
+		return 'Increase the length of the bucket log event name column';
+	}
+
+	public function up( Schema $schema ): void {
+		$table = $schema->getTable( 'bucket_log' );
+		$table->changeColumn( 'event_name', [ 'length' => 64 ] );
+
+		$this->addSql( 'UPDATE bucket_log SET event_name = "membershipApplicationCreated" WHERE event_name = "membershipApplicationCre"' );
+	}
+
+	public function down( Schema $schema ): void {
+		$table = $schema->getTable( 'bucket_log' );
+		$table->changeColumn( 'event_name', [ 'length' => 24 ] );
+	}
+}
