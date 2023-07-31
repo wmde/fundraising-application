dd26b274a7a081fcca47f3249bf29bca4879efff
Gabriel Birke <gabriel.birke@wikimedia.de>
2023-07-31 16:38:59 +0200

    Integrate Commands from payment BC
    Add services to Symfony configuration so its dependency injection
    framework can find the commands for creating and listing subscription
    plans in the payment bounded context.
    
    Loading the credentials from the `.env` files is done automatically in
    `bin/console`, so the credentials will exist when the environ-specific
    factory needs them.
    
    https://phabricator.wikimedia.org/T341568
    
    Co-Authored-By: Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
    Co-Authored-By: Sperling-0 <tanuja.doriya@wikimedia.de>

--- src/Factories/FunFunFactory.php
+++ src/Factories/FunFunFactory.php
@@ -2135,7 +2135,7 @@ class FunFunFactory implements LoggerAwareInterface {
 		$this->sharedObjects[PaypalAPI::class] = $paypalAPI;
 	}
 
-	private function getPayPalApiClient(): PaypalAPI {
+	public function getPayPalApiClient(): PaypalAPI {
 		return $this->createSharedObject( PaypalAPI::class, static function (): PaypalAPI {
 			return new PayPalAPIStub();
 		} );
