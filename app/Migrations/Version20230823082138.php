e3fe70399220735e148ef3aa25815356e12eae74
Gabriel Birke <gabriel.birke@wikimedia.de>
2023-10-13 17:28:01 +0200

    Fix Migration for Authentication
    Add Application migration files to migration configuration.
    Delete old XML configuration - it was not used and led to confusion.
    
    Correct table name in migration
    
    Fix schema definition for authentication token table - `nullable` is
    only used in the ORM API, `notnull` is the option for the Schema API

--- app/Migrations/Version20230823082138.php
+++ app/Migrations/Version20230823082138.php
@@ -13,18 +13,18 @@ final class Version20230823082138 extends AbstractMigration {
 	}
 
 	public function up( Schema $schema ): void {
-		$table = $schema->createTable( 'legacy_authentication_tokens' );
+		$table = $schema->createTable( 'legacy_auth_tokens' );
 		$table->addColumn( 'id', 'integer', [ 'unsigned' => true ] );
 		$table->addColumn( 'authentication_context', 'string', [ 'length' => 16 ] );
 		$table->addColumn( 'access_token', 'string', [ 'length' => 64 ] );
 		$table->addColumn( 'update_token', 'string', [ 'length' => 64 ] );
-		$table->addColumn( 'update_token_expiry', 'datetime', [ 'nullable' => false ] );
+		$table->addColumn( 'update_token_expiry', 'datetime', [ 'notnull' => false ] );
 		$table->setPrimaryKey( [ 'id', 'authentication_context' ] );
 		$table->addIndex( [ 'access_token' ], 'access_token_idx' );
 		$table->addIndex( [ 'update_token' ], 'update_token_idx' );
 	}
 
 	public function down( Schema $schema ): void {
-		$schema->dropTable( 'legacy_authentication_tokens' );
+		$schema->dropTable( 'legacy_auth_tokens' );
 	}
 }
