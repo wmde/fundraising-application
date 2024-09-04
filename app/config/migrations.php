0beadd0a537b5f162762f24f8fb34c83e0852283
Sperling-0 <tanujadoriya@gmail.com>
2024-09-04 10:57:46 +0200

    Adapt migrations configurations
    - This is a follow-up to f9b132395cb411202e1653708e0e74273d067292
    - Newer versions of MariaDB use utf8mb4_unicode_ci, which is not compatible to the key length in the configuration file
    - New version of Doctrine migrations has deprecated transactional changes
    
    Ticket: https://phabricator.wikimedia.org/T370617
    
    Co-authored-by: Gabriel Birke <gabriel.birke@wikimedia.de>

--- app/config/migrations.php
+++ app/config/migrations.php
@@ -5,7 +5,7 @@ return [
 	'table_storage' => [
 		'table_name' => 'doctrine_migration_versions',
 		'version_column_name' => 'version',
-		'version_column_length' => 1024,
+		'version_column_length' => 250,
 		'executed_at_column_name' => 'executed_at',
 		'execution_time_column_name' => 'execution_time',
 	],
@@ -18,7 +18,7 @@ return [
 	],
 
 	'all_or_nothing' => true,
-	'transactional' => true,
+	'transactional' => false,
 	'check_database_platform' => true,
 	'organize_migrations' => 'none',
 	'connection' => null,
