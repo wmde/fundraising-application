b3121a890d89389f9254a5358d9335b02aeab051
Abban Dunne <abban.dunne@wikimedia.de>
2025-05-06 16:36:37 +0200

    Add pattern library route

---
+++ app/Controllers/StaticContent/ShowPatternLibraryController.php
@@ -0,0 +1,19 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace WMDE\Fundraising\Frontend\App\Controllers\StaticContent;
+
+use Symfony\Component\HttpFoundation\Response;
+use WMDE\Fundraising\Frontend\Factories\FunFunFactory;
+
+class ShowPatternLibraryController {
+	public function index( FunFunFactory $ffFactory ): Response {
+		$template = $ffFactory->getLayoutTemplate( 'Pattern_Library.html.twig' );
+		return new Response( $template->render(
+			[
+				'faq_content' => $ffFactory->getFaqContent(),
+			]
+		) );
+	}
+}
