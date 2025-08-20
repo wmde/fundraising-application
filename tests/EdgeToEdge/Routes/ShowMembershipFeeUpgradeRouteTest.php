77fb7f89ecb1b3281c35ea3e101839c277005652
Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
2025-08-20 15:32:45 +0200

    Connect ShowMembershipFeeUpgradeController to repository
    - create a function to get the repository in the funfunfactory
    - adds a web route test
    
    https://phabricator.wikimedia.org/T395016

--- tests/EdgeToEdge/Routes/ShowMembershipFeeUpgradeRouteTest.php
+++ tests/EdgeToEdge/Routes/ShowMembershipFeeUpgradeRouteTest.php
@@ -7,27 +7,74 @@ namespace EdgeToEdge\Routes;
 use PHPUnit\Framework\Attributes\CoversClass;
 use WMDE\Fundraising\Frontend\App\Controllers\Membership\ShowMembershipFeeUpgradeController;
 use WMDE\Fundraising\Frontend\Tests\EdgeToEdge\WebRouteTestCase;
+use Symfony\Component\HttpFoundation\Response;
+use WMDE\Fundraising\MembershipContext\Tests\Fixtures\FeeChanges;
 
 #[CoversClass( ShowMembershipFeeUpgradeController::class )]
 class ShowMembershipFeeUpgradeRouteTest extends WebRouteTestCase {
 
 	private const PATH = '/show-membership-confirmation';
-	private const WRONG_ACCESS_TOKEN = 'foobar';
-	private const VALID_ACCESS_TOKEN = 9998;
+	private const INVALID_TEST_UUID = 'foorchbar';
+	private const VALID_TEST_UUID = FeeChanges::UUID_1;
+
+	private const INVALID_TEST_EMAIL = '';
+	private const VALID_TEST_EMAIL = FeeChanges::EMAIL;
 
 	public function testNoUUIDInRequest_rendersErrorPageWithCustomMessage(): void {
 		//TODO test custom message
+		$client = $this->createClient();
+		$client->request(
+			'GET',
+			'show-membership-fee-upgrade',
+			[
+				'email' => self::VALID_TEST_EMAIL
+			]
+		);
+
+		$response = $client->getResponse();
 		$this->assertFalse();
 	}
 
 	public function testInvalidUUIDInRequest_rendersErrorPageWithCustomMessage(): void {
 		//TODO test custom message
+		$client = $this->createClient();
+		$client->request(
+			'GET',
+			'show-membership-fee-upgrade',
+			[
+				'uuid' => self::INVALID_TEST_UUID,
+				'email' => self::VALID_TEST_EMAIL
+			]
+		);
+
+		$response = $client->getResponse();
 		$this->assertFalse();
 	}
 
 	public function testValidUUIDInRequest_rendersFeeUpgradeForm(): void {
-		$this->assertFalse();
+		$client = $this->createClient();
+		$this->givenStoredFeeChangeInRepository();
+
+
+		$client->request(
+			'GET',
+			'show-membership-fee-upgrade',
+			[
+				'uuid' => self::VALID_TEST_UUID,
+				'email' => self::VALID_TEST_EMAIL
+			]
+		);
+
+		$response = $client->getResponse();
+
+		$this->assertEquals(new Response(), $response);
 	}
 
+ private function givenStoredFeeChangeInRepository(): void {
+		$feeChange = FeeChanges::newNewFeeChange( FeeChanges::UUID_1 );
+	 	$ffFactory = $this->getFactory();
+		$feeChangeRepository = $ffFactory->getFeeChangeRepository();
+		$feeChangeRepository->storeFeeChange( $feeChange );
+ }
 
 }
