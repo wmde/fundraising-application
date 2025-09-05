515625937d73d1c07bb38cd0364be3234a7536d1
Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
2025-09-05 17:41:21 +0200

    Update database schemas for local docker environment

--- .docker/database/02.Default_Data.sql
+++ .docker/database/02.Default_Data.sql
@@ -4,3 +4,57 @@ INSERT INTO last_generated_membership_id (membership_id) VALUES (0);
 
 -- Only needed until https://phabricator.wikimedia.org/T270721 is done
 INSERT INTO incentive (`id`, `name`) VALUES (1, 'tote_bag');
+
+INSERT INTO membership_fee_changes (
+                                    uuid,
+                                    external_member_id,
+                                    current_amount_in_cents,
+                                    suggested_amount_in_cents,
+                                    current_interval,
+                                    state,
+                                    export_date
+) VALUES (
+        '12345-abcde',
+        987654321,
+        500,
+        1000,
+        3,
+		'NEW',
+        null
+);
+
+INSERT INTO membership_fee_changes (
+	uuid,
+	external_member_id,
+	current_amount_in_cents,
+	suggested_amount_in_cents,
+	current_interval,
+	state,
+	export_date
+) VALUES (
+			 '1234-abcd',
+			 98765432,
+			 1000,
+			 2000,
+			 12,
+			 'FILLED',
+			 null
+		 );
+
+INSERT INTO membership_fee_changes (
+	uuid,
+	external_member_id,
+	current_amount_in_cents,
+	suggested_amount_in_cents,
+	current_interval,
+	state,
+	export_date
+) VALUES (
+			 '123-abc',
+			 9876543,
+			 500,
+			 1000,
+			 6,
+			 'EXPORTED',
+			 '2025-04-03 1:02:00'
+		 );
